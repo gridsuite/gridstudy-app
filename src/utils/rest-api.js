dcc23d1adcfe6c6e0499e2b33e0446731f3ba467
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-02-07 13:33:43 +0100

    static studies
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/utils/rest-api.js
+++ src/utils/rest-api.js
@@ -5,13 +5,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-// Routes
-const networksUrl = process.env.REACT_APP_API_NETWORK_STORE_SERVER + "/v1/networks"
-const studiesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies"
-
 export function fetchStudies() {
     console.info("Fetching studies...");
-    return fetch(studiesUrl)
+    return fetch('studies.json')
         .then(response => response.json());
 }
 
