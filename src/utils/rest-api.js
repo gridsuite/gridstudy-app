a00252953bccca941375aa2c01eda4597ebd20da
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-02-20 13:42:26 +0100

    Build docker image (#9)
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/utils/rest-api.js
+++ src/utils/rest-api.js
@@ -5,13 +5,59 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-// Routes
-const networksUrl = process.env.REACT_APP_API_NETWORK_STORE_SERVER + "/v1/networks"
-const studiesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies"
-
 export function fetchStudies() {
     console.info("Fetching studies...");
-    return fetch('studies.json')
+    const fetchStudiesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies";
+    console.debug(fetchStudiesUrl)
+    return fetch(fetchStudiesUrl)
+        .then(response => response.json());
+}
+
+export function fetchCases() {
+    console.info("Fetching cases...");
+    const fetchCasesUrl = process.env.REACT_APP_API_CASE_SERVER + "/v1/cases";
+    console.debug(fetchCasesUrl)
+    return fetch(fetchCasesUrl)
+        .then(response => response.json());
+}
+
+export function fetchVoltageLevelDiagram(studyName, voltageLevelId) {
+    console.info(`Fetching voltage level diagram '${voltageLevelId}' of study '${studyName}'...`);
+    const fetchVoltageLevelDiagramUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network/voltage-levels/" + voltageLevelId + "/svg";
+    console.debug(fetchVoltageLevelDiagramUrl)
+    return fetch(fetchVoltageLevelDiagramUrl)
+        .then(response => response.text());
+}
+
+export function fetchSubstations(studyName) {
+    console.info(`Fetching substations of study '${studyName}'...`);
+    const fetchSubstationsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network-map/substations";
+    console.debug(fetchSubstationsUrl)
+    return fetch(fetchSubstationsUrl)
+        .then(response => response.json());
+}
+
+export function fetchSubstationPositions(studyName) {
+    console.info(`Fetching substation positions of study '${studyName}'...`);
+    const fetchSubstationPositionsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/geo-data/substations";
+    console.debug(fetchSubstationPositionsUrl)
+    return fetch(fetchSubstationPositionsUrl)
+        .then(response => response.json());
+}
+
+export function fetchLines(studyName) {
+    console.info(`Fetching lines of study '${studyName}'...`);
+    const fetchLinesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network-map/lines";
+    console.debug(fetchLinesUrl)
+    return fetch(fetchLinesUrl)
+        .then(response => response.json());
+}
+
+export function fetchLinePositions(studyName) {
+    console.info(`Fetching line positions of study '${studyName}'...`);
+    const fetchLinePositionsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/geo-data/lines";
+    console.debug(fetchLinePositionsUrl)
+    return fetch(fetchLinePositionsUrl)
         .then(response => response.json());
 }
 
@@ -19,32 +65,3 @@ export function createStudy() {
     console.info("Creating a new study...");
 }
 
-export function fetchVoltageLevelDiagram(studyName, voltageLevelId) {
-    console.info(`Fetching voltage level diagram '${voltageLevelId}' of study '${studyName}'...`);
-    return fetch('sld.svg')
-        .then(response => response.text());
-}
-
-export function fetchSubstations(studyName) {
-    console.info(`Fetching substations of study '${studyName}'...`);
-    return fetch('substations.json')
-        .then(response => response.json());
-}
-
-export function fetchSubstationPositions(studyName) {
-    console.info(`Fetching substation positions of study '${studyName}'...`);
-    return fetch('substation-positions.json')
-        .then(response => response.json());
-}
-
-export function fetchLines(studyName) {
-    console.info(`Fetching lines of study '${studyName}'...`);
-    return fetch('lines.json')
-        .then(response => response.json());
-}
-
-export function fetchLinePositions(studyName) {
-    console.info(`Fetching line positions of study '${studyName}'...`);
-    return fetch('line-positions.json')
-        .then(response => response.json());
-}
