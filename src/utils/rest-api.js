15ec1c9a3ff09fdbf705fb74f32d664680d2bcb6
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-02-20 10:10:15 +0100

    MAJ rest-api
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/utils/rest-api.js
+++ src/utils/rest-api.js
@@ -5,46 +5,59 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-// Routes
-const networksUrl = process.env.REACT_APP_API_NETWORK_STORE_SERVER + "/v1/networks"
-const studiesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies"
-
 export function fetchStudies() {
     console.info("Fetching studies...");
-    return fetch('studies.json')
+    let fetchStudiesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies"
+    console.log(fetchStudiesUrl)
+    return fetch(fetchStudiesUrl)
         .then(response => response.json());
 }
 
-export function createStudy() {
-    console.info("Creating a new study...");
+export function fetchCases() {
+    console.info("Fetching cases...");
+    let fetchCasesUrl = process.env.REACT_APP_API_CASE_SERVER + "/v1/cases"
+    console.log(fetchCasesUrl)
+    return fetch(fetchCasesUrl)
+        .then(response => response.json());
 }
 
 export function fetchVoltageLevelDiagram(studyName, voltageLevelId) {
     console.info(`Fetching voltage level diagram '${voltageLevelId}' of study '${studyName}'...`);
-    return fetch('sld.svg')
+    const fetchVoltageLevelDiagramUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network/voltage-levels/" + voltageLevelId + "/svg"
+    console.log(fetchVoltageLevelDiagramUrl)
+    return fetch(fetchVoltageLevelDiagramUrl)
         .then(response => response.text());
 }
 
 export function fetchSubstations(studyName) {
     console.info(`Fetching substations of study '${studyName}'...`);
-    return fetch('substations.json')
+    const fetchSubstationsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network-map/substations"
+    console.log(fetchSubstationsUrl)
+    return fetch(fetchSubstationsUrl)
         .then(response => response.json());
 }
 
 export function fetchSubstationPositions(studyName) {
     console.info(`Fetching substation positions of study '${studyName}'...`);
-    return fetch('substation-positions.json')
+    const fetchSubstationPositionsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/geo-data/substations"
+    console.log(fetchSubstationPositionsUrl)
+    return fetch(fetchSubstationPositionsUrl)
         .then(response => response.json());
 }
 
 export function fetchLines(studyName) {
     console.info(`Fetching lines of study '${studyName}'...`);
-    return fetch('lines.json')
+    const fetchLinesUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/network-map/lines"
+    console.log(fetchLinesUrl)
+    return fetch(fetchLinesUrl)
         .then(response => response.json());
 }
 
 export function fetchLinePositions(studyName) {
     console.info(`Fetching line positions of study '${studyName}'...`);
-    return fetch('line-positions.json')
+    const fetchLinePositionsUrl = process.env.REACT_APP_API_STUDY_SERVER + "/v1/studies/" + studyName + "/geo-data/lines"
+    console.log(fetchLinePositionsUrl)
+    return fetch(fetchLinePositionsUrl)
         .then(response => response.json());
 }
+
