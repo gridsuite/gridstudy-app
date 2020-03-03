916c8bb4ffc9e9e220c552b647f47f717650e92c
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-03-03 22:08:13 +0100

    Fix unknown positions bug (#24)
    *        fix unknown positions bug
    
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
    
    * Add warning
    
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
    
    Co-authored-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -84,7 +84,8 @@ export default class Network {
     getSubstationPosition(substation) {
         const position = this.substationPositionsById.get(substation);
         if (!position) {
-            throw new Error(`Position not found for ${substation}`);
+            console.warn(`Position not found for ${substation}`);
+            return [0, 0, 0];
         }
         return [position.lon, position.lat, 0];
     }
