a5c414a676b4a8110b383dab2f9c5f4bfc04a541
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-03-11 16:15:20 +0100

    Cleanup
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/components/network/network-map.js
+++ src/components/network/network-map.js
@@ -166,7 +166,6 @@ const NetworkMap = (props) => {
                 preventStyleDiffing={true}
                 mapboxApiAccessToken={MAPBOX_TOKEN}>
                 { renderTooltip() }
-                { props.children }
             </StaticMap>
             <div style={{ position: "absolute", right: 10, top: 10, zIndex: 1 }}>
                 <NavigationControl />
