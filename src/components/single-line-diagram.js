1fbff767bfabc3d22bcb0f480814e9dacafd5ef6
Jon Harper <jon.harper87@gmail.com>
2020-03-17 17:56:47 +0100

    Move svg fetching in single-line-diagram
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- src/components/single-line-diagram.js
+++ src/components/single-line-diagram.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
 import Box from '@material-ui/core/Box';
@@ -14,6 +14,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Paper from "@material-ui/core/Paper";
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from '@material-ui/core/Typography';
+import { fetchSvg } from "../utils/rest-api";
 
 const useStyles = makeStyles(theme => ({
     diagram: {
@@ -32,6 +33,19 @@ const useStyles = makeStyles(theme => ({
 
 const SingleLineDiagram = (props) => {
 
+    const [svg, setSvg] = useState(null);
+
+    useEffect(() => {
+        if (props.svgUrl != null) {
+            fetchSvg(props.svgUrl)
+                .then(svg => {
+                    setSvg(svg);
+                });
+        } else {
+            setSvg(null);
+        }
+    }, [props.svgUrl]);
+
     useEffect(() => {
         var svg = document.getElementById("sld-svg").getElementsByTagName("svg")[0];
         if (svg) {
@@ -40,7 +54,7 @@ const SingleLineDiagram = (props) => {
             var bbox = svg.getBBox();
             svg.setAttribute("viewBox", 0 + " " + 0 + " " + (bbox.width + 20) + " " + (bbox.height + 20));
         }
-    }, [props.svg]);
+    }, [svg]);
 
     const classes = useStyles();
 
@@ -60,14 +74,14 @@ const SingleLineDiagram = (props) => {
                     <CloseIcon/>
                 </IconButton>
             </Box>
-            <div id="sld-svg" style={{height : '100%'}} dangerouslySetInnerHTML={{__html:props.svg}}/>
+            <div id="sld-svg" style={{height : '100%'}} dangerouslySetInnerHTML={{__html:svg}}/>
         </Paper>
     );
 };
 
 SingleLineDiagram.propTypes = {
     diagramTitle: PropTypes.string.isRequired,
-    svg: PropTypes.string.isRequired,
+    svgUrl: PropTypes.string.isRequired,
     onClose: PropTypes.func
 };
 
