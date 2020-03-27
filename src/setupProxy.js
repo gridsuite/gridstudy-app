29066df85a52e2c7508e6af5cdfdbd8dc8c816fc
Jon Harper <jon.harper87@gmail.com>
2020-03-27 15:59:02 +0100

    Simplify head handling and add dev reverse proxy
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware('http://localhost:5001/api/study-server', {
+        pathRewrite: { '^/api/study-server/': '/' }
+    })
+  );
+  app.use(
+    createProxyMiddleware('http://localhost:5000/api/case-server', {
+        pathRewrite: { '^/api/case-server/': '/' }
+    })
+  );
+};
