c69c36721dd732f5a0c82168746ca449d0aec719
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-03-31 18:28:30 +0200

    cosmetic changes
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,5 +7,4 @@
 import {createStore} from 'redux'
 import {reducer} from './reducer'
 
-export const store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(reducer);
