a05235cadad5f1ce173b3f37d674d7d7a5be4766
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-04-02 16:43:23 +0200

    update AuthService
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/utils/authentication/AuthService.js
+++ src/utils/authentication/AuthService.js
@@ -6,19 +6,24 @@
  */
 import {UserManager} from 'oidc-client';
 import {UserManagerMock} from "./UserManagerMock";
+let userManagerPromise;
+if (process.env.REACT_APP_USE_AUTHENTICATION === "true") {
+    userManagerPromise = fetch('idpSettings.json')
+        .then(r => r.json())
+        .then(idpSettings => {
+            let settings = {
+                authority: idpSettings.authority,
+                client_id: idpSettings.client_id,
+                redirect_uri: idpSettings.redirect_uri,
+                post_logout_redirect_uri: idpSettings.post_logout_redirect_uri,
+                response_mode: 'fragment',
+                response_type: 'id_token token',
+                scope: idpSettings.scope,
+            };
+            return new UserManager(settings);
+        });
+} else {
+    userManagerPromise = Promise.resolve( new UserManagerMock({}));
+}
 
-const userManagerPromise = fetch('idpSettings.json')
-    .then(r => r.json())
-    .then(idpSettings => {
-        let settings = {
-            authority: idpSettings.authority,
-            client_id: idpSettings.client_id,
-            redirect_uri: idpSettings.redirect_uri,
-            post_logout_redirect_uri: idpSettings.post_logout_redirect_uri,
-            response_mode : 'fragment',
-            response_type: 'id_token token',
-            scope: idpSettings.scope,
-        };
-        return process.env.REACT_APP_USE_AUTHENTICATION === "true" ? new UserManager(settings) : new UserManagerMock(settings);
-    });
 export {userManagerPromise}
