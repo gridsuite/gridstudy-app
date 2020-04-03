ef62f0f9b8acaa7559a123426f7b2d8fa397272d
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-04-03 14:56:25 +0200

    Authentication (#54)
    Add authentication
    
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -11,14 +11,24 @@ import {FormattedMessage} from "react-intl";
 import {useHistory} from 'react-router-dom';
 
 import AppBar from "@material-ui/core/AppBar";
-import IconButton from "@material-ui/core/IconButton";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 import {makeStyles} from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import SettingsIcon from '@material-ui/icons/Settings';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import {ReactComponent as PowsyblLogo} from "../images/powsybl_logo.svg";
 import PropTypes from "prop-types";
+import {useSelector} from "react-redux";
 
 const useStyles = makeStyles(() => ({
     grow: {
@@ -35,13 +45,56 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
+const StyledMenu = withStyles({
+    paper: {
+        border: '1px solid #d3d4d5',
+    },})(props => (
+        <Menu
+            elevation={0}
+            getContentAnchorEl={null}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+            }}
+            {...props}
+        />
+));
+
+const StyledMenuItem = withStyles(theme => ({
+    root: {
+        '&:focus': {
+            backgroundColor: theme.palette.primary.main,
+            '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+                color: theme.palette.common.white,
+            },
+        },
+    },
+}))(MenuItem);
+
 const TopBar = (props) => {
+
+    const user = useSelector(state => state.user);
 
     const classes = useStyles();
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     const history = useHistory();
 
+
     const onParametersClick = () => {
+        handleClose();
       if (props.onParametersClick) {
           props.onParametersClick();
       }
@@ -58,10 +111,45 @@ const TopBar = (props) => {
                 <Typography variant="h6" className={classes.title} onClick={onLogoClick}>
                     <FormattedMessage id="appName"/>
                 </Typography>
-                <div className={classes.grow}/>
-                <IconButton aria-label="Parameters" color="inherit" onClick={onParametersClick}>
-                    <SettingsIcon/>
-                </IconButton>
+                <div className={classes.grow} />
+                <h3>{user !== null ? user.profile.name : ""}</h3>
+
+                {user && (
+                <div>
+                    <Button
+                        aria-controls="customized-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                    >
+                        <MenuIcon/>
+                    </Button>
+
+                  <StyledMenu
+                        id="customized-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <StyledMenuItem onClick={onParametersClick}>
+                            <ListItemIcon>
+                                <SettingsIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>
+                                <FormattedMessage id="settings"/>
+                            </ListItemText>
+                        </StyledMenuItem>
+                        <StyledMenuItem onClick={props.onLogoutClick}>
+                            <ListItemIcon>
+                                <ExitToAppIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText >
+                                <FormattedMessage id="logout"/>
+                            </ListItemText>
+                        </StyledMenuItem>
+                    </StyledMenu>
+                </div>
+                    )}
             </Toolbar>
         </AppBar>
     )
