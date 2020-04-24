f680fd111f1f9842c8a7dbf1a0dfe5dc5315be9e
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-04-24 12:54:18 -0400

    Token silent renew  (#97)
    add silent renew on expiring event
    
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/components/app.js
+++ src/components/app.js
@@ -18,8 +18,18 @@ import StudyManager from './study-manager';
 import TopBar from './top-bar';
 import {LIGHT_THEME} from '../redux/actions'
 import Parameters from "./parameters";
-import {userManagerPromise, login, logout, handleSigninCallback, dispatchUser, getPreLoginPath} from '../utils/authentication/AuthService';
+
+import {
+    login,
+    logout,
+    handleSigninCallback,
+    getPreLoginPath,
+    handleSilentRenewCallback,
+    initializeAuthentication
+} from '../utils/authentication/AuthService';
+
 import Authentication from "./authentication";
+import {useRouteMatch} from "react-router";
 
 const lightTheme = createMuiTheme({
     palette: {
@@ -55,6 +65,18 @@ const SignInCallback = (props) => {
     )
 };
 
+const SilentRenewCallback = (props) => {
+    useEffect(() => {
+        if (props.userManager.instance !== null) {
+            props.handleSilentRenewCallback();
+        }
+    }, [props.userManager]);
+
+    return (
+        <h1>Technical token renew window, you should not see this</h1>
+    )
+};
+
 const noUserManager = {instance: null, error: null};
 
 const App = () => {
@@ -75,14 +97,18 @@ const App = () => {
 
     const location = useLocation();
 
+    let matchSilentRenewCallbackUrl= useRouteMatch({
+        path: '/silent-renew-callback',
+        exact: true,
+    });
+
     useEffect(() => {
-        userManagerPromise
+        initializeAuthentication(dispatch, matchSilentRenewCallbackUrl != null)
             .then(userManager => {
-                setUserManager({instance : userManager, error : null });
-                dispatchUser(dispatch, userManager);
+                setUserManager({instance: userManager, error: null});
             })
-            .catch(function(error) {
-                setUserManager({instance : null, error : error.message});
+            .catch(function (error) {
+                setUserManager({instance: null, error: error.message});
                 console.debug("error when importing the idp settings")
             });
     }, []);
@@ -129,6 +155,9 @@ const App = () => {
                             <Switch>
                                 <Route exact path="/sign-in-callback">
                                     <SignInCallback userManager={userManager} handleSigninCallback={() => handleSigninCallback(dispatch, history, userManager.instance)}/>
+                                </Route>
+                                <Route exact path="/silent-renew-callback">
+                                    <SilentRenewCallback userManager={userManager} handleSilentRenewCallback={() => handleSilentRenewCallback(userManager.instance)}/>
                                 </Route>
                                 <Route exact path="/logout-callback">
                                     <Redirect to="/" />
