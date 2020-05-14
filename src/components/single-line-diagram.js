3d492d3f3680a17c6918a19e502ce40986ee3894
Jon Harper <jon.harper87@gmail.com>
2020-05-14 15:55:40 +0200

    Restore svg viewbox for all reloadSvg calls, not just clicks on switches
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- src/components/single-line-diagram.js
+++ src/components/single-line-diagram.js
@@ -69,9 +69,16 @@ const SingleLineDiagram = forwardRef((props, ref)  => {
 
     const [svg, setSvg] = useState(noSvg);
     const svgPrevViewbox = useRef();
+    const svgDraw = useRef();
 
     const [forceState, updateState] = React.useState(false);
-    const forceUpdate = React.useCallback(() => updateState(s=>!s), []);
+    const forceUpdate = React.useCallback(() => {
+            if (svgDraw.current) {
+                svgPrevViewbox.current = svgDraw.current.viewbox();
+            }
+            updateState(s=>!s)
+    }, []);
+
 
     useImperativeHandle(ref, () => ({
         reloadSvg: forceUpdate
@@ -142,10 +149,10 @@ const SingleLineDiagram = forwardRef((props, ref)  => {
                     const switchMetadata = svg.metadata.nodes.find(value => value.id === clickedElementId);
                     const switchId = switchMetadata.equipmentId;
                     const open = switchMetadata.open;
-                    svgPrevViewbox.current = draw.viewbox();
                     props.onBreakerClick(switchId, !open);
                 });
             });
+            svgDraw.current = draw;
         }
     }, [svg]);
 
