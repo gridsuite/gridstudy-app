6c16aebaaf461bec36ccc0cbc17477a086047ac0
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-05-15 11:52:42 +0200

    404 Not Found
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -9,14 +9,27 @@ import React from 'react';
 
 import Container from "@material-ui/core/Container";
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import {makeStyles} from "@material-ui/core/styles";
+import {FormattedMessage, useIntl} from "react-intl";
 
+const useStyles = makeStyles(() => ({
+    error: {
+        fontSize: '64px',
+        width: '20%',
+        marginLeft: '40%',
+    },
+    container: {
+        marginTop:"70px",
+    },
+}));
 
 const PageNotFound = () => {
+    const classes = useStyles();
     return (
-        <Container >
+        <Container className={classes.container} >
             <br/>
-            <ErrorOutlineIcon/>
-            <h1 style={{textAlign:"center"}}>404 Not Found</h1>
+            <ErrorOutlineIcon className={classes.error}/>
+            <h1 style={{textAlign:"center"}}>404 <FormattedMessage id="PageNotFound"/></h1>
         </Container>
             );
 };
