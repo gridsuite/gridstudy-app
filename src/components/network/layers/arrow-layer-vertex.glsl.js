1f9b3862bf332bbbb8d00300726070d2e7f3edcb
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-05-15 17:04:41 +0200

    Remove trunc
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/components/network/layers/arrow-layer-vertex.glsl.js
+++ src/components/network/layers/arrow-layer-vertex.glsl.js
@@ -33,16 +33,12 @@ vec4 texelFetch(sampler2D sampler, ivec2 index, ivec2 size) {
   return texture2D(sampler, vec2(x, y));
 }
 
-float trunc(float v) {
-  return float(int(v));
-}
-
 /**
  * Calculate 2 dimensions texture index from flat index. 
  */
 ivec2 calulateTextureIndex(int flatIndex, ivec2 textureSize) {
   int x = int(mod(float(flatIndex), float(textureSize.x)));
-  int y = int(trunc(float(flatIndex) / float(textureSize.y)));
+  int y = int(float(flatIndex) / float(textureSize.y));
   return ivec2(x, y);
 }
 
