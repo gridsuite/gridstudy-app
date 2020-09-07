8c7e9f8e9e083304c933ad4353abf094c92b117b
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-09-07 15:19:28 +0200

    parent bd9dcc6f4ebd5ec31d23992be941678cd709479e author Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com> 1599484768 +0200 committer Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com> 1599646244 +0200
    Show loader when waiting to load geo data
    
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

---
+++ src/components/loader-with-overlay.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { CircularProgress } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { FormattedMessage } from 'react-intl';
+
+const useStyles = makeStyles((theme) => ({
+    overlay: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        textAlign: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: 'grey',
+        opacity: '0.8',
+        zIndex: 99,
+    },
+    message: {
+        position: 'absolute',
+        marginTop: '130px',
+    },
+}));
+
+const LoaderWithOverlay = ({
+    color,
+    loaderSize,
+    loadingMessageText,
+    loadingMessageSize,
+}) => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.overlay} id={color}>
+            <CircularProgress color={color} size={loaderSize} />
+            <div
+                className={classes.message}
+                style={{ fontSize: loadingMessageSize }}
+            >
+                <FormattedMessage id={loadingMessageText} />
+            </div>
+        </div>
+    );
+};
+
+export default LoaderWithOverlay;
