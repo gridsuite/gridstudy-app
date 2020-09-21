gridsuite/gridstudy-app

commit 48343062cf00a14eeedc05211566d818e742922e

Fix all the warnings! This actually fixes one bug (voltage level alphabetical order) (#197)
Signed-off-by: Jon Harper <jon.harper87@gmail.com>

src/components/study-pane.js
@@ -15,7 +15,6 @@ import { FormattedMessage } from 'react-intl';
 
 import { parse, stringify } from 'qs';
 
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
@@ -67,6 +66,55 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
     },
 }));
+
+const loadFlowButtonStyles = makeStyles({
+    root: {
+        backgroundColor: green[500],
+        '&:hover': {
+            backgroundColor: green[700],
+        },
+    },
+    label: {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+    },
+});
+
+const RunLoadFlowButton = (props) => {
+    const loadFlowButtonClasses = loadFlowButtonStyles();
+
+    const { loadFlowRunning, setLoadFlowRunning, studyName } = props;
+
+    useEffect(() => {
+        if (loadFlowRunning) {
+            startLoadFlow(studyName)
+                .then(() => {})
+                .then(() => {
+                    setLoadFlowRunning(false);
+                });
+        }
+        // Note: setLoadFlowRunning, studyName don't change
+    }, [loadFlowRunning, setLoadFlowRunning, studyName]);
+
+    const handleClick = () => setLoadFlowRunning(true);
+
+    return (
+        <Button
+            variant="contained"
+            fullWidth={true}
+            className={loadFlowButtonClasses.root}
+            startIcon={<PlayIcon />}
+            disabled={loadFlowRunning}
+            onClick={!loadFlowRunning ? handleClick : null}
+        >
+            <div className={loadFlowButtonClasses.label}>
+                <Typography noWrap>
+                    {loadFlowRunning ? 'LoadFlow running…' : 'Start LoadFlow'}
+                </Typography>
+            </div>
+        </Button>
+    );
+};
 
 const INITIAL_POSITION = [0, 0];
 
@@ -121,14 +169,77 @@ const StudyPane = () => {
 
     const websocketExpectedCloseRef = useRef();
 
-    // study creation, network and geo data loading: will be called only one time at creation mount event because
-    // studyName won't change
+    const loadNetwork = useCallback(() => {
+        console.info(`Loading network of study '${studyName}'...`);
+
+        const substations = fetchSubstations(studyName);
+
+        const lines = fetchLines(studyName);
+
+        Promise.all([substations, lines])
+            .then((values) => {
+                const network = new Network();
+                network.setSubstations(values[0]);
+                network.setLines(values[1]);
+                dispatch(loadNetworkSuccess(network));
+            })
+            .catch(function (error) {
+                console.error(error.message);
+                setStudyNotFound(true);
+            });
+        // Note: studyName and dispatch don't change
+    }, [studyName, dispatch]);
+
+    const loadGeoData = useCallback(() => {
+        console.info(`Loading geo data of study '${studyName}'...`);
+
+        const substationPositions = fetchSubstationPositions(studyName);
+
+        const linePositions = fetchLinePositions(studyName);
+
+        Promise.all([substationPositions, linePositions])
+            .then((values) => {
+                const geoData = new GeoData();
+                geoData.setSubstationPositions(values[0]);
+                geoData.setLinePositions(values[1]);
+                dispatch(loadGeoDataSuccess(geoData));
+                setWaitingLoadGeoData(false);
+            })
+            .catch(function (error) {
+                console.error(error.message);
+                setStudyNotFound(true);
+            });
+        // Note: studyName and dispatch don't change
+    }, [studyName, dispatch]);
+
+    const connectNotifications = useCallback(
+        (studyName) => {
+            console.info(`Connecting to notifications '${studyName}'...`);
+
+            const ws = connectNotificationsWebsocket(studyName);
+            ws.onmessage = function (event) {
+                dispatch(studyUpdated(JSON.parse(event.data)));
+            };
+            ws.onclose = function (event) {
+                if (!websocketExpectedCloseRef.current) {
+                    console.error('Unexpected Notification WebSocket closed');
+                }
+            };
+            ws.onerror = function (event) {
+                console.error('Unexpected Notification WebSocket error', event);
+            };
+            return ws;
+        },
+        // Note: dispatch doesn't change
+        [dispatch]
+    );
+
     useEffect(() => {
         websocketExpectedCloseRef.current = false;
         dispatch(openStudy(studyName));
 
-        loadNetwork(studyName);
-        loadGeoData(studyName);
+        loadNetwork();
+        loadGeoData();
         const ws = connectNotifications(studyName);
 
         // study cleanup at unmount event
@@ -137,7 +248,9 @@ const StudyPane = () => {
             ws.close();
             dispatch(closeStudy());
         };
-    }, [studyName]);
+        // Note: dispach, studyName, loadNetwork, loadGeoData,
+        // connectNotifications don't change
+    }, [dispatch, studyName, loadNetwork, loadGeoData, connectNotifications]);
 
     // set single line diagram voltage level id, contained in url query parameters
     useEffect(() => {
@@ -157,76 +270,21 @@ const StudyPane = () => {
         }
     }, [network]);
 
-    function loadNetwork(studyName) {
-        console.info(`Loading network of study '${studyName}'...`);
-
-        const substations = fetchSubstations(studyName);
-
-        const lines = fetchLines(studyName);
-
-        Promise.all([substations, lines])
-            .then((values) => {
-                const network = new Network();
-                network.setSubstations(values[0]);
-                network.setLines(values[1]);
-                dispatch(loadNetworkSuccess(network));
-            })
-            .catch(function (error) {
-                console.error(error.message);
-                setStudyNotFound(true);
-            });
-    }
-
-    function loadGeoData(studyName) {
-        console.info(`Loading geo data of study '${studyName}'...`);
-
-        const substationPositions = fetchSubstationPositions(studyName);
-
-        const linePositions = fetchLinePositions(studyName);
-
-        Promise.all([substationPositions, linePositions])
-            .then((values) => {
-                const geoData = new GeoData();
-                geoData.setSubstationPositions(values[0]);
-                geoData.setLinePositions(values[1]);
-                dispatch(loadGeoDataSuccess(geoData));
-                setWaitingLoadGeoData(false);
-            })
-            .catch(function (error) {
-                console.error(error.message);
-                setStudyNotFound(true);
-            });
-    }
-
-    function connectNotifications(studyName) {
-        console.info(`Connecting to notifications '${studyName}'...`);
-
-        const ws = connectNotificationsWebsocket(studyName);
-        ws.onmessage = function (event) {
-            dispatch(studyUpdated(JSON.parse(event.data)));
-        };
-        ws.onclose = function (event) {
-            if (!websocketExpectedCloseRef.current) {
-                console.error('Unexpected Notification WebSocket closed');
-            }
-        };
-        ws.onerror = function (event) {
-            console.error('Unexpected Notification WebSocket error', event);
-        };
-        return ws;
-    }
-
-    const showVoltageLevelDiagram = useCallback((voltageLevelId) => {
-        setUpdateSwitchMsg('');
-        history.replace(
-            '/studies/' +
-                encodeURIComponent(studyName) +
-                stringify(
-                    { voltageLevelId: voltageLevelId },
-                    { addQueryPrefix: true }
-                )
-        );
-    }, []);
+    const showVoltageLevelDiagram = useCallback(
+        (voltageLevelId) => {
+            setUpdateSwitchMsg('');
+            history.replace(
+                '/studies/' +
+                    encodeURIComponent(studyName) +
+                    stringify(
+                        { voltageLevelId: voltageLevelId },
+                        { addQueryPrefix: true }
+                    )
+            );
+        },
+        // Note: studyName and history don't change
+        [studyName, history]
+    );
 
     function closeVoltageLevelDiagram() {
         history.replace('/studies/' + encodeURIComponent(studyName));
@@ -262,20 +320,21 @@ const StudyPane = () => {
                 sldRef.current.reloadSvg();
             }
             if (
-                studyUpdatedForce.eventData.headers['updateType'] == 'loadflow'
+                studyUpdatedForce.eventData.headers['updateType'] === 'loadflow'
             ) {
                 //TODO reload data more intelligently
                 loadNetwork(studyName);
             }
         }
-    }, [studyUpdatedForce]);
+        // Note: studyName and loadNetwork don't change
+    }, [studyUpdatedForce, studyName, loadNetwork]);
 
     const updateFilteredNominalVoltages = (vnoms, isToggle) => {
         // filter on nominal voltage
         let newFiltered;
         if (isToggle) {
             newFiltered = [...filteredNominalVoltages];
-            vnoms.map((vnom) => {
+            vnoms.forEach((vnom) => {
                 const currentIndex = filteredNominalVoltages.indexOf(vnom);
                 if (currentIndex === -1) {
                     newFiltered.push(vnom);
@@ -288,54 +347,6 @@ const StudyPane = () => {
         }
         setFilteredNominalVoltages(newFiltered);
     };
-
-    const loadFlowButtonStyles = makeStyles({
-        root: {
-            backgroundColor: green[500],
-            '&:hover': {
-                backgroundColor: green[700],
-            },
-        },
-        label: {
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-        },
-    });
-
-    function RunLoadFlowButton() {
-        const loadFlowButtonClasses = loadFlowButtonStyles();
-
-        useEffect(() => {
-            if (loadFlowRunning) {
-                startLoadFlow(studyName)
-                    .then(() => {})
-                    .then(() => {
-                        setLoadFlowRunning(false);
-                    });
-            }
-        }, [loadFlowRunning]);
-
-        const handleClick = () => setLoadFlowRunning(true);
-
-        return (
-            <Button
-                variant="contained"
-                fullWidth={true}
-                className={loadFlowButtonClasses.root}
-                startIcon={<PlayIcon />}
-                disabled={loadFlowRunning}
-                onClick={!loadFlowRunning ? handleClick : null}
-            >
-                <div className={loadFlowButtonClasses.label}>
-                    <Typography noWrap>
-                        {loadFlowRunning
-                            ? 'LoadFlow running…'
-                            : 'Start LoadFlow'}
-                    </Typography>
-                </div>
-            </Button>
-        );
-    }
 
     const mapRef = useRef();
     const centerSubstation = useCallback(
@@ -359,8 +370,7 @@ const StudyPane = () => {
             />
         );
     } else {
-        let displayedVoltageLevel,
-            focusedVoltageLevel = null;
+        let displayedVoltageLevel;
         if (network) {
             if (displayedVoltageLevelId) {
                 displayedVoltageLevel = network.getVoltageLevel(
@@ -392,7 +402,14 @@ const StudyPane = () => {
                                                 marginTop: 8,
                                             }}
                                         >
-                                            <RunLoadFlowButton />
+                                            <RunLoadFlowButton
+                                                loadFlowRunning={
+                                                    loadFlowRunning
+                                                }
+                                                setLoadFlowRunning={
+                                                    setLoadFlowRunning
+                                                }
+                                            />
                                         </div>
                                     </Grid>
                                     <Grid item key="explorer">

src/components/app.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -79,15 +79,22 @@ const App = () => {
 
     const location = useLocation();
 
-    let matchSilentRenewCallbackUrl = useRouteMatch({
+    const matchSilentRenewCallbackUrl = useRouteMatch({
         path: '/silent-renew-callback',
         exact: true,
     });
 
+    // Get the routeMatch at page load, so we ignore the exhaustive deps check
+    const initialMatchSilentRenewCallbackUrl = useCallback(
+        () => matchSilentRenewCallbackUrl,
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )();
+
     useEffect(() => {
         initializeAuthenticationProd(
             dispatch,
-            matchSilentRenewCallbackUrl != null,
+            initialMatchSilentRenewCallbackUrl != null,
             fetch('idpSettings.json')
         )
             .then((userManager) => {
@@ -116,7 +123,8 @@ const App = () => {
                 setUserManager({ instance: null, error: error.message });
                 console.debug('error when importing the idp settings');
             });
-    }, []);
+        // Note: initialMatchSilentRenewCallbackUrl and dispatch don't change
+    }, [initialMatchSilentRenewCallbackUrl, dispatch]);
 
     function studyClickHandler(studyName) {
         history.push('/studies/' + encodeURIComponent(studyName));

src/components/create-study-form.js
@@ -64,8 +64,8 @@ const SelectCase = () => {
         fetchCases().then((cases) => {
             dispatch(loadCasesSuccess(cases));
         });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        // Note: dispatch doesn't change
+    }, [dispatch]);
 
     const handleChangeSelectCase = (event) => {
         dispatch(selectCase(event.target.value));

src/components/network/geo-data.js
@@ -203,8 +203,8 @@ export default class GeoData {
         let wantedDistance = lineDistance * arrowPosition;
 
         if (
-            Math.abs(lineParallelIndex) != 9999 &&
-            cumulativeDistances.length == 2
+            Math.abs(lineParallelIndex) !== 9999 &&
+            cumulativeDistances.length === 2
         ) {
             // For parallel lines, the initial fork line distance does not count
             // when there are no intermediate points between the substations.
@@ -232,6 +232,9 @@ export default class GeoData {
                 break;
             case ArrowDirection.NONE:
                 multiplier = 1;
+                break;
+            default:
+                throw new Error('impossible');
         }
         let remainingDistance = goodSegment.remainingDistance * multiplier;
 
@@ -250,14 +253,14 @@ export default class GeoData {
             angle: angle,
             offset: neededOffset,
         };
-        if (Math.abs(lineParallelIndex) != 9999) {
+        if (Math.abs(lineParallelIndex) !== 9999) {
             // apply parallel spread between lines
             position.position = computeDestinationPoint(
                 position.position,
                 distanceBetweenLines * lineParallelIndex,
                 lineAngle + 90
             );
-            if (cumulativeDistances.length == 2) {
+            if (cumulativeDistances.length === 2) {
                 // For line with only one segment, we can just apply a translation by lineAngle because both segment ends
                 // connect to fork lines. This accounts for the fact that the forkline part of the line doesn't count
                 position.position = computeDestinationPoint(
@@ -266,8 +269,8 @@ export default class GeoData {
                     lineAngle
                 );
             } else if (
-                goodSegment.idx == 0 ||
-                goodSegment.idx == cumulativeDistances.length - 2
+                goodSegment.idx === 0 ||
+                goodSegment.idx === cumulativeDistances.length - 2
             ) {
                 // When the label is on the first or last segment and there is an intermediate point,
                 // when must shift by the percentange of position of the label on this segment
@@ -276,7 +279,7 @@ export default class GeoData {
                     cumulativeDistances[goodSegment.idx];
                 const alreadyDoneDistance = segmentDistance - remainingDistance;
                 let labelDistanceInSegment;
-                if (goodSegment.idx == 0) {
+                if (goodSegment.idx === 0) {
                     labelDistanceInSegment = -alreadyDoneDistance;
                 } else {
                     labelDistanceInSegment = remainingDistance;
@@ -305,6 +308,9 @@ export default class GeoData {
                 break;
             case ArrowDirection.NONE:
                 direction = 0;
+                break;
+            default:
+                throw new Error('impossible');
         }
         //Y offset is negative because deckGL pixel uses a top-left coordinate system and our computation use orthogonal coordinates
         return [

src/components/network/layers/arrow-layer.js
@@ -128,6 +128,8 @@ export default class ArrowLayer extends Layer {
                             return 1.0;
                         case ArrowDirection.FROM_SIDE_2_TO_SIDE_1:
                             return 2.0;
+                        default:
+                            throw new Error('impossible');
                     }
                 },
                 defaultValue: 0.0,
@@ -381,8 +383,6 @@ export default class ArrowLayer extends Layer {
     }
 
     draw({ uniforms }) {
-        const { viewport } = this.context;
-
         const { sizeMinPixels, sizeMaxPixels } = this.props;
 
         const {

src/components/network/line-layer.js
@@ -51,13 +51,13 @@ function getArrowDirection(p) {
 }
 
 function getLineColor(line, color, props) {
-    if (props.lineFlowColorMode == LineFlowColorMode.NOMINAL_VOLTAGE) {
+    if (props.lineFlowColorMode === LineFlowColorMode.NOMINAL_VOLTAGE) {
         if (isDisconnected(line)) {
             return props.disconnectedLineColor;
         } else {
             return color;
         }
-    } else if (props.lineFlowColorMode == LineFlowColorMode.OVERLOADS) {
+    } else if (props.lineFlowColorMode === LineFlowColorMode.OVERLOADS) {
         let limits = [line.permanentLimit1, line.permanentLimit2];
         let intensities = [line.i1, line.i2];
         let iColor1, iColor2;
@@ -65,9 +65,9 @@ function getLineColor(line, color, props) {
 
         for (let i = 0; i < 2; ++i) {
             if (
-                limits[i] == undefined ||
-                intensities[i] == undefined ||
-                intensities[i] == 0
+                limits[i] === undefined ||
+                intensities[i] === undefined ||
+                intensities[i] === 0
             ) {
                 iColor[i] = 0;
             } else {
@@ -240,7 +240,7 @@ class LineLayer extends CompositeLayer {
             changeFlags.dataChanged ||
             (changeFlags.propsChanged &&
                 (oldProps.lineFullPath !== props.lineFullPath ||
-                    props.lineParallelPath != oldProps.lineParallelPath))
+                    props.lineParallelPath !== oldProps.lineParallelPath))
         ) {
             //add labels
             compositeData.forEach((compositeData) => {
@@ -296,9 +296,9 @@ class LineLayer extends CompositeLayer {
                 (oldProps.lineFullPath !== props.lineFullPath ||
                     //For lineFlowMode, recompute only if mode goes to or from LineFlowMode.FEEDERS
                     //because for LineFlowMode.STATIC_ARROWS and LineFlowMode.ANIMATED_ARROWS it's the same
-                    (props.lineFlowMode != oldProps.lineFlowMode &&
-                        (props.lineFlowMode == LineFlowMode.FEEDERS ||
-                            oldProps.lineFlowMode == LineFlowMode.FEEDERS))))
+                    (props.lineFlowMode !== oldProps.lineFlowMode &&
+                        (props.lineFlowMode === LineFlowMode.FEEDERS ||
+                            oldProps.lineFlowMode === LineFlowMode.FEEDERS))))
         ) {
             // add arrows
             compositeData.forEach((compositeData) => {

src/components/network/network-explorer.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { useSelector } from 'react-redux';
@@ -59,18 +59,19 @@ const NetworkExplorer = ({
     const [filteredVoltageLevels, setFilteredVoltageLevels] = React.useState(
         []
     );
-    const [currentVoltageLevel, setCurrentVoltageLevel] = React.useState(null);
-
     const buttonRef = React.useRef();
     const [voltageLevelMenuIndex, setVoltageLevelMenuIndex] = React.useState(
         -1
     );
 
-    const voltageLevelComparator = (vl1, vl2) => {
-        return useName
-            ? vl1.name.localeCompare(vl2.name)
-            : vl1.id.localeCompare(vl2.id);
-    };
+    const voltageLevelComparator = useCallback(
+        (vl1, vl2) => {
+            return useName
+                ? vl1.name.localeCompare(vl2.name)
+                : vl1.id.localeCompare(vl2.id);
+        },
+        [useName]
+    );
 
     useEffect(() => {
         if (network) {
@@ -78,17 +79,7 @@ const NetworkExplorer = ({
                 network.getVoltageLevels().sort(voltageLevelComparator)
             );
         }
-    }, [network]);
-
-    useEffect(() => {
-        if (currentVoltageLevel !== null) {
-            onVoltageLevelDisplayClick(
-                currentVoltageLevel.id,
-                currentVoltageLevel.name
-            );
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [useName]);
+    }, [network, voltageLevelComparator]);
 
     function handleClick(index) {
         setVoltageLevelMenuIndex(index);
@@ -102,7 +93,6 @@ const NetworkExplorer = ({
         if (onVoltageLevelDisplayClick !== null) {
             const vl = filteredVoltageLevels[index];
             onVoltageLevelDisplayClick(vl.id);
-            setCurrentVoltageLevel(vl);
         }
         handleClose();
     }

src/components/network/network-map.js
@@ -7,7 +7,6 @@
 
 import React, {
     forwardRef,
-    useEffect,
     useImperativeHandle,
     useMemo,
     useRef,

commit c6fa25e77b57363a9173a130eff4d6bb49779c10

Fix the loadflow button (regression from 48343062cf0 fix warnings)
Signed-off-by: Jon Harper <jon.harper87@gmail.com>

src/components/study-pane.js
@@ -403,6 +403,7 @@ const StudyPane = () => {
                                             }}
                                         >
                                             <RunLoadFlowButton
+                                                studyName={studyName}
                                                 loadFlowRunning={
                                                     loadFlowRunning
                                                 }