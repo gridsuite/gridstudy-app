c82f3dc8f7bba4919a5b5c7b1ed1d34f5d586093
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-10-11 23:38:48 +0200

    first commit
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
+        createProxyMiddleware('http://localhost:8070/api/apps-metadata-service', {
+            pathRewrite: { '^/api/apps-metadata-service/': '/' },
+        })
+    );
+    app.use(
         createProxyMiddleware('http://localhost:5001/api/study-server', {
             pathRewrite: { '^/api/study-server/': '/' },
         })
