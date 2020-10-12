60af0995ec85fedeaa1ded864a036888162ab8af
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-10-12 11:33:38 +0200

    rename apps-metadata-service
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('http://localhost:8070/api/apps-metadata-service', {
-            pathRewrite: { '^/api/apps-metadata-service/': '/' },
+        createProxyMiddleware('http://localhost:8070/api/apps-metadata-server', {
+            pathRewrite: { '^/api/apps-metadata-server/': '/' },
         })
     );
     app.use(
