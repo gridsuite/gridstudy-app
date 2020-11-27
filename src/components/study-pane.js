gridsuite/gridstudy-app

commit 9257e44f5729ed1aabb960f34c0d52b4997dfe68

Add overlay and loader for the inprogress study (#185)
Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

src/components/study-pane.js
@@ -550,6 +550,15 @@ const StudyPane = (props) => {
 
         return (
             <div>
+                {waitingLoadGeoData && (
+                    <LoaderWithOverlay
+                        color="inherit"
+                        loaderSize={70}
+                        loadingMessageText="loadingGeoData"
+                        loadingMessageSize={25}
+                        loaderMessageSpace={135}
+                    />
+                )}
                 <Grid container direction="row" className={classes.main}>
                     <Grid item xs={12} md={2}>
                         <AutoSizer>

src/components/create-study-form.js
@@ -26,18 +26,12 @@ import FormControl from '@material-ui/core/FormControl';
 import Alert from '@material-ui/lab/Alert';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import {
-    createStudy,
-    fetchCases,
-    fetchStudies,
-    studyExists,
-} from '../utils/rest-api';
+import { createStudy, fetchCases, studyExists } from '../utils/rest-api';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import { useDispatch, useSelector } from 'react-redux';
 import {
     loadCasesSuccess,
-    loadStudiesSuccess,
     removeSelectedFile,
     selectCase,
     selectFile,
@@ -166,7 +160,6 @@ export const CreateStudyForm = () => {
     const [studyPrivacy, setStudyPrivacy] = React.useState('private');
     const [createStudyErr, setCreateStudyErr] = React.useState('');
 
-    const [loading, setLoading] = React.useState(false);
     const [studyInvalid, setStudyInvalid] = useState(false);
     const [loadingCheckStudyName, setLoadingCheckStudyName] = React.useState(
         false
@@ -280,7 +273,7 @@ export const CreateStudyForm = () => {
             isPrivateStudy = true;
         }
 
-        setLoading(true);
+        setOpen(false);
         createStudy(
             caseExist,
             studyName,
@@ -289,17 +282,12 @@ export const CreateStudyForm = () => {
             selectedFile,
             isPrivateStudy
         ).then((res) => {
-            if (res.ok) {
-                setCreateStudyErr('');
-                setStudyName('');
-                setStudyDescription('');
-                dispatch(removeSelectedFile());
-                setLoading(false);
-                setOpen(false);
-                fetchStudies().then((studies) => {
-                    dispatch(loadStudiesSuccess(studies));
-                });
-            } else {
+            setCreateStudyErr('');
+            setStudyName('');
+            setStudyDescription('');
+            dispatch(removeSelectedFile());
+
+            if (!res.ok) {
                 console.debug('Error when creating the study');
                 if (res.status === 409) {
                     setCreateStudyErr(
@@ -314,7 +302,6 @@ export const CreateStudyForm = () => {
                             setCreateStudyErr(error);
                         });
                 }
-                setLoading(false);
             }
         });
     };
@@ -426,16 +413,6 @@ export const CreateStudyForm = () => {
                     {!caseExist && <UploadCase />}
                     {createStudyErr !== '' && (
                         <Alert severity="error">{createStudyErr}</Alert>
-                    )}
-                    {loading && (
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <CircularProgress className={classes.progress} />
-                        </div>
                     )}
                 </DialogContent>
                 <DialogActions>

src/components/loader-with-overlay.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     },
     message: {
         position: 'absolute',
-        marginTop: '130px',
+        lineHeight: 1.2,
     },
 }));
 
@@ -35,7 +35,9 @@ const LoaderWithOverlay = ({
             <CircularProgress color={color} size={loaderSize} />
             <div
                 className={classes.message}
-                style={{ fontSize: loadingMessageSize }}
+                style={{
+                    fontSize: loadingMessageSize,
+                }}
             >
                 <FormattedMessage id={loadingMessageText} />
             </div>

src/components/study-manager.js
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -24,8 +24,19 @@ import { ReactComponent as EntsoeLogo } from '../images/entsoe_logo.svg';
 import { ReactComponent as UcteLogo } from '../images/ucte_logo.svg';
 import { ReactComponent as IeeeLogo } from '../images/ieee_logo.svg';
 
-import { loadStudiesSuccess } from '../redux/actions';
-import { deleteStudy, fetchStudies, renameStudy } from '../utils/rest-api';
+import {
+    loadStudiesSuccess,
+    loadStudyCreationRequestsSuccess,
+} from '../redux/actions';
+
+import {
+    deleteStudy,
+    fetchStudies,
+    renameStudy,
+    fetchStudyCreationRequests,
+    connectNotificationsWsUpdateStudies,
+} from '../utils/rest-api';
+
 import { CardHeader } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -44,6 +55,7 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import CreateStudyForm from './create-study-form';
+import LoaderWithOverlay from './loader-with-overlay';
 
 const useStyles = makeStyles((theme) => ({
     card: {
@@ -86,6 +98,9 @@ const useStyles = makeStyles((theme) => ({
     tooltip: {
         fontSize: 18,
     },
+    container: {
+        position: 'relative',
+    },
 }));
 
 const DonwnloadIframe = 'downloadIframe';
@@ -97,9 +112,9 @@ const DonwnloadIframe = 'downloadIframe';
  * @param {String} study.description Description of the study
  * @param {Date} study.caseDate Date of the study
  * @param {EventListener} onClick Event to open the study
+ * @param inprogressLoader
  */
-const StudyCard = ({ study, onClick }) => {
-    const dispatch = useDispatch();
+const StudyCard = ({ study, onClick, studyCreationLoader }) => {
     const classes = useStyles();
     const intl = useIntl();
 
@@ -162,13 +177,9 @@ const StudyCard = ({ study, onClick }) => {
 
     const handleClickDelete = () => {
         deleteStudy(study.studyName, study.userId).then((response) => {
-            response.ok
-                ? fetchStudies().then((studies) => {
-                      dispatch(loadStudiesSuccess(studies));
-                  })
-                : setDeleteError(
-                      intl.formatMessage({ id: 'deleteStudyError' })
-                  );
+            if (!response.ok) {
+                setDeleteError(intl.formatMessage({ id: 'deleteStudyError' }));
+            }
         });
     };
 
@@ -190,9 +201,6 @@ const StudyCard = ({ study, onClick }) => {
     const handleClickRename = (newStudyNameValue) => {
         renameStudy(study.studyName, study.userId, newStudyNameValue).then(
             () => {
-                fetchStudies().then((studies) => {
-                    dispatch(loadStudiesSuccess(studies));
-                });
                 setOpenRename(false);
             }
         );
@@ -231,8 +239,16 @@ const StudyCard = ({ study, onClick }) => {
     };
 
     return (
-        <div>
+        <div className={classes.container}>
             <Card className={classes.root}>
+                {studyCreationLoader && (
+                    <LoaderWithOverlay
+                        color="inherit"
+                        loaderSize={35}
+                        loadingMessageText="loadingCreationStudy"
+                        loadingMessageSize={15}
+                    />
+                )}
                 <CardActionArea
                     onClick={() => onClick()}
                     className={classes.card}
@@ -246,7 +262,13 @@ const StudyCard = ({ study, onClick }) => {
                         classes={classes}
                     >
                         <CardHeader
-                            avatar={logo(study.caseFormat)}
+                            avatar={
+                                studyCreationLoader ? (
+                                    <canvas className={classes.logo} />
+                                ) : (
+                                    logo(study.caseFormat)
+                                )
+                            }
                             title={
                                 <div className={classes.cardTitle}>
                                     <Typography noWrap variant="h5">
@@ -378,17 +400,53 @@ StudyCard.propTypes = {
  */
 const StudyManager = ({ onClick }) => {
     const dispatch = useDispatch();
+    const websocketExpectedCloseRef = useRef();
 
-    useEffect(() => {
+    const studies = useSelector((state) => state.studies);
+    const studyCreationRequests = useSelector(
+        (state) => state.temporaryStudies
+    );
+
+    const classes = useStyles();
+
+    const dispatchStudies = useCallback(() => {
+        fetchStudyCreationRequests().then((studies) => {
+            dispatch(loadStudyCreationRequestsSuccess(studies));
+        });
         fetchStudies().then((studies) => {
             dispatch(loadStudiesSuccess(studies));
         });
         // Note: dispatch doesn't change
     }, [dispatch]);
 
-    const studies = useSelector((state) => state.studies);
+    const connectNotificationsUpdateStudies = useCallback(() => {
+        const ws = connectNotificationsWsUpdateStudies();
 
-    const classes = useStyles();
+        ws.onmessage = function (event) {
+            dispatchStudies();
+        };
+        ws.onclose = function (event) {
+            if (!websocketExpectedCloseRef.current) {
+                console.error('Unexpected Notification WebSocket closed');
+            }
+        };
+        ws.onerror = function (event) {
+            console.error('Unexpected Notification WebSocket error', event);
+        };
+        return ws;
+    }, [dispatchStudies]);
+
+    useEffect(() => {
+        dispatchStudies();
+
+        const ws = connectNotificationsUpdateStudies();
+        // Note: dispatch doesn't change
+
+        // cleanup at unmount event
+        return function () {
+            ws.close();
+        };
+    }, [connectNotificationsUpdateStudies, dispatchStudies]);
 
     return (
         <Container maxWidth="lg" className={classes.cardContainer}>
@@ -398,6 +456,16 @@ const StudyManager = ({ onClick }) => {
                         <CreateStudyForm />
                     </Box>
                 </Grid>
+                {studyCreationRequests &&
+                    studyCreationRequests.map((study) => (
+                        <Grid item xs={12} sm={6} md={3}>
+                            <StudyCard
+                                studyCreationLoader={true}
+                                study={study}
+                                onClick={() => onClick(study.studyName)}
+                            />
+                        </Grid>
+                    ))}
                 {studies.map((study) => (
                     <Grid
                         item
@@ -407,6 +475,7 @@ const StudyManager = ({ onClick }) => {
                         key={study.userId + '/' + study.studyName}
                     >
                         <StudyCard
+                            studyCreationLoader={false}
                             study={study}
                             onClick={() =>
                                 onClick(study.studyName, study.userId)

src/redux/actions.js
@@ -31,6 +31,12 @@ export function loadStudiesSuccess(studies) {
     return { type: LOAD_STUDIES_SUCCESS, studies: studies };
 }
 
+export const LOAD_TEMPORARY_STUDIES = 'LOAD_TEMPORARY_STUDIES';
+
+export function loadStudyCreationRequestsSuccess(temporaryStudies) {
+    return { type: LOAD_TEMPORARY_STUDIES, temporaryStudies: temporaryStudies };
+}
+
 export const LOAD_CASES_SUCCESS = 'LOAD_CASES_SUCCESS';
 
 export function loadCasesSuccess(cases) {

src/redux/reducer.js
@@ -43,6 +43,7 @@ import {
     LOAD_GEO_DATA_SUCCESS,
     LOAD_NETWORK_SUCCESS,
     LOAD_STUDIES_SUCCESS,
+    LOAD_TEMPORARY_STUDIES,
     OPEN_STUDY,
     REMOVE_SELECTED_CASE,
     REMOVE_SELECTED_FILE,
@@ -86,6 +87,10 @@ const initialState = {
 export const reducer = createReducer(initialState, {
     [LOAD_STUDIES_SUCCESS]: (state, action) => {
         state.studies = action.studies;
+    },
+
+    [LOAD_TEMPORARY_STUDIES]: (state, action) => {
+        state.temporaryStudies = action.temporaryStudies;
     },
 
     [LOAD_CASES_SUCCESS]: (state, action) => {

src/utils/rest-api.js
@@ -38,8 +38,16 @@ function backendFetch(url, init) {
 export function fetchStudies() {
     console.info('Fetching studies...');
     const fetchStudiesUrl = PREFIX_STUDY_QUERIES + '/v1/studies';
-    console.debug(fetchStudiesUrl);
     return backendFetch(fetchStudiesUrl).then((response) => response.json());
+}
+
+export function fetchStudyCreationRequests() {
+    console.info('Fetching study creation requests...');
+    const creationRequestsUrl =
+        PREFIX_STUDY_QUERIES + '/v1/study_creation_requests';
+    return backendFetch(creationRequestsUrl).then((response) =>
+        response.json()
+    );
 }
 
 export function fetchStudy(studyName, userId) {
@@ -417,6 +425,33 @@ export function connectNotificationsWebsocket(studyName) {
         console.info('Connected Websocket ' + wsadress + ' ...');
     };
     return rws;
+}
+
+/**
+ * Function will be called to connect with notification websocket to update the studies list
+ * @returns {ReconnectingWebSocket}
+ */
+export function connectNotificationsWsUpdateStudies() {
+    const webSocketBaseUrl = document.baseURI
+        .replace(/^http:\/\//, 'ws://')
+        .replace(/^https:\/\//, 'wss://');
+    const webSocketUrl =
+        webSocketBaseUrl +
+        PREFIX_NOTIFICATION_WS +
+        '/notify?updateType=studies';
+
+    let webSocketUrlWithToken;
+    webSocketUrlWithToken = webSocketUrl + '&access_token=' + getToken();
+
+    const reconnectingWebSocket = new ReconnectingWebSocket(
+        webSocketUrlWithToken
+    );
+    reconnectingWebSocket.onopen = function (event) {
+        console.info(
+            'Connected Websocket update studies' + webSocketUrl + ' ...'
+        );
+    };
+    return reconnectingWebSocket;
 }
 
 export function getAvailableExportFormats() {

commit c9098a2f0a2d6805cf920e7e6b201b2420e24bce

Remove spurious duplicate LoaderWithOverlay
regression from bad merge in 9257e44f572 reintrocing the overlay,
because it was moved in 83aa3a931a33de8

Signed-off-by: Jon Harper <jon.harper87@gmail.com>

src/components/study-pane.js
@@ -669,14 +669,6 @@ const StudyPane = (props) => {
 
         return (
             <div className={classes.main}>
-                {waitingLoadGeoData && (
-                    <LoaderWithOverlay
-                        color="inherit"
-                        loaderSize={70}
-                        isFixed={true}
-                        loadingMessageText="loadingGeoData"
-                    />
-                )}
                 <Drawer
                     variant={'persistent'}
                     className={classes.drawer}