fc3ab4b5cb85fbbb3bc82f5e36f63c096c66d435
jacqbors <jacques.borsenberger@rte-france.com>
2020-12-22 13:48:46 +0100

    add sorting to security analysis table
    Signed-off-by: jacqbors <jacques.borsenberger@rte-france.com>

--- src/components/security-analysis-result.js
+++ src/components/security-analysis-result.js
@@ -14,6 +14,7 @@ import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
+import { isNumeric } from 'rxjs/internal-compatibility';
 
 export const NMK_TYPE_RESULT = {
     CONSTRAINTS_FROM_CONTINGENCIES: 'constraints-from-contingencies',
@@ -135,6 +136,8 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
                     violationCount:
                         postContingencyResult.limitViolationsResult
                             .limitViolations.length,
+                    _group: index,
+                    _root: true,
                 });
                 postContingencyResult.limitViolationsResult.limitViolations.forEach(
                     (limitViolation) => {
@@ -148,12 +151,80 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
                             value: limitViolation.value,
                             loading: computeLoading(limitViolation),
                             side: limitViolation.side,
+                            _group: index,
                         });
                     }
                 );
             }
         });
         return rows;
+    }
+
+    /**
+     * sortResult : generate an array of index representing the rows sorted by key
+     * rows are grouped by their attribute _group, the first one is root, the other children, we assume that the rows
+     * are already grouped (next to each other)
+     *
+     * rows : rows to sort
+     * rootSet : Set of keys of the root row (if key is in root, we sort root lines, not the children inside
+     *           else we sort children for each root (and do not change order of root)
+     * key : sort key
+     * reverse : ascending or descending sort
+     * isNumeric : is the associated column numeric
+     * */
+    function sortResult(rows, rootSet, key, reverse, isNumeric) {
+        /* utility functions */
+        function sortAndAddResults(result, array) {
+            const compareValue = (a, b) => {
+                const mult = reverse ? 1 : -1;
+                if (a === undefined && b === undefined) return 0;
+                else if (b === undefined) return -mult;
+                else if (a === undefined) return mult;
+                return isNumeric
+                    ? (Number(a) < Number(b) ? 1 : -1) * mult
+                    : ('' + a).localeCompare(b) * mult;
+            };
+
+            const getIndexes = (k) => [k.index].concat(k.indexes);
+            array
+                .sort((a, b) => compareValue(a.key, b.key))
+                .flatMap((k) => getIndexes(k))
+                .map((i) => result.push(i));
+        }
+
+        let currentSorting = [];
+        const addRowToSort = (key, index) => {
+            currentSorting.push({
+                key: key,
+                index: index,
+                indexes: [],
+            });
+        };
+
+        const rootSorting = rootSet.has(key);
+        let group = undefined;
+        let result = [];
+        /* now we sort */
+        rows.forEach((row, index) => {
+            if (group !== row._group) {
+                /* new set of lines */
+                if (!rootSorting) {
+                    sortAndAddResults(result, currentSorting); // add previous batch
+                    currentSorting = [];
+                    result.push(index); // add current row (we do not sort root)
+                } else {
+                    addRowToSort(row[key], index); // we sort root
+                }
+                group = row._group;
+            } else if (rootSorting) {
+                currentSorting[currentSorting.length - 1].indexes.push(index); // we don't want to loose children
+            } else {
+                addRowToSort(row[key], index); // children need sorting
+            }
+        });
+        /* add last group (if any) or all if root sorting */
+        sortAndAddResults(result, currentSorting);
+        return result;
     }
 
     function renderTableNmKContingencies(postContingencyResults) {
@@ -163,6 +234,15 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
                 rowCount={rows.length}
                 rowGetter={({ index }) => rows[index]}
                 onCellClick={onClickNmKConstraint}
+                sort={(dataKey, reverse, isNumeric) =>
+                    sortResult(
+                        rows,
+                        new Set(['contingencyId', 'computationOk']),
+                        dataKey,
+                        reverse,
+                        isNumeric
+                    )
+                }
                 columns={[
                     {
                         width: 200,
@@ -251,9 +331,12 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
             }
         });
 
+        let group = 0;
         mapConstraints.forEach((contingencies, subjectId) => {
             rows.push({
                 subjectId: subjectId,
+                _group: group,
+                _root: true,
             });
 
             contingencies.forEach((contingency) => {
@@ -265,8 +348,10 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
                     value: contingency.value,
                     loading: contingency.loading,
                     side: contingency.side,
+                    _group: group,
                 });
             });
+            group++;
         });
 
         return rows;
@@ -280,6 +365,15 @@ const SecurityAnalysisResult = ({ result, onClickNmKConstraint }) => {
                 rowCount={rows.length}
                 rowGetter={({ index }) => rows[index]}
                 onCellClick={onClickNmKConstraint}
+                sort={(dataKey, reverse, isNumeric) =>
+                    sortResult(
+                        rows,
+                        new Set(['subjectId']),
+                        dataKey,
+                        reverse,
+                        isNumeric
+                    )
+                }
                 columns={[
                     {
                         width: 200,
