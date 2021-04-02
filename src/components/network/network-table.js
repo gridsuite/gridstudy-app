847a9502daad6ed60704c7bf5df6ce23e17e2b02
Slimane Amar <63394973+SlimaneAmar@users.noreply.github.com>
2021-04-02 08:37:00 +0200

    Get only the parameters belonging to the app (#318) (#318)
    Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

--- src/components/network/network-table.js
+++ src/components/network/network-table.js
@@ -34,13 +34,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
-
 import {
     COLUMNS_PARAMETER_PREFIX_IN_DATABASE,
     TABLES_COLUMNS_NAMES,
     TABLES_DEFINITIONS,
     TABLES_NAMES,
-} from './constants';
+} from './config-tables';
 
 const useStyles = makeStyles((theme) => ({
     cell: {
