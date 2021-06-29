1564772826fb5cf9f0d6cda0578843de833d5672
AbdelHedhili <abdelsalem.hedhili@rte-france.com>
2021-06-29 09:37:40 +0200

    fix result badges
    Signed-off-by: AbdelHedhili <abdelsalem.hedhili@rte-france.com>

--- src/components/study-pane.js
+++ src/components/study-pane.js
@@ -35,12 +35,14 @@ import {
     updateSwitchState,
 } from '../utils/rest-api';
 import {
+    addLoadflowNotif,
+    addSANotif,
     closeStudy,
     filteredNominalVoltagesUpdated,
-    increaseResultCount,
     loadGeoDataSuccess,
     networkCreated,
     openStudy,
+    resetLoadflowNotif,
     selectItemNetwork,
     studyUpdated,
 } from '../redux/actions';
@@ -229,6 +231,10 @@ const StudyPane = (props) => {
 
     const [loadFlowResult, setLoadFlowResult] = useState(null);
 
+    const [ranLoadflow, setRanLoadflow] = useState(false);
+
+    const [ranSA, setRanSA] = useState(false);
+
     const [securityAnalysisStatus, setSecurityAnalysisStatus] = useState(
         RunningStatus.IDLE
     );
@@ -361,8 +367,10 @@ const StudyPane = (props) => {
     const startComputation = (runnable) => {
         if (runnable === Runnable.LOADFLOW) {
             startLoadFlow(studyUuid);
+            setRanLoadflow(true);
         } else if (runnable === Runnable.SECURITY_ANALYSIS) {
             setShowContingencyListSelector(true);
+            setRanSA(true);
         }
     };
 
@@ -682,11 +690,17 @@ const StudyPane = (props) => {
                 //TODO reload data more intelligently
                 updateSld();
                 loadNetwork(true);
+                //add the loadflow notif (only if the user clicked on run loadflow)
+                if (ranLoadflow) {
+                    dispatch(addLoadflowNotif());
+                }
             } else if (
                 studyUpdatedForce.eventData.headers['updateType'] ===
                 'loadflow_status'
             ) {
                 updateLoadFlowResult();
+                //we reset the notif when the previous loadflow results are removed
+                dispatch(resetLoadflowNotif());
             } else if (
                 studyUpdatedForce.eventData.headers['updateType'] ===
                 'securityAnalysis_status'
@@ -697,9 +711,11 @@ const StudyPane = (props) => {
                 'securityAnalysisResult'
             ) {
                 updateSecurityAnalysisResult();
-
+                //add the SA notif (only if the user clicked on run SA)
+                if (ranSA) {
+                    dispatch(addSANotif());
+                }
                 // update badge
-                dispatch(increaseResultCount());
             }
         }
         // Note: studyUuid, and loadNetwork don't change
@@ -710,6 +726,8 @@ const StudyPane = (props) => {
         updateLoadFlowResult,
         updateSecurityAnalysisStatus,
         updateSecurityAnalysisResult,
+        ranLoadflow,
+        ranSA,
         dispatch,
     ]);
 
