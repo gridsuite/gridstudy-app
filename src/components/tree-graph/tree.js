7224cddae8ca3837e8dde4549ca0c580e28dd867
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2021-07-05 15:24:47 +0200

    Modified graph layout
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- src/components/tree-graph/tree.js
+++ src/components/tree-graph/tree.js
@@ -12,31 +12,51 @@ const Tree = ({data}) => {
                 container: ReactDOM.findDOMNode(ref.current),
                 width: 1200,
                 height: 800,
+                fitView: true,
+                linkCenter: true,
                 modes: {
-                    default: ['drag-canvas', 'zoom-canvas'],
-                    edit: ['click-select'],
+                    default: [
+                        {
+                            type: 'collapse-expand',
+                            onChange: function onChange(item, collapsed) {
+                                const data = item.getModel();
+                                data.collapsed = collapsed;
+                                return true;
+                            },
+                        },
+                        'drag-canvas',
+                        'zoom-canvas',
+                    ],
+                },
+                defaultNode: {
+                    type: 'rect',
+                    size: [100,20],
+                    anchorPoints: [
+                        [0, 0],
+                        [1, 1],
+                    ],
+                },
+                defaultEdge: {
+                    type: 'cubic-vertical',
                 },
                 layout: {
                     type: 'compactBox',
-                    direction: 'TB', // H / V / LR / RL / TB / BT
-                    nodeSep: 50,
-                    rankSep: 100,
-                },
-                defaultNode: {
-                    type: 'node',
-                    labelCfg: {
-                        style: {
-                            fill: '#000000A6',
-                            fontSize: 10,
-                        },
+                    direction: 'TB',
+                    getId: function getId(d) {
+                        return d.id;
                     },
-                    style: {
-                        stroke: '#72CC4A',
-                        width: 150,
+                    getHeight: function getHeight() {
+                        return 16;
                     },
-                },
-                defaultEdge: {
-                    type: 'polyline',
+                    getWidth: function getWidth() {
+                        return 16;
+                    },
+                    getVGap: function getVGap() {
+                        return 30;
+                    },
+                    getHGap: function getHGap() {
+                        return 50;
+                    },
                 },
             });
         }
