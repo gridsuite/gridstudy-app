6fcd926e7c0e0f5143e049229a294f5ec351bafa
jacques borsenberger <jacques.borsenberger@rte-france.com>
2021-07-16 13:18:42 +0200

    revert to controled forms
    Signed-off-by: jacques borsenberger <jacques.borsenberger@rte-france.com>

--- src/components/util/checkbox-list.js
+++ src/components/util/checkbox-list.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -12,16 +12,22 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const CheckboxList = (props) => {
-    const checked = useRef(new Set());
+    const [checked, setChecked] = useState([]);
 
     const handleToggle = (value) => () => {
-        if (!checked.current.delete(value)) {
-            // element was not present
-            checked.current.add(value);
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
         }
 
+        setChecked(newChecked);
+
         if (props.onChecked) {
-            props.onChecked([...checked.current]);
+            props.onChecked(newChecked);
         }
     };
 
@@ -40,7 +46,7 @@ const CheckboxList = (props) => {
                             <Checkbox
                                 color={'primary'}
                                 edge="start"
-                                defaultValue={false}
+                                checked={checked.indexOf(props.id(item)) !== -1}
                                 tabIndex={-1}
                                 disableRipple
                             />
