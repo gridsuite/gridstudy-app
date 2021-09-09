650f55bc8a9900549d9c734c24e1f716c942b783
Franck LECUYER <franck.lecuyer@rte-france.com>
2021-09-09 17:17:22 +0200

    Add newly created equipments when updating the network
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -110,11 +110,21 @@ export default class Network {
     }
 
     updateEquipments(currentEquipments, newEquipements) {
+        // replace current modified equipments
         currentEquipments.forEach((equipment1, index) => {
             const found = newEquipements.filter(
                 (equipment2) => equipment2.id === equipment1.id
             );
             currentEquipments[index] = found.length > 0 ? found[0] : equipment1;
+        });
+        // add newly created equipments
+        newEquipements.forEach((equipment1) => {
+            const found = currentEquipments.find(
+                (equipment2) => equipment2.id === equipment1.id
+            );
+            if (found === undefined) {
+                currentEquipments.push(equipment1);
+            }
         });
     }
 
