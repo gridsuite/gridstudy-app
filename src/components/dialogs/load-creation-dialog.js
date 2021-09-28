afa1532655232a02ab7cb3f9bf46f5e8a16aefb8
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2021-09-28 16:07:42 +0200

    Prettyfied code
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- src/components/dialogs/load-creation-dialog.js
+++ src/components/dialogs/load-creation-dialog.js
@@ -13,13 +13,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
-import {
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
-    Typography,
-} from '@material-ui/core';
+import { InputLabel, MenuItem, Select, TextField } from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
@@ -63,8 +57,7 @@ const LoadCreationDialog = ({ open, onClose, title }) => {
 
         if (loadId === '') {
             tmpErrors['load-id'] = true;
-        }
-        else {
+        } else {
             handleClose();
         }
 
