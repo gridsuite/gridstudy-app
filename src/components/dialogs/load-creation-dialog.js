1883ae3f79d070fd339d407210f7ff1731e147d6
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2021-09-28 16:06:26 +0200

    Added sections labels Corrected label display in Select components
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- src/components/dialogs/load-creation-dialog.js
+++ src/components/dialogs/load-creation-dialog.js
@@ -13,7 +13,15 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
-import { MenuItem, Select, TextField } from '@material-ui/core';
+import {
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    Typography,
+} from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 /**
  * Dialog to create a load in the network
@@ -55,7 +63,8 @@ const LoadCreationDialog = ({ open, onClose, title }) => {
 
         if (loadId === '') {
             tmpErrors['load-id'] = true;
-        } else {
+        }
+        else {
             handleClose();
         }
 
@@ -108,83 +117,112 @@ const LoadCreationDialog = ({ open, onClose, title }) => {
                         />
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <Select
-                            id="load-type"
-                            value={loadType}
-                            onChange={handleChangeLoadType}
-                            label={intl.formatMessage({ id: 'TypeOptional' })}
-                            variant="filled"
-                            fullWidth
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={'UNDEFINED'}>
-                                {intl.formatMessage({
-                                    id: 'UndefinedDefaultValue',
-                                })}
-                            </MenuItem>
-                            <MenuItem value={'AUXILIARY'}>
-                                {intl.formatMessage({ id: 'Auxiliary' })}
-                            </MenuItem>
-                            <MenuItem value={'FICTITIOUS'}>
-                                {intl.formatMessage({ id: 'Fictitious' })}
-                            </MenuItem>
-                        </Select>
+                        <FormControl fullWidth>
+                            <InputLabel id="load-type-label" margin={'dense'}>
+                                {intl.formatMessage({ id: 'TypeOptional' })}
+                            </InputLabel>
+                            <Select
+                                id="load-type"
+                                value={loadType}
+                                onChange={handleChangeLoadType}
+                                variant="filled"
+                                fullWidth
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value={'UNDEFINED'}>
+                                    {intl.formatMessage({
+                                        id: 'UndefinedDefaultValue',
+                                    })}
+                                </MenuItem>
+                                <MenuItem value={'AUXILIARY'}>
+                                    {intl.formatMessage({ id: 'Auxiliary' })}
+                                </MenuItem>
+                                <MenuItem value={'FICTITIOUS'}>
+                                    {intl.formatMessage({ id: 'Fictitious' })}
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
                     </Grid>
                 </Grid>
+                <FormattedMessage id="Setpoints" />
                 <Grid container spacing={2}>
                     <Grid item xs={4} align="center">
                         <TextField
                             required
-                            type="number"
                             id="load-active-power"
                             label={intl.formatMessage({
                                 id: 'ActivePowerText',
                             })}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        MW
+                                    </InputAdornment>
+                                ),
+                            }}
                             defaultValue=""
                         />
                     </Grid>
                     <Grid item xs={4} align="center">
                         <TextField
                             required
-                            type="number"
                             id="load-reactive-power"
                             label={intl.formatMessage({
                                 id: 'ReactivePowerText',
                             })}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        MW
+                                    </InputAdornment>
+                                ),
+                            }}
                             defaultValue=""
                         />
                     </Grid>
                 </Grid>
+                <FormattedMessage id="Connectivity" />
                 <Grid container spacing={2}>
                     <Grid item xs={4} align="center">
-                        <Select
-                            id="voltage-level"
-                            value={voltageLevel}
-                            onChange={handleChangeVoltageLevel}
-                            label={intl.formatMessage({ id: 'VoltageLevel' })}
-                            variant="filled"
-                            fullWidth
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                        </Select>
+                        <FormControl fullWidth>
+                            <InputLabel
+                                id="voltage-level-label"
+                                margin={'dense'}
+                            >
+                                {intl.formatMessage({ id: 'VoltageLevel' })}
+                            </InputLabel>
+                            <Select
+                                id="voltage-level"
+                                value={voltageLevel}
+                                onChange={handleChangeVoltageLevel}
+                                variant="filled"
+                                fullWidth
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
                     </Grid>
                     <Grid item xs={4} align="center">
-                        <Select
-                            id="bus"
-                            value={bus}
-                            onChange={handleChangeBus}
-                            label={intl.formatMessage({ id: 'BusBarBus' })}
-                            variant="filled"
-                            fullWidth
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                        </Select>
+                        <FormControl fullWidth>
+                            <InputLabel id="bus-label" margin={'dense'}>
+                                {intl.formatMessage({ id: 'BusBarBus' })}
+                            </InputLabel>
+                            <Select
+                                id="bus"
+                                value={bus}
+                                onChange={handleChangeBus}
+                                variant="filled"
+                                fullWidth
+                            >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
                     </Grid>
                 </Grid>
             </DialogContent>
