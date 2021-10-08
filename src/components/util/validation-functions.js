983d3a51b53f4c7cbb3e3ee1a41a463f347eb621
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2021-10-08 17:06:54 +0200

    Modified dialog validation
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

---
+++ src/components/util/validation-functions.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export function validateField(value, toValidate) {
+    if (toValidate.isFieldRequired) {
+        if (!value) {
+            return {
+                error: true,
+                errorMsgId: 'FieldIsRequired',
+            };
+        }
+    }
+
+    if (toValidate.isFieldNumeric) {
+        let valueNumericVal = Number(value.replace(',', '.'));
+        if (isNaN(valueNumericVal)) {
+            return {
+                error: true,
+                errorMsgId: 'FieldAcceptNumeric',
+            };
+        }
+    }
+
+    return {
+        error: false,
+        errorMsgId: '',
+    };
+}
