f6a6924c7f775fef73480d75d1a95c322abd5c98
Etienne Homer <etiennehomer@gmail.com>
2022-02-11 10:17:37 +0100

    fix modification tree rerender
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/components/graph/util/node-single-double-click-hook.js
+++ src/components/graph/util/node-single-double-click-hook.js
@@ -12,24 +12,38 @@ export function useNodeSingleAndDoubleClick(
     delay = 250
 ) {
     const [clickEvent, setClickEvent] = useState({ count: 0 });
+    const [isTimerActive, setIsTimerActive] = useState(false);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            // simple click
-            if (clickEvent.count === 1)
-                actionSimpleClick(clickEvent.event, clickEvent.node);
-            setClickEvent({ count: 0 });
-        }, delay);
-
-        // the duration between this click and the previous one
-        // is less than the value of delay = double-click
-        if (clickEvent.count === 2)
-            actionDoubleClick(clickEvent.event, clickEvent.node);
-
+        let timer = null;
+        if (isTimerActive) {
+            timer = setTimeout(() => {
+                // simple click
+                if (clickEvent.count === 1) {
+                    actionSimpleClick(clickEvent.event, clickEvent.node);
+                }
+                // the duration between this click and the previous one
+                // is less than the value of delay = double-click
+                if (clickEvent.count === 2) {
+                    actionDoubleClick(clickEvent.event, clickEvent.node);
+                }
+                setIsTimerActive(false);
+                setClickEvent({ count: 0 });
+            }, delay);
+        } else {
+            clearTimeout(timer);
+        }
         return () => clearTimeout(timer);
-    }, [clickEvent, actionSimpleClick, actionDoubleClick, delay]);
+    }, [
+        clickEvent,
+        actionSimpleClick,
+        actionDoubleClick,
+        delay,
+        isTimerActive,
+    ]);
 
     return (event, node) => {
+        setIsTimerActive(true);
         setClickEvent((prev) => ({
             count: prev.count + 1,
             event: event,
