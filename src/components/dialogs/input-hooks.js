677946dee4de8aea562640bef8f753b40cfaf628
Etienne Homer <etiennehomer@gmail.com>
2022-03-01 14:31:14 +0100

    fix warnings
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/components/dialogs/input-hooks.js
+++ src/components/dialogs/input-hooks.js
@@ -552,8 +552,8 @@ export const useEnumValue = ({
                     fullWidth
                     {...formProps}
                 >
-                    {enumValues.map((e) => (
-                        <MenuItem value={e.id} key={e.id}>
+                    {enumValues.map((e, index) => (
+                        <MenuItem value={e.id} key={e.id + '_' + index}>
                             <em>
                                 <FormattedMessage id={e.label} />
                             </em>
@@ -573,11 +573,11 @@ export const useEnumValue = ({
     ]);
 
     useEffect(() => {
-        setValue(undefined);
+        setValue('');
     }, [inputForm.toggleClear]);
 
     useEffect(() => {
-        setValue(defaultValue);
+        setValue(defaultValue || '');
     }, [defaultValue]);
 
     return [value, field];
