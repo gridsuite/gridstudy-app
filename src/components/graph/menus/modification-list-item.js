cdc51ab6859bb6bdefd14fad26f44aa9febd88ba
jacques borsenberger <jacques.borsenberger@rte-france.com>
2022-04-07 11:51:42 +0200

    fix unused icon
    Signed-off-by: jacques borsenberger <jacques.borsenberger@rte-france.com>

--- src/components/graph/menus/modification-list-item.js
+++ src/components/graph/menus/modification-list-item.js
@@ -12,7 +12,6 @@ import { useSelector } from 'react-redux';
 import { PARAM_USE_NAME } from '../../../utils/config-params';
 import Divider from '@material-ui/core/Divider';
 import PropTypes from 'prop-types';
-import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
