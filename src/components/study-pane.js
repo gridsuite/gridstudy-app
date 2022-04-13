db18812ea6a1dbfa9f5fc4d5b56ff41b6ca2c941
JacquesBorsenberger <63354128+JacquesBorsenberger@users.noreply.github.com>
2022-04-13 16:49:33 +0200

    Center on substation in search window

--- src/components/study-pane.js
+++ src/components/study-pane.js
@@ -1,11 +1,11 @@
 /**
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -143,8 +143,6 @@ const StudyPane = ({
 
     const [closeVoltageLevelDiagram, showVoltageLevelDiagram] =
         useSingleLineDiagram(studyUuid);
-
-    const mapRef = useRef();
 
     useEffect(() => {
         if (
@@ -297,7 +295,6 @@ const StudyPane = ({
                                     }
                                     runnable={runnable}
                                     setErrorMessage={setErrorMessage}
-                                    mapRef={mapRef}
                                 />
                             </div>
 
