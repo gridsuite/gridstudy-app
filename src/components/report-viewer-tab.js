869061ec36b55e4aa22fcb1d864935742aa874ee
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2022-05-18 14:50:11 +0200

    Associate report to node (#535)
    * View logs associated to a node (only logs for node or the logs of all parent nodes upwards to root node)
    * Group all network modification logs together
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/report-viewer-tab.js
+++ src/components/report-viewer-tab.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { fetchReport } from '../utils/rest-api';
 import { displayErrorMessageWithSnackbar } from '../utils/messages';
 import Paper from '@mui/material/Paper';
@@ -13,30 +13,80 @@ import { ReportViewer } from '@gridsuite/commons-ui';
 import { useSnackbar } from 'notistack';
 import PropTypes from 'prop-types';
 import WaitingLoader from './util/waiting-loader';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { useIntl } from 'react-intl';
+import makeStyles from '@mui/styles/makeStyles';
+
+const useStyles = makeStyles((theme) => ({
+    reportOnlyNode: {
+        margin: 5,
+    },
+}));
+
+const NETWORK_MODIFICATION = 'NetworkModification';
 
 /**
  * control the ReportViewer (fetch and waiting)
- * @param reportId : string  uuid of report
+ * @param studyId : string study id
  * @param visible : boolean window visible
+ * @param workingNode : object visualized node
  * @returns {*} node
  * @constructor
  */
-export const ReportViewerTab = ({ reportId, visible }) => {
+export const ReportViewerTab = ({ studyId, visible, workingNode }) => {
+    const intl = useIntl();
+    const classes = useStyles();
+
     const [report, setReport] = useState(null);
     const [waitingLoadReport, setWaitingLoadReport] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
+    const [nodeOnlyReport, setNodeOnlyReport] = useState(true);
 
-    const reportIdRef = useRef();
+    const handleChangeValue = useCallback((event) => {
+        setNodeOnlyReport(event.target.checked);
+    }, []);
+
+    function condenseReport(report) {
+        let newReport = {
+            taskKey: report.taskKey,
+            defaultName: report.defaultName,
+            taskValues: report.taskValues,
+            reports: report.reports,
+            subReporters: [],
+        };
+        report.subReporters.forEach((subReport1) => {
+            if (subReport1.taskKey === NETWORK_MODIFICATION) {
+                // we group all network modifications together
+                subReport1.subReporters.forEach((subReport2) =>
+                    newReport.subReporters.push(subReport2)
+                );
+            } else {
+                newReport.subReporters.push(subReport1);
+            }
+        });
+        return newReport;
+    }
 
     useEffect(() => {
-        if (visible && reportIdRef.current !== reportId) {
+        if (visible) {
             setWaitingLoadReport(true);
-            reportIdRef.current = reportId;
-            fetchReport(reportId)
+            fetchReport(studyId, workingNode.id, nodeOnlyReport)
                 .then((fetchedReport) => {
-                    if (reportIdRef.current === reportId)
-                        // set the report only if it's the last expected/fetched report
-                        setReport(fetchedReport);
+                    if (fetchedReport.length === 1) {
+                        setReport(condenseReport(fetchedReport[0]));
+                    } else {
+                        let globalReport = {
+                            taskKey: 'root',
+                            defaultName: 'Logs',
+                            taskValues: {},
+                            reports: [],
+                            subReporters: fetchedReport.map((r) =>
+                                condenseReport(r)
+                            ),
+                        };
+                        setReport(globalReport);
+                    }
                 })
                 .catch((errorMessage) =>
                     displayErrorMessageWithSnackbar({
@@ -48,12 +98,27 @@ export const ReportViewerTab = ({ reportId, visible }) => {
                     setWaitingLoadReport(false);
                 });
         }
-    }, [visible, reportId, enqueueSnackbar]);
+    }, [visible, studyId, workingNode, nodeOnlyReport, enqueueSnackbar]);
 
     return (
         <WaitingLoader loading={waitingLoadReport} message={'loadingReport'}>
             {report && (
                 <Paper className={clsx('singlestretch-child')}>
+                    <FormControlLabel
+                        className={classes.reportOnlyNode}
+                        control={
+                            <Switch
+                                checked={nodeOnlyReport}
+                                inputProps={{
+                                    'aria-label': 'primary checkbox',
+                                }}
+                                onChange={(e) => handleChangeValue(e)}
+                            />
+                        }
+                        label={intl.formatMessage({
+                            id: 'LogOnlySingleNode',
+                        })}
+                    />
                     <ReportViewer jsonReport={report} />
                 </Paper>
             )}
@@ -62,6 +127,7 @@ export const ReportViewerTab = ({ reportId, visible }) => {
 };
 
 ReportViewerTab.propTypes = {
-    reportId: PropTypes.string,
+    studyId: PropTypes.string,
     visible: PropTypes.bool,
+    workingNode: PropTypes.object,
 };
