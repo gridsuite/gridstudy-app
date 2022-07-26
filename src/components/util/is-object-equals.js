ab254adfafb5dd614f239ce9f8ab795395819a0f
David BRAQUART <david.braquart@rte-france.com>
2022-07-26 17:35:55 +0200

    add missing header

--- src/components/util/is-object-equals.js
+++ src/components/util/is-object-equals.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export function isEqual(obj1, obj2) {
     var props1 = Object.getOwnPropertyNames(obj1);
     var props2 = Object.getOwnPropertyNames(obj2);
