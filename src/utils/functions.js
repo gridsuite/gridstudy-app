dd76162613f58e456725576b82ff09165fae8112
Laurent Garnier <laurent.garnier@rte-france.com>
2022-08-18 13:02:23 +0200

    fix:adds a js for debug hooks

---
+++ src/utils/functions.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { useCallback, useEffect, useRef } from 'react';
+
+const usePrevious = (value, initialValue) => {
+    const ref = useRef(initialValue);
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+};
+
+export const useEffectDebug = (
+    effectHook,
+    dependencies,
+    dependencyNames = []
+) => {
+    const previousDeps = usePrevious(dependencies, []);
+
+    const changedDeps = dependencies.reduce((accum, dependency, index) => {
+        if (dependency !== previousDeps[index]) {
+            const keyName = dependencyNames[index] || index;
+            return {
+                ...accum,
+                [keyName]: {
+                    before: previousDeps[index],
+                    after: dependency,
+                },
+            };
+        }
+
+        return accum;
+    }, {});
+
+    if (Object.keys(changedDeps).length) {
+        console.log('[use-effect-debugger] ', changedDeps);
+    }
+
+    useEffect(effectHook, dependencies);
+};
+
+export const useCallbackDebug = (
+    effectHook,
+    dependencies,
+    dependencyNames = []
+) => {
+    const previousDeps = usePrevious(dependencies, []);
+
+    const changedDeps = dependencies.reduce((accum, dependency, index) => {
+        if (dependency !== previousDeps[index]) {
+            const keyName = dependencyNames[index] || index;
+            return {
+                ...accum,
+                [keyName]: {
+                    before: previousDeps[index],
+                    after: dependency,
+                },
+            };
+        }
+
+        return accum;
+    }, {});
+
+    if (Object.keys(changedDeps).length) {
+        console.log('[use-callback-debugger] ', changedDeps);
+    }
+
+    return useCallback(effectHook, dependencies);
+};
