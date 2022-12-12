8780aaeea594c7c0a1e231fcb2ca6cb64fefc889
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2022-12-12 11:05:16 +0100

    Refactor network modifications endpoints (#894)
    Use the new generic endpoints from network modification server
    
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/components/dialogs/line-attach-to-voltage-level-dialog.js
+++ src/components/dialogs/line-attach-to-voltage-level-dialog.js
@@ -39,6 +39,7 @@ import VoltageLevelCreationDialog from './voltage-level-creation-dialog';
 import { makeRefreshBusOrBusbarSectionsCallback } from './connectivity-edition';
 import { Box } from '@mui/system';
 import { useAutocompleteField } from './inputs/use-autocomplete-field';
+import { MODIFICATION_TYPE } from '../network/constants';
 
 const getId = (e) => e?.id || (typeof e === 'string' ? e : '');
 
@@ -384,6 +385,7 @@ const LineAttachToVoltageLevelDialog = ({
         }) => {
             return new Promise(() => {
                 const preparedVoltageLevel = {
+                    type: MODIFICATION_TYPE.VOLTAGE_LEVEL_CREATION,
                     equipmentId: voltageLevelId,
                     equipmentName: voltageLevelName,
                     nominalVoltage: nominalVoltage,
@@ -441,6 +443,7 @@ const LineAttachToVoltageLevelDialog = ({
         ) => {
             return new Promise(() => {
                 const preparedLine = {
+                    type: MODIFICATION_TYPE.LINE_CREATION,
                     equipmentId: lineId,
                     equipmentName: lineName,
                     seriesResistance: seriesResistance,
--- src/components/dialogs/line-split-with-voltage-level-dialog.js
+++ src/components/dialogs/line-split-with-voltage-level-dialog.js
@@ -39,6 +39,7 @@ import EquipmentSearchDialog from './equipment-search-dialog';
 import { useFormSearchCopy } from './form-search-copy-hook';
 import EditIcon from '@mui/icons-material/Edit';
 import { useAutocompleteField } from './inputs/use-autocomplete-field';
+import { MODIFICATION_TYPE } from '../network/constants';
 
 const getId = (e) => e?.id || (typeof e === 'string' ? e : '');
 
@@ -352,6 +353,7 @@ const LineSplitWithVoltageLevelDialog = ({
         }) => {
             return new Promise(() => {
                 const preparedVoltageLevel = {
+                    type: MODIFICATION_TYPE.VOLTAGE_LEVEL_CREATION,
                     equipmentId: voltageLevelId,
                     equipmentName: voltageLevelName,
                     nominalVoltage: nominalVoltage,
