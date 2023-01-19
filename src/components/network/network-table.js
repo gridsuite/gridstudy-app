2e5522e2b322a50353e64ff5cf1c06177dc78f23
Florent MILLOT <millotflo@gmail.com>
2023-01-19 10:52:34 +0100

    Put back not useless check
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/components/network/network-table.js
+++ src/components/network/network-table.js
@@ -404,7 +404,7 @@ const NetworkTable = (props) => {
 
     const getRows = useCallback(
         (index) => {
-            if (props.disabled) {
+            if (props.disabled || !props.network) {
                 return [];
             }
             const tableDefinition = TABLES_DEFINITION_INDEXES.get(index);
