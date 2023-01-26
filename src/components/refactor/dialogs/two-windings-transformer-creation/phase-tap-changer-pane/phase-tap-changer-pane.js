874f1487abf0df6baba9cab5eff611755b800d58
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-01-26 11:33:35 +0100

    fix: copyright headers
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/refactor/dialogs/two-windings-transformer-creation/phase-tap-changer-pane/phase-tap-changer-pane.js
+++ src/components/refactor/dialogs/two-windings-transformer-creation/phase-tap-changer-pane/phase-tap-changer-pane.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { Grid } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
 import BooleanInput from '../../../rhf-inputs/boolean-input';
--- src/components/refactor/rhf-inputs/table-inputs/table-numerical-input.js
+++ src/components/refactor/rhf-inputs/table-inputs/table-numerical-input.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { TextField, Tooltip } from '@mui/material';
 import { useController, useFormContext } from 'react-hook-form';
 import { useIntl } from 'react-intl';
