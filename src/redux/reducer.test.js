146a9db4f48d94b1bcc1b88cab14d98e415376c0
BOUTIER Charly <charly.boutier@rte-france.com>
2023-01-27 01:24:42 +0100

    Some unit tests for the reducer.
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

---
+++ src/redux/reducer.test.js
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import React from 'react';
+
+import { store } from './store';
+import { reducer } from './reducer';
+import {
+    DECREMENT_NETWORK_AREA_DIAGRAM_DEPTH,
+    INCREMENT_NETWORK_AREA_DIAGRAM_DEPTH,
+    RESET_NETWORK_AREA_DIAGRAM_DEPTH,
+    SET_FULLSCREEN_DIAGRAM,
+} from './actions';
+import { SvgType } from '../components/diagrams/diagram-common';
+
+test('reducer.RESET_NETWORK_AREA_DIAGRAM_DEPTH', () => {
+    const initialState = { networkAreaDiagramDepth: 12 };
+    const expectedState = { networkAreaDiagramDepth: 0 };
+
+    expect(
+        reducer(initialState, {
+            type: RESET_NETWORK_AREA_DIAGRAM_DEPTH,
+        })
+    ).toEqual(expectedState);
+});
+
+test('reducer.INCREMENT_NETWORK_AREA_DIAGRAM_DEPTH', () => {
+    const initialState = { networkAreaDiagramDepth: 12 };
+    const expectedState = { networkAreaDiagramDepth: 13 };
+
+    expect(
+        reducer(initialState, {
+            type: INCREMENT_NETWORK_AREA_DIAGRAM_DEPTH,
+        })
+    ).toEqual(expectedState);
+});
+
+test('reducer.DECREMENT_NETWORK_AREA_DIAGRAM_DEPTH', () => {
+    const initialState = { networkAreaDiagramDepth: 12 };
+    const expectedState = { networkAreaDiagramDepth: 11 };
+
+    expect(
+        reducer(initialState, {
+            type: DECREMENT_NETWORK_AREA_DIAGRAM_DEPTH,
+        })
+    ).toEqual(expectedState);
+
+    const initialState2 = { networkAreaDiagramDepth: 0 };
+    const expectedState2 = { networkAreaDiagramDepth: 0 };
+
+    expect(
+        reducer(initialState2, {
+            type: DECREMENT_NETWORK_AREA_DIAGRAM_DEPTH,
+        })
+    ).toEqual(expectedState2);
+});
+
+test('reducer.SET_FULLSCREEN_DIAGRAM', () => {
+    // From initial values
+    const initialState = { fullScreenDiagram: null };
+    const expectedState = {
+        fullScreenDiagram: { id: 3, svgType: SvgType.VOLTAGE_LEVEL },
+    };
+
+    expect(
+        reducer(initialState, {
+            type: SET_FULLSCREEN_DIAGRAM,
+            diagramId: 3,
+            svgType: SvgType.VOLTAGE_LEVEL,
+        })
+    ).toEqual(expectedState);
+
+    // Changing the fullscreen diagram
+    const initialState2 = {
+        fullScreenDiagram: { id: 6, svgType: SvgType.NETWORK_AREA_DIAGRAM },
+    };
+    const expectedState2 = {
+        fullScreenDiagram: { id: 12, svgType: SvgType.SUBSTATION },
+    };
+
+    expect(
+        reducer(initialState2, {
+            type: SET_FULLSCREEN_DIAGRAM,
+            diagramId: 12,
+            svgType: SvgType.SUBSTATION,
+        })
+    ).toEqual(expectedState2);
+
+    // Removing the fullscreen
+    const initialState3 = {
+        fullScreenDiagram: { id: 18, svgType: SvgType.NETWORK_AREA_DIAGRAM },
+    };
+    const expectedState3 = { fullScreenDiagram: { id: null } };
+
+    expect(
+        reducer(initialState3, {
+            type: SET_FULLSCREEN_DIAGRAM,
+            diagramId: null,
+            svgType: undefined,
+        })
+    ).toEqual(expectedState3);
+});
