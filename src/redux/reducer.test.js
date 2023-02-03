a5de9740dc29719a3337aab41afc0a90fd24a996
Franck LECUYER <franck.lecuyer@rte-france.com>
2023-02-03 11:38:40 +0100

    Fix tests on reducer
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -27,7 +27,10 @@ import { SvgType, ViewState } from '../components/diagrams/diagram-common';
 
 test('reducer.RESET_NETWORK_AREA_DIAGRAM_DEPTH', () => {
     const initialState = { networkAreaDiagramDepth: 12 };
-    const expectedState = { networkAreaDiagramDepth: 0 };
+    const expectedState = {
+        networkAreaDiagramDepth: 0,
+        networkAreaDiagramNbVoltageLevels: 0,
+    };
 
     expect(reducer(initialState, resetNetworkAreaDiagramDepth())).toEqual(
         expectedState
