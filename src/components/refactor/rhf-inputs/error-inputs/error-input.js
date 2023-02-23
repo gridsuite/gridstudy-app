3391faafe2ce2f916092261016861ceda4196d4a
Seddik Yengui <seddik.yengui@rte-france.com>
2023-02-23 11:07:04 +0100

    add license

--- src/components/refactor/rhf-inputs/error-inputs/error-input.js
+++ src/components/refactor/rhf-inputs/error-inputs/error-input.js
@@ -1,13 +1,13 @@
-import { useController } from 'react-hook-form';
-import { FormattedMessage } from 'react-intl';
-import React from 'react';
-
 /**
  * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import { useController } from 'react-hook-form';
+import { FormattedMessage } from 'react-intl';
+import React from 'react';
 
 const ErrorInput = ({ name, InputField }) => {
     const {
