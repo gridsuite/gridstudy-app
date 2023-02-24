c29565396dc55af52373532ee353bb438b665994
ElMehdiTailassan <tailassan.elmehdi@gmail.com>
2023-02-24 10:26:16 +0100

    unify list equipments (#1040)
    * Refactorization of equipment types and modification types strings : uses enum instead.
    
    ---------
    
    Co-authored-by: BOUTIER Charly <charly.boutier@rte-france.com>
    Co-authored-by: TAILASSAN EL MEHDI <mehdi.tailassan@rte-france.com>

---
+++ src/components/util/equipment-types.js
@@ -0,0 +1,83 @@
+import {
+    fetchBatteries,
+    fetchDanglingLines,
+    fetchGenerators,
+    fetchHvdcLines,
+    fetchLccConverterStations,
+    fetchLines,
+    fetchLoads,
+    fetchShuntCompensators,
+    fetchStaticVarCompensators,
+    fetchSubstations,
+    fetchThreeWindingsTransformers,
+    fetchTwoWindingsTransformers,
+    fetchVoltageLevels,
+    fetchVscConverterStations,
+} from 'utils/rest-api';
+
+export const EQUIPMENT_TYPES = {
+    SUBSTATION: {
+        type: 'SUBSTATION',
+        fetchers: [fetchSubstations],
+    },
+    VOLTAGE_LEVEL: {
+        type: 'VOLTAGE_LEVEL',
+        fetchers: [fetchVoltageLevels],
+    },
+    LINE: {
+        type: 'LINE',
+        fetchers: [fetchLines],
+    },
+    TWO_WINDINGS_TRANSFORMER: {
+        type: 'TWO_WINDINGS_TRANSFORMER',
+        fetchers: [fetchTwoWindingsTransformers],
+    },
+    THREE_WINDINGS_TRANSFORMER: {
+        type: 'THREE_WINDINGS_TRANSFORMER',
+        fetchers: [fetchThreeWindingsTransformers],
+    },
+    HVDC_LINE: {
+        type: 'HVDC_LINE',
+        fetchers: [fetchHvdcLines],
+    },
+    GENERATOR: {
+        type: 'GENERATOR',
+        fetchers: [fetchGenerators],
+    },
+    BATTERY: {
+        type: 'BATTERY',
+        fetchers: [fetchBatteries],
+    },
+    LOAD: {
+        type: 'LOAD',
+        fetchers: [fetchLoads],
+    },
+    SHUNT_COMPENSATOR: {
+        type: 'SHUNT_COMPENSATOR',
+        fetchers: [fetchShuntCompensators],
+    },
+    DANGLING_LINE: {
+        type: 'DANGLING_LINE',
+        fetchers: [fetchDanglingLines],
+    },
+    STATIC_VAR_COMPENSATOR: {
+        type: 'STATIC_VAR_COMPENSATOR',
+        fetchers: [fetchStaticVarCompensators],
+    },
+    HVDC_CONVERTER_STATION: {
+        type: 'HVDC_CONVERTER_STATION',
+        fetchers: [fetchLccConverterStations, fetchVscConverterStations],
+    },
+    VSC_CONVERTER_STATION: {
+        type: 'VSC_CONVERTER_STATION',
+        fetchers: [fetchVscConverterStations],
+    },
+    LCC_CONVERTER_STATION: {
+        type: 'LCC_CONVERTER_STATION',
+        fetchers: [fetchLccConverterStations],
+    },
+    SWITCH: {
+        type: 'SWITCH',
+        fetchers: [],
+    },
+};
