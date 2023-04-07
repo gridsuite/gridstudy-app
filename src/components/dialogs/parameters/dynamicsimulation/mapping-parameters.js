7000624752472ee8f0d5f42964c6149c99c89b3d
Thang PHAM <117309322+thangqp@users.noreply.github.com>
2023-04-07 18:23:19 +0200

    Dynamic Simulation - Config parameters startTime, stopTime, mapping, solver and provider  (#1131)

---
+++ src/components/dialogs/parameters/dynamicsimulation/mapping-parameters.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { Grid } from '@mui/material';
+import { makeComponentsFor, TYPES } from '../util/make-component-utils';
+import { useCallback } from 'react';
+
+const MappingParameters = ({ mapping, onUpdateMapping }) => {
+    const { mappings } = mapping;
+
+    const handleUpdateMapping = useCallback(
+        (newMapping) => {
+            onUpdateMapping(newMapping);
+        },
+        [onUpdateMapping]
+    );
+
+    const defParams = {
+        mapping: {
+            type: TYPES.enum,
+            description: 'DynamicSimulationMapping',
+            values: mappings.reduce((obj, curr) => {
+                obj[curr.name] = curr.name;
+                return obj;
+            }, {}),
+            renderValue: (value) => value,
+        },
+    };
+
+    return (
+        mapping && (
+            <Grid container>
+                {makeComponentsFor(defParams, mapping, handleUpdateMapping)}
+            </Grid>
+        )
+    );
+};
+
+export default MappingParameters;
---
+++ src/components/dialogs/parameters/dynamicsimulation/solver/simplified-solver-parameters.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { makeComponentsFor, TYPES } from '../../util/make-component-utils';
+import { useCallback, useMemo } from 'react';
+import { debounce } from '@mui/material';
+
+const SimplifiedSolverParameters = ({
+    simplifiedSolver,
+    onUpdateSimplifiedSolver,
+}) => {
+    const defParams = {
+        hMin: {
+            type: TYPES.float,
+            description: 'DynamicSimulationSimplifiedSolverHMin',
+        },
+        hMax: {
+            type: TYPES.float,
+            description: 'DynamicSimulationSimplifiedSolverHMax',
+        },
+        kReduceStep: {
+            type: TYPES.float,
+            description: 'DynamicSimulationSimplifiedSolverKReduceStep',
+        },
+        nEff: {
+            type: TYPES.integer,
+            description: 'DynamicSimulationSimplifiedSolverNEff',
+        },
+        nDeadband: {
+            type: TYPES.integer,
+            description: 'DynamicSimulationSimplifiedSolverNDeadband',
+        },
+        maxNewtonTry: {
+            type: TYPES.integer,
+            description: 'DynamicSimulationSimplifiedSolverMaxNewtonTry',
+        },
+        linearSolverName: {
+            type: TYPES.text,
+            description: 'DynamicSimulationSimplifiedSolverLinearSolverName',
+        },
+        recalculateStep: {
+            type: TYPES.bool,
+            description: 'DynamicSimulationSimplifiedSolverRecalculateStep',
+        },
+    };
+
+    const handleUpdateSimplifiedSolver = useCallback(
+        (newSimplifiedParameters) => {
+            onUpdateSimplifiedSolver(newSimplifiedParameters);
+        },
+        [onUpdateSimplifiedSolver]
+    );
+
+    const delayedHandleUpdateSimplifiedSolver = useMemo(
+        () => debounce(handleUpdateSimplifiedSolver, 500),
+        [handleUpdateSimplifiedSolver]
+    );
+
+    return (
+        <>
+            {makeComponentsFor(
+                defParams,
+                simplifiedSolver,
+                delayedHandleUpdateSimplifiedSolver
+            )}
+        </>
+    );
+};
+
+export default SimplifiedSolverParameters;
