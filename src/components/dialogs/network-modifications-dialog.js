594c42946c8caa719ee8384745afe33924781874
TOURI ANIS <anis-1.touri@rte-france.com>
2023-04-20 14:18:48 +0200

    clean props

--- src/components/dialogs/network-modifications-dialog.js
+++ src/components/dialogs/network-modifications-dialog.js
@@ -36,7 +36,6 @@ const NetworkModificationDialog = ({
     onClose,
     onOpenDialog,
     dialogs,
-    setIsUpdate,
 }) => {
     const classes = useStyles();
     const intl = useIntl();
@@ -102,6 +101,8 @@ const NetworkModificationDialog = ({
 NetworkModificationDialog.propTypes = {
     open: PropTypes.bool.isRequired,
     onClose: PropTypes.func.isRequired,
+    onOpenDialog: PropTypes.func.isRequired,
+    dialogs: PropTypes.array,
 };
 
 export default NetworkModificationDialog;
