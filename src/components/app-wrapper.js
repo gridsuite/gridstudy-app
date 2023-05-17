c3331f989585efc0ec9644647376fdaf299e46e1
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2023-05-17 15:26:37 +0200

    improve popover style
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/components/app-wrapper.js
+++ src/components/app-wrapper.js
@@ -61,6 +61,9 @@ const lightTheme = createTheme({
     selectedRow: {
         background: '#8E9C9B',
     },
+    tooltipTable: {
+        background: '#e6e6e6',
+    },
     mapboxStyle: 'mapbox://styles/mapbox/light-v9',
     aggrid: 'ag-theme-alpine',
 });
@@ -80,7 +83,10 @@ const darkTheme = createTheme({
     selectedRow: {
         background: '#545C5B',
     },
-    mapboxStyle: 'mapbox://styles/mapbox/dark-v9',
+    tooltipTable: {
+        background: '#121212',
+    },
+    mapboxStyle: 'mapbox://styles   /mapbox/dark-v9',
     aggrid: 'ag-theme-alpine-dark',
 });
 
