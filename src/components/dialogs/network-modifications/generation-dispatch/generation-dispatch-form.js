f955e30bb1c86db7cc77902029a024ff3ccc2ccc
BOUTIER Charly <charly.boutier@rte-france.com>
2023-05-25 10:51:12 +0200

    Adds missing copyright comments, code homogenization.

--- src/components/dialogs/network-modifications/generation-dispatch/generation-dispatch-form.js
+++ src/components/dialogs/network-modifications/generation-dispatch/generation-dispatch-form.js
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 import { EQUIPMENT_TYPES } from 'components/utils/equipment-types';
 import DirectoryItemsInput from 'components/utils/rhf-inputs/directory-items-input';
 import FloatInput from 'components/utils/rhf-inputs/float-input';
