d1671d4ea328151320fb7e362babb91e379b6271
Thang PHAM <phamthang37@gmail.com>
2023-05-25 13:32:19 +0200

    Dynamic Simulation - Refactoring naming to avoid conflict with EQUIPMENT_TYPES

--- src/components/dialogs/parameters/dynamicsimulation/curve/dialog/equipment-filter.js
+++ src/components/dialogs/parameters/dynamicsimulation/curve/dialog/equipment-filter.js
@@ -23,11 +23,11 @@ import CountrySelect from '../country-select';
 import CheckboxSelect from '../common/checkbox-select';
 import { useSelector } from 'react-redux';
 import { useSnackMessage } from '@gridsuite/commons-ui';
-import { EQUIPMENT_TYPES as ALL_EQUIPMENT_TYPES } from '../../../../../utils/equipment-types';
+import { EQUIPMENT_TYPES } from '../../../../../utils/equipment-types';
 
-export const EQUIPMENT_TYPES = {
-    [ALL_EQUIPMENT_TYPES.GENERATOR.type]: ALL_EQUIPMENT_TYPES.GENERATOR,
-    [ALL_EQUIPMENT_TYPES.LOAD.type]: ALL_EQUIPMENT_TYPES.LOAD,
+export const CURVE_EQUIPMENT_TYPES = {
+    [EQUIPMENT_TYPES.GENERATOR.type]: EQUIPMENT_TYPES.GENERATOR,
+    [EQUIPMENT_TYPES.LOAD.type]: EQUIPMENT_TYPES.LOAD,
 };
 
 const TENSION_UNIT = 'kV';
@@ -361,7 +361,7 @@ const EquipmentFilter = forwardRef(
                             size="small"
                             sx={{ width: '100%' }}
                         >
-                            {Object.entries(EQUIPMENT_TYPES).map(
+                            {Object.entries(CURVE_EQUIPMENT_TYPES).map(
                                 ([key, value]) => (
                                     <MenuItem key={key} value={value}>
                                         {intl.formatMessage({ id: value.type })}
