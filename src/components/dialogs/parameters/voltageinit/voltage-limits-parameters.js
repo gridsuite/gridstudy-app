88c852563ef5254f91c46a37ee381ebed1f146cf
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-06-05 14:57:48 +0200

    Add copyrights

--- src/components/dialogs/parameters/voltageinit/voltage-limits-parameters.js
+++ src/components/dialogs/parameters/voltageinit/voltage-limits-parameters.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { elementType, useSnackMessage } from '@gridsuite/commons-ui';
 import { Button, DialogActions, IconButton, Tooltip } from '@mui/material';
 import SubmitButton from 'components/dialogs/commons/submitButton';
