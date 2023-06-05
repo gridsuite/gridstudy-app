165ab39a6561f618bbcc6b530dc045823be9a9cd
Hugo Marcellin <hugo.marcelin@rte-france.com>
2023-06-05 15:47:26 +0200

    Change tab order

--- src/components/dialogs/parameters/parameters.js
+++ src/components/dialogs/parameters/parameters.js
@@ -540,13 +540,6 @@ const Parameters = ({ user, isParametersOpen, hideParameters }) => {
                         <Tab
                             disabled={!studyUuid}
                             label={
-                                <FormattedMessage id="VoltageInitOptimalReactivePowerFlow" />
-                            }
-                            value={TAB_VALUES.voltageInitParamsTabValue}
-                        />
-                        <Tab
-                            disabled={!studyUuid}
-                            label={
                                 <FormattedMessage id="SensitivityAnalysis" />
                             }
                             value={TAB_VALUES.sensitivityAnalysisParamsTabValue}
@@ -569,6 +562,13 @@ const Parameters = ({ user, isParametersOpen, hideParameters }) => {
                                 }
                             />
                         )}
+                        <Tab
+                            disabled={!studyUuid}
+                            label={
+                                <FormattedMessage id="VoltageInitOptimalReactivePowerFlow" />
+                            }
+                            value={TAB_VALUES.voltageInitParamsTabValue}
+                        />
                         <Tab
                             label={<FormattedMessage id="Advanced" />}
                             value={TAB_VALUES.advancedParamsTabValue}
