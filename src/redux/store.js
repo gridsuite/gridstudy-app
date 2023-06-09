1d8605140855f4b80cfd8fafeb4ff023ea644e2f
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-06-09 12:24:19 +0200

    fix: wrong changes
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,4 @@
 import { createStore } from 'redux';
 import { reducer } from './reducer';
 
-const enhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
-export const store = createStore(reducer, enhancers);
+export const store = createStore(reducer);
