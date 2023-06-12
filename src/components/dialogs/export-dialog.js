cc2edea2bebaf8e4530ee024942ffb5e4a4c34f6
Mahdi Triki <mohamed-mahdi.triki@rte-france.com>
2023-06-12 14:16:36 +0200

    Reset currentParameters state when closing dialog
    Signed-off-by: Mahdi Triki <mohamed-mahdi.triki@rte-france.com>

--- src/components/dialogs/export-dialog.js
+++ src/components/dialogs/export-dialog.js
@@ -105,6 +105,7 @@ const ExportDialog = ({
     };
 
     const handleClose = () => {
+        setCurrentParameters({})
         setExportStudyErr('');
         setSelectedFormat('');
         setLoading(false);
