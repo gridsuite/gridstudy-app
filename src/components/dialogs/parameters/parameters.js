4e5bed375c730cbce526795f6960877ed06f4fac
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2023-06-12 15:41:45 +0200

    fix(parameters): debounce fetch always after setState to keep reactivity. (#1341)
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/dialogs/parameters/parameters.js
+++ src/components/dialogs/parameters/parameters.js
@@ -213,6 +213,24 @@ export const useParametersBackend = (
     const [specificParamsDescription, setSpecificParamsDescription] =
         useState(null);
 
+    const backendUpdateParametersCb = useCallback(
+        (studyUuid, newParams, oldParams) => {
+            backendUpdateParameters(studyUuid, newParams).catch((error) => {
+                setParams(oldParams);
+                snackError({
+                    messageTxt: error.message,
+                    headerId: 'update' + type + 'ParametersError',
+                });
+            });
+        },
+        [backendUpdateParameters, snackError, type]
+    );
+
+    const debouncedBackendUpdateParameters = useDebounce(
+        backendUpdateParametersCb,
+        1000
+    );
+
     const updateProvider = useCallback(
         (newProvider) => {
             backendUpdateProvider(studyUuid, newProvider)
@@ -254,26 +272,20 @@ export const useParametersBackend = (
             if (backendUpdateParameters) {
                 let oldParams = { ...params };
                 setParams(newParams);
-                backendUpdateParameters(studyUuid, newParams).catch((error) => {
-                    setParams(oldParams);
-                    snackError({
-                        messageTxt: error.message,
-                        headerId: 'update' + type + 'ParametersError',
-                    });
-                });
+                debouncedBackendUpdateParameters(
+                    studyUuid,
+                    newParams,
+                    oldParams
+                );
             }
         },
         [
-            type,
+            debouncedBackendUpdateParameters,
             backendUpdateParameters,
             params,
-            snackError,
             studyUuid,
-            setParams,
         ]
     );
-
-    const debouncedUpdateParameter = useDebounce(updateParameter, 1000);
 
     const resetParameters = useCallback(
         (callBack) => {
@@ -391,7 +403,7 @@ export const useParametersBackend = (
         updateProvider,
         resetProvider,
         params,
-        debouncedUpdateParameter,
+        updateParameter,
         resetParameters,
         specificParamsDescription,
     ];
@@ -408,10 +420,9 @@ export function useParameterState(paramName) {
         setParamLocalState(paramGlobalState);
     }, [paramGlobalState]);
 
-    const handleChangeParamLocalState = useCallback(
-        (value) => {
-            setParamLocalState(value);
-            updateConfigParameter(paramName, value).catch((error) => {
+    const backendupdateConfigParameterCb = useCallback(
+        (studyUuid, newParams) => {
+            updateConfigParameter(studyUuid, newParams).catch((error) => {
                 setParamLocalState(paramGlobalState);
                 snackError({
                     messageTxt: error.message,
@@ -419,7 +430,20 @@ export function useParameterState(paramName) {
                 });
             });
         },
-        [paramName, setParamLocalState, paramGlobalState, snackError]
+        [paramGlobalState, snackError]
+    );
+
+    const debouncedBackendupdateConfigParameterCb = useDebounce(
+        backendupdateConfigParameterCb,
+        1000
+    );
+
+    const handleChangeParamLocalState = useCallback(
+        (value) => {
+            setParamLocalState(value);
+            debouncedBackendupdateConfigParameterCb(paramName, value);
+        },
+        [debouncedBackendupdateConfigParameterCb, paramName]
     );
 
     return [paramLocalState, handleChangeParamLocalState];
