6c877114e56ff6893d9f956edcf64305448307ae
TOURI ANIS <anis-1.touri@rte-france.com>
2023-06-14 18:11:54 +0200

    clean code

--- src/components/study-pane.js
+++ src/components/study-pane.js
@@ -23,7 +23,6 @@ import {
     PARAM_LINE_FULL_PATH,
     PARAM_LINE_PARALLEL_PATH,
 } from '../utils/config-params';
-import { getLoadFlowRunningStatus } from './utils/running-status';
 import NetworkMapTab from './network-map-tab';
 import { ReportViewerTab } from './report-viewer-tab';
 import { ResultViewTab } from './result-view-tab';
