de2f8d8452ba1079335c94da5897cc7f8f0ed55b
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2023-06-14 10:21:50 +0200

    Fix NetworkModificationNodeEditor : Clean modifications clipboard when origin node is removed (#1356)
    This clipboard needs to be cleaned when those modifications came from the newly deleted node.
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/graph/menus/network-modification-node-editor.js
+++ src/components/graph/menus/network-modification-node-editor.js
@@ -164,6 +164,8 @@ const NetworkModificationNodeEditor = () => {
     const [toggleSelectAll, setToggleSelectAll] = useState();
     const [copiedModifications, setCopiedModifications] = useState([]);
     const [copyInfos, setCopyInfos] = useState(null);
+    const copyInfosRef = useRef();
+    copyInfosRef.current = copyInfos;
 
     const [isDragging, setIsDragging] = useState(false);
 
@@ -179,15 +181,16 @@ const NetworkModificationNodeEditor = () => {
     const [isUpdate, setIsUpdate] = useState(false);
 
     const cleanClipboard = useCallback(() => {
-        if (copiedModifications.length <= 0) {
-            return;
-        }
         setCopyInfos(null);
-        setCopiedModifications([]);
-        snackInfo({
-            messageId: 'CopiedModificationInvalidationMessage',
+        setCopiedModifications((old) => {
+            if (old.length > 0) {
+                snackInfo({
+                    messageId: 'CopiedModificationInvalidationMessage',
+                });
+            }
+            return [];
         });
-    }, [snackInfo, copiedModifications]);
+    }, [snackInfo]);
 
     // TODO this is not complete.
     // We should clean Clipboard on notifications when another user edit
@@ -422,6 +425,21 @@ const NetworkModificationNodeEditor = () => {
     useEffect(() => {
         if (studyUpdatedForce.eventData.headers) {
             if (
+                studyUpdatedForce.eventData.headers['updateType'] ===
+                'nodeDeleted'
+            ) {
+                if (
+                    copyInfosRef.current &&
+                    studyUpdatedForce.eventData.headers['nodes'].some(
+                        (nodeId) =>
+                            nodeId === copyInfosRef.current.originNodeUuid
+                    )
+                ) {
+                    // Must clean modifications clipboard if the origin Node is removed
+                    cleanClipboard();
+                }
+            }
+            if (
                 currentNodeIdRef.current !==
                 studyUpdatedForce.eventData.headers['parentNode']
             ) {
@@ -460,6 +478,7 @@ const NetworkModificationNodeEditor = () => {
         dofetchNetworkModifications,
         manageNotification,
         studyUpdatedForce,
+        cleanClipboard,
     ]);
 
     const [openNetworkModificationsDialog, setOpenNetworkModificationsDialog] =
@@ -535,8 +554,11 @@ const NetworkModificationNodeEditor = () => {
 
     const doCopyModifications = useCallback(() => {
         setCopiedModifications(selectedModificationsIds());
-        setCopyInfos({ copyType: CopyType.COPY });
-    }, [selectedModificationsIds]);
+        setCopyInfos({
+            copyType: CopyType.COPY,
+            originNodeUuid: currentNode.id,
+        });
+    }, [currentNode?.id, selectedModificationsIds]);
 
     const doPasteModifications = useCallback(() => {
         if (copyInfos.copyType === CopyType.MOVE) {
