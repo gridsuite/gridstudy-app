94356c72b60c32ae81964095376c19fd340dde56
souissimai <133104748+souissimai@users.noreply.github.com>
2023-06-27 16:18:56 +0200

    Availabity of voltage violations and fixes for load flow Result (#1385)
    * Use CustomAgGrid for loadFlowResult
    ---------
    
    Signed-off-by: Maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/utils/rest-api.js
+++ src/utils/rest-api.js
@@ -691,17 +691,17 @@ export function fetchLineOrTransformer(
     return backendFetchJson(fetchEquipmentInfosUrl);
 }
 
-export function fetchCurrentLimitViolations(
+export function fetchLimitViolations(
     studyUuid,
     currentNodeUuid,
     limitReduction
 ) {
     console.info(
-        `Fetching current limit violations (with limit reduction ${limitReduction}) ...`
+        `Fetching limit violations with (limit reduction ${limitReduction}) ...`
     );
     const url =
         getStudyUrlWithNodeUuid(studyUuid, currentNodeUuid) +
-        '/current-limit-violations?limitReduction=' +
+        '/limit-violations?limitReduction=' +
         limitReduction.toString();
     return backendFetchJson(url);
 }
