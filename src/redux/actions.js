afa66b7c171863ce204e56ad32ef18628128707a
Mahdi Triki <mohamed-mahdi.triki@rte-france.com>
2023-07-12 14:30:17 +0200

    eslint fix
    Signed-off-by: Mahdi Triki <mohamed-mahdi.triki@rte-france.com>

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -666,11 +666,11 @@ export function setComputingStatus(computingType, runningStatus) {
     };
 }
 
-export const SET_AVAILABLE_SERVICES = 'SET_AVAILABLE_SERVICES'
+export const SET_AVAILABLE_SERVICES = 'SET_AVAILABLE_SERVICES';
 
 export const setAvailableServices = (availableServices) => {
     return {
         type: SET_AVAILABLE_SERVICES,
         availableServices: availableServices,
     };
-}
+};
