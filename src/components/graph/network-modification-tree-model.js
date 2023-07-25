81a6c02668f397b45b8f1a8c6ec8f4dd8926c23a
Mahdi Triki <mohamed-mahdi.triki@rte-france.com>
2023-07-25 17:54:35 +0200

    eslint fixes
    Signed-off-by: Mahdi Triki <mohamed-mahdi.triki@rte-france.com>

--- src/components/graph/network-modification-tree-model.js
+++ src/components/graph/network-modification-tree-model.js
@@ -21,8 +21,14 @@ export default class NetworkModificationTreeModel {
 
     addChild(newNode, parentId, insertMode) {
         // Add node
-        const parentIndex = this.treeNodes.findIndex(node => node.id === parentId)
-        this.treeNodes.splice(parentIndex, 0, convertNodetoReactFlowModelNode(newNode, parentId))
+        const parentIndex = this.treeNodes.findIndex(
+            (node) => node.id === parentId
+        );
+        this.treeNodes.splice(
+            parentIndex,
+            0,
+            convertNodetoReactFlowModelNode(newNode, parentId)
+        );
         // Add edge between node and its parent
         this.treeEdges.push({
             id: 'e' + parentId + '-' + newNode.id,
