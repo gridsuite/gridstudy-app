45d012353a900be57d3adfd33f18377378d8d857
sBouzols <sylvain.bouzols@gmail.com>
2023-07-31 15:20:51 +0200

    fix(CustomAGGrid:postSortRows): optimize groupPostSort N² complexity to N.
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/results/shortcircuit/shortcircuit-analysis-result-table.tsx
+++ src/components/results/shortcircuit/shortcircuit-analysis-result-table.tsx
@@ -114,19 +114,28 @@ const ShortCircuitAnalysisResult: FunctionComponent<
         idField: string,
         linkedIdField: string
     ) => {
-        const result: IRowNode[] = [];
-        const idRows = sortedRows.filter((row) => row.data[idField] != null);
-        idRows.forEach((idRow) => {
-            result.push(idRow);
-            result.push(
-                ...sortedRows.filter(
-                    (row) => row.data[linkedIdField] === idRow.data[idField]
-                )
-            );
+        const rowsMap = new Map<string, IRowNode[]>();
+        // first reduce by main resource idField
+        sortedRows.forEach((row) => {
+            if (row.data[idField] != null) {
+                rowsMap.set(row.data[idField], [row]);
+            }
         });
 
-        return result;
+        // then reduce by linked resource linkedIdField
+        let currentRows;
+        sortedRows.forEach((row) => {
+            if (row.data[idField] == null) {
+                currentRows = rowsMap.get(row.data[linkedIdField]);
+                if (currentRows) {
+                    currentRows.push(row);
+                    rowsMap.set(row.data[linkedIdField], currentRows);
+                }
+            }
+        });
+        return [...rowsMap.values()].flat();
     };
+
     const getRowStyle = useCallback(
         (params: RowClassParams) => {
             if (!params?.data?.linkedElementId) {
