354fbdfe8ead5df82dcb252557b8986e195ec26d
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2023-08-10 09:23:54 +0200

    Use of a substations hierarchy for ordering generators in generation dispatch (#1454)
    * substations hierarchy for generators startup
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/services/study/network-modifications.js
+++ src/services/study/network-modifications.js
@@ -156,7 +156,8 @@ export function generationDispatch(
     defaultOutageRate,
     generatorsWithoutOutage,
     generatorsWithFixedActivePower,
-    generatorsFrequencyReserve
+    generatorsFrequencyReserve,
+    substationsGeneratorsOrdering
 ) {
     const body = JSON.stringify({
         type: MODIFICATION_TYPES.GENERATION_DISPATCH.type,
@@ -165,6 +166,7 @@ export function generationDispatch(
         generatorsWithoutOutage: generatorsWithoutOutage,
         generatorsWithFixedSupply: generatorsWithFixedActivePower,
         generatorsFrequencyReserve: generatorsFrequencyReserve,
+        substationsGeneratorsOrdering: substationsGeneratorsOrdering,
     });
 
     let generationDispatchUrl =
