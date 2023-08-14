f6f374ea8e5617ffc052bb1f9d0b4bd0bb0034fe
Thang PHAM <phamthang37@gmail.com>
2023-08-14 20:11:03 +0200

    correct dependencies missing

--- src/components/diagrams/singleLineDiagram/single-line-diagram-content.js
+++ src/components/diagrams/singleLineDiagram/single-line-diagram-content.js
@@ -344,7 +344,7 @@ function SingleLineDiagramContent(props) {
                 equipmentType,
             });
         },
-        []
+        [closeEquipmentMenu]
     );
 
     const handleCloseDynamicSimulationEventDialog = useCallback(() => {
