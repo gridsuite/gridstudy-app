783f84155d38363c85bad88e353dddab4e0dc605
Mahdi Triki <133099132+trikimahdi@users.noreply.github.com>
2023-08-23 17:38:11 +0200

    display only available services (#1419)
    ---------
    
    Signed-off-by: Mahdi Triki <mohamed-mahdi.triki@rte-france.com>

---
+++ src/components/utils/optional-services.ts
@@ -0,0 +1,35 @@
+export enum OptionalServicesNames {
+    SecurityAnalysis = 'SecurityAnalysis',
+    SensitivityAnalysis = 'SensitivityAnalysis',
+    DynamicSimulation = 'DynamicSimulation',
+    ShortCircuit = 'ShortCircuit',
+    VoltageInit = 'VoltageInit',
+}
+export enum OptionalServicesStatus {
+    Up = 'UP',
+    Down = 'DOWN',
+    Pending = 'PENDING',
+}
+
+export interface IOptionalService {
+    name: OptionalServicesNames;
+    status: OptionalServicesStatus;
+}
+export const getOptionalServiceByServerName = (
+    serverName: string
+): OptionalServicesNames | undefined => {
+    switch (serverName) {
+        case 'security-analysis-server':
+            return OptionalServicesNames.SecurityAnalysis;
+        case 'sensitivity-analysis-server':
+            return OptionalServicesNames.SensitivityAnalysis;
+        case 'dynamic-simulation-server':
+            return OptionalServicesNames.DynamicSimulation;
+        case 'shortcircuit-server':
+            return OptionalServicesNames.ShortCircuit;
+        case 'voltage-init-server':
+            return OptionalServicesNames.VoltageInit;
+        default:
+            return;
+    }
+};
