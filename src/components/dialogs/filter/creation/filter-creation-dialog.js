f5215f9e9d21d35426c5905e48cfb1d5ef3a7d45
walid-sahnoun <walid.sahnoun@rte-france.com>
2023-08-24 16:23:51 +0200

    add translation keys
    Signed-off-by: walid-sahnoun <walid.sahnoun@rte-france.com>

--- src/components/dialogs/filter/creation/filter-creation-dialog.js
+++ src/components/dialogs/filter/creation/filter-creation-dialog.js
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import CustomMuiDialog from '../custom-mui-dialog';
 import NameWrapper from '../name-wrapper';
 import {
     RadioInput,
