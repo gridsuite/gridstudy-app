acdaec4d7083fae587f05f73e4af90ead349f4b2
BOUTIER Charly <charly.boutier@rte-france.com>
2023-08-25 14:18:37 +0200

    Emotion migration, makeStyles batch 2.
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/results/dynamicsimulation/dynamic-simulation-result-table.js
+++ src/components/results/dynamicsimulation/dynamic-simulation-result-table.js
@@ -11,14 +11,13 @@ import { Paper, TableCell } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { Lens } from '@mui/icons-material';
 import React from 'react';
-import makeStyles from '@mui/styles/makeStyles';
 import { green, red } from '@mui/material/colors';
 
 /* must be coherent to LoadFlowResult component */
-const useStyles = makeStyles((theme) => ({
+const styles = {
     tablePaper: {
         flexGrow: 1,
-        height: 100,
+        height: '100px',
     },
     cell: {
         display: 'flex',
@@ -35,24 +34,23 @@ const useStyles = makeStyles((theme) => ({
     fail: {
         color: red[500],
     },
-}));
+};
 
 const DynamicSimulationResultTable = ({ result }) => {
     const intl = useIntl();
-    const classes = useStyles();
 
     function StatusCellRender(cellData) {
         const status = cellData.rowData[cellData.dataKey];
-        const color = status === 'CONVERGED' ? classes.succeed : classes.fail;
+        const color = status === 'CONVERGED' ? styles.succeed : styles.fail;
         return (
             <TableCell
                 component={'div'}
                 id={cellData.dataKey}
-                className={classes.cell}
+                sx={styles.cell}
             >
                 <Grid container direction="row" spacing={4} alignItems="center">
                     <Grid item xs={1}>
-                        <Lens fontSize={'medium'} className={color} />
+                        <Lens fontSize={'medium'} sx={color} />
                     </Grid>
                     <Grid item xs={1}>
                         {status}
@@ -64,7 +62,7 @@ const DynamicSimulationResultTable = ({ result }) => {
 
     return (
         result && (
-            <Paper className={classes.tablePaper}>
+            <Paper sx={styles.tablePaper}>
                 {
                     <VirtualizedTable
                         rows={result}
