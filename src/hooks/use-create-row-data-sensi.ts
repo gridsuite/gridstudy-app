ef030f552d03f7c654c6934befccc9b4c67b9fbb
REHILI Ghazwa Ext <rehiligha@gm0winl628.bureau.si.interne>
2023-09-18 07:08:55 +0200

    add copyright

--- src/hooks/use-create-row-data-sensi.ts
+++ src/hooks/use-create-row-data-sensi.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { SELECTED } from '../components/utils/field-constants';
 import { useFieldArray } from 'react-hook-form';
 import { ISensiParameters } from '../components/dialogs/parameters/sensi/columns-definitions';
