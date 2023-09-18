1df023f6e175acd3e4d8f8b228dac5f803f6a148
AAJELLAL <ali.ajellalod@gmail.com>
2023-09-18 17:14:58 +0200

    revert store config
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { createStore, applyMiddleware, compose } from 'redux';
+
+import { createStore } from 'redux';
 import { reducer } from './reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(reducer, composeEnhancers(applyMiddleware()));
+export const store = createStore(reducer);
