66ee5f59f2efea543b3f101ebdf28d20aac712dd
AAJELLAL <ali.ajellalod@gmail.com>
2023-11-06 14:58:27 +0100

    imporve short circuit tension table based on review remarks.
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/components/dialogs/parameters/shortcircuit/short-circuit-tension-table.tsx
+++ src/components/dialogs/parameters/shortcircuit/short-circuit-tension-table.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback, useMemo } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { Table, TableBody, TableCell, TableRow } from '@mui/material';
 import { useIntl } from 'react-intl';
 
@@ -8,59 +8,30 @@ interface TensionTableProps {
 const TensionTable: FunctionComponent<TensionTableProps> = ({ isNominal }) => {
     const intl = useIntl();
 
-    const createRow = useCallback(
-        (
-            name: string,
-            v1: number,
-            v2: number,
-            v3: number,
-            v4: number,
-            v5: number,
-            v6: number,
-            v7: number
-        ) => {
-            return { name, v1, v2, v3, v4, v5, v6, v7 };
-        },
-        []
+    const rows = useMemo(
+        () => [
+            {
+                name: intl.formatMessage({ id: 'shortCircuitNominalVoltage' }),
+                values: [380, 225, 150, 90, 63, 45, 20],
+            },
+            {
+                name: intl.formatMessage({ id: 'shortCircuitInitialTension' }),
+                values: isNominal
+                    ? [400, 225, 150, 90, 63, 45, 20]
+                    : [420, 245, 165, 99, 69.3, 49.5, 22],
+            },
+        ],
+        [isNominal, intl]
     );
-
-    const rows = useMemo(() => {
-        return [
-            createRow(
-                intl.formatMessage({ id: 'shortCircuitNominalVoltage' }),
-                380,
-                225,
-                150,
-                90,
-                63,
-                45,
-                20
-            ),
-            createRow(
-                intl.formatMessage({ id: 'shortCircuitInitialTension' }),
-                isNominal ? 400 : 420,
-                isNominal ? 225 : 245,
-                isNominal ? 150 : 165,
-                isNominal ? 90 : 99,
-                isNominal ? 63 : 69.3,
-                isNominal ? 45 : 49.5,
-                isNominal ? 20 : 22
-            ),
-        ];
-    }, [isNominal, createRow, intl]);
     return (
         <Table>
             <TableBody>
                 {rows.map((row) => (
                     <TableRow key={row.name}>
                         <TableCell>{row.name}</TableCell>
-                        <TableCell>{row.v1}</TableCell>
-                        <TableCell>{row.v2}</TableCell>
-                        <TableCell>{row.v3}</TableCell>
-                        <TableCell>{row.v4}</TableCell>
-                        <TableCell>{row.v5}</TableCell>
-                        <TableCell>{row.v6}</TableCell>
-                        <TableCell>{row.v7}</TableCell>
+                        {row.values.map((value) => (
+                            <TableCell key={value}>{value}</TableCell>
+                        ))}
                     </TableRow>
                 ))}
             </TableBody>
