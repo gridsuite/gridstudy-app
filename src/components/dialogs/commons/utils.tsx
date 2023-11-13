e28b23490415867627c96dc896efe0867d77d31d
YenguiSeddik <101111441+YenguiSeddik@users.noreply.github.com>
2023-11-13 14:42:11 +0100

    Add by formula modification (#1655)
    Signed-off-by: Seddik Yengui <seddik.yengui@rte-france.com>

---
+++ src/components/dialogs/commons/utils.tsx
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export function getIdOrValue(value: { id: string; label: string } | string) {
+    return typeof value !== 'string' ? value?.id : value;
+}
+
+export function getLabelOrValue(value: { id: string; label: string } | string) {
+    return typeof value !== 'string' ? value?.label : value;
+}
