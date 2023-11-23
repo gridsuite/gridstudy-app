384e215bc9f67926a77415dadf46b7bab4727983
Maissa SOUISSI <maissa.souissi@rte-france.com>
2023-11-23 16:28:10 +0100

    fix warning when undefined value
    Signed-off-by: Maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/components/spreadsheet/utils/config-tables.js
+++ src/components/spreadsheet/utils/config-tables.js
@@ -1682,7 +1682,7 @@ export const TABLES_DEFINITIONS = {
                     return {
                         defaultValue:
                             params.data?.activePowerControl
-                                ?.activePowerControlOn,
+                                ?.activePowerControlOn ?? '',
                         gridContext: params.context,
                         gridApi: params.api,
                         colDef: params.colDef,
