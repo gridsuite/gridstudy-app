250010d553b6fe6cd72afa817c4eb37e3c844eb0
jamal-khey <myjamal89@gmail.com>
2023-12-07 11:50:25 +0100

    implement a second solution with calc , using a margin of 100% in the buttom
    Signed-off-by: jamal-khey <myjamal89@gmail.com>

--- src/components/report-viewer/report-viewer.js
+++ src/components/report-viewer/report-viewer.js
@@ -253,13 +253,13 @@ export default function ReportViewer({
 
     return (
         rootReport.current && (
-            <Grid container style={{ height: '99%' }}>
+            <Grid container style={{ height: 'calc(100% - 100px)' }}>
                 <Grid
                     item
                     xs={12}
                     sm={3}
                     style={{
-                        height: '95%',
+                        height: '100%',
                         borderRight: '1px solid rgba(81, 81, 81, 1)',
                     }}
                 >
@@ -286,7 +286,7 @@ export default function ReportViewer({
                         </TreeView>
                     </ReportTreeViewContext.Provider>
                 </Grid>
-                <Grid item xs={12} sm={9} style={{ height: '95%' }}>
+                <Grid item xs={12} sm={9} style={{ height: '100%' }}>
                     <WaitingLoader
                         loading={waitingLoadReport}
                         message={'loadingReport'}
