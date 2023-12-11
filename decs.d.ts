005252a6f25c393425ae635e341d42e4b1b563f5
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2023-12-11 11:45:29 +0100

    Inject informations into about dialog (#1750)
    * Inject informations into about dialog
    * Update commons-ui
    
    ---------
    
    Co-authored-by: Florent MILLOT <millotflo@gmail.com>

--- decs.d.ts
+++ decs.d.ts
@@ -4,6 +4,8 @@ declare module '@gridsuite/commons-ui' {
     import { RadioGroupProps, SxProps, TextFieldProps } from '@mui/material';
     import { CaseImportParameters } from 'services/network-conversion';
 
+    export const DARK_THEME: String, LIGHT_THEME: String;
+
     interface SnackInputs {
         messageTxt?: string;
         messageId?: string;
@@ -189,6 +191,6 @@ declare module '@gridsuite/commons-ui' {
         label?: string;
         formProps?: Omit<CheckboxInputProps, 'disabled'>;
     }
-  
+
     export const CheckboxInput: FunctionComponent<CheckboxInputProps>;
 }
