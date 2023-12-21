d0f076e9b5e870d45a70ef0766a71773c39cc1e3
jamal-khey <myjamal89@gmail.com>
2023-12-21 21:45:26 +0100

    dont use node
    Signed-off-by: jamal-khey <myjamal89@gmail.com>

--- src/hooks/use-computation-notification-count.ts
+++ src/hooks/use-computation-notification-count.ts
@@ -8,7 +8,6 @@ import { useSelector } from 'react-redux';
 import { ReduxState } from '../redux/reducer.type';
 import ComputingType from 'components/computing-status/computing-type';
 import RunningStatus from 'components/utils/running-status';
-import { isNodeBuilt } from 'components/graph/util/model-functions';
 
 /**
  * Custom hook that calculates the number of computation notifications.
@@ -16,10 +15,6 @@ import { isNodeBuilt } from 'components/graph/util/model-functions';
  * @returns the number of computation results accessible.
  */
 export const useComputationResultsCount = () => {
-    // const currentNode = useSelector(
-    //     (state: ReduxState) => state.currentTreeNode
-    // );
-
     const loadFlowStatus = useSelector(
         (state: ReduxState) => state.computingStatus[ComputingType.LOADFLOW]
     );
@@ -59,31 +54,24 @@ export const useComputationResultsCount = () => {
     // see running-status.ts for more details
 
     const loadflowResultPresent =
-        // isNodeBuilt(currentNode) &&
-        (loadFlowStatus === RunningStatus.SUCCEED ||
-            loadFlowStatus === RunningStatus.FAILED); // Can be failed for technical reasons (e.g., server not responding or computation divergence)
+        loadFlowStatus === RunningStatus.SUCCEED ||
+        loadFlowStatus === RunningStatus.FAILED; // Can be failed for technical reasons (e.g., server not responding or computation divergence)
     const saResutPresent =
-        // isNodeBuilt(currentNode) &&
-        (securityAnalysisStatus === RunningStatus.SUCCEED ||
-            securityAnalysisStatus === RunningStatus.FAILED); // Can be failed for technical reasons (e.g., server not responding or computation divergence)
+        securityAnalysisStatus === RunningStatus.SUCCEED ||
+        securityAnalysisStatus === RunningStatus.FAILED; // Can be failed for technical reasons (e.g., server not responding or computation divergence)
     const sensiResultPresent =
-        // isNodeBuilt(currentNode) &&
         sensitivityAnalysisStatus === RunningStatus.SUCCEED;
     const allBusesshortCircuitResultPresent =
-        // isNodeBuilt(currentNode) &&
         allBusesShortCircuitStatus === RunningStatus.SUCCEED;
 
     const oneBusShortCircuitResultPresent =
-        // isNodeBuilt(currentNode) &&
         oneBusallBusesShortCircuitStatus === RunningStatus.SUCCEED;
     const voltageInitResultPresent =
-        // (isNodeBuilt(currentNode) &&
-            voltageInitStatus === RunningStatus.SUCCEED ||
+        voltageInitStatus === RunningStatus.SUCCEED ||
         voltageInitStatus === RunningStatus.FAILED; // Can be failed for technical reasons (e.g., server not responding or computation divergence)
 
     const dynamicSimulationResultPresent =
-        // (isNodeBuilt(currentNode) &&
-            dynamicSimulationStatus === RunningStatus.SUCCEED ||
+        dynamicSimulationStatus === RunningStatus.SUCCEED ||
         dynamicSimulationStatus === RunningStatus.FAILED; // Can be failed for technical reasons (e.g., server not responding or computation divergence)
 
     return [
