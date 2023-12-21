aae73f33962ce552f501347eeb936806ced94acc
David Sartori <33061222+ne0ds@users.noreply.github.com>
2023-12-21 10:28:04 +0100

    Added fields for generators tabular modifications (#1773)
    Signed-off-by: David SARTORI <david.sartori_externe@rte-france.com>

--- src/components/spreadsheet/utils/config-tables.js
+++ src/components/spreadsheet/utils/config-tables.js
@@ -624,7 +624,7 @@ export const TABLES_DEFINITIONS = {
                 getQuickFilterText: excludeFromGlobalFilter,
             },
             {
-                id: 'RatedNominalPower',
+                id: 'ratedNominalPower',
                 field: 'ratedS',
                 numeric: true,
                 filter: 'agNumberColumnFilter',
@@ -1100,7 +1100,7 @@ export const TABLES_DEFINITIONS = {
                 fractionDigits: 0,
             },
             {
-                id: 'Type',
+                id: 'energySource',
                 field: 'energySource',
                 changeCmd: 'equipment.setEnergySource(EnergySource.{})\n',
                 editable: isEditable,
@@ -1280,7 +1280,7 @@ export const TABLES_DEFINITIONS = {
                 },
             },
             {
-                id: 'TargetQ',
+                id: 'reactivePowerSetpoint',
                 field: 'targetQ',
                 numeric: true,
                 filter: 'agNumberColumnFilter',
@@ -1326,7 +1326,7 @@ export const TABLES_DEFINITIONS = {
                 getQuickFilterText: excludeFromGlobalFilter,
             },
             {
-                id: 'TargetV',
+                id: 'voltageSetpoint',
                 field: 'targetV',
                 numeric: true,
                 filter: 'agNumberColumnFilter',
@@ -2156,7 +2156,7 @@ export const TABLES_DEFINITIONS = {
                 getQuickFilterText: excludeFromGlobalFilter,
             },
             {
-                id: 'TargetQ',
+                id: 'reactivePowerSetpoint',
                 field: 'targetQ',
                 numeric: true,
                 filter: 'agNumberColumnFilter',
