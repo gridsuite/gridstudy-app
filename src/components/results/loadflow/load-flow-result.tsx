1b2d46c4d7d6c81c5025aa6301e946df8e74ac87
Rehili Ghazwa <ghazwarhili@gmail.com>
2023-12-26 11:56:09 +0100

    delete unused import

--- src/components/results/loadflow/load-flow-result.tsx
+++ src/components/results/loadflow/load-flow-result.tsx
@@ -45,7 +45,6 @@ import {
     getRows,
     useIntlResultStatusMessages,
 } from '../../utils/aggrid-rows-handler';
-import { CustomAGGrid } from '../../custom-aggrid/custom-aggrid';
 import { fetchLimitViolations } from '../../../services/study';
 import { DefaultCellRenderer } from '../../spreadsheet/utils/cell-renderers';
 import { Box } from '@mui/system';
