08ba4a263a5d5bbb8f7a5aa3c29c5bbab0118037
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-01-29 17:49:21 +0100

    fix: missing copyright
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/results/securityanalysis/security-analysis-export-button.tsx
+++ src/components/results/securityanalysis/security-analysis-export-button.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { useSnackMessage } from '@gridsuite/commons-ui';
 import { ExportButton } from 'components/utils/export-button';
 import { UUID } from 'crypto';
