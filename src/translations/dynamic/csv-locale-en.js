be0bea5258fedd68b68ebf0fc1c2248f11d6ea59
BOUTIER Charly <charly.boutier@rte-france.com>
2024-02-13 16:04:43 +0100

    PR Review
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/translations/dynamic/csv-locale-en.js
+++ src/translations/dynamic/csv-locale-en.js
@@ -7,15 +7,15 @@
 
 const csv_locale_en = {
     // Used in the tabular creation and modification
-    'TabularCreationSkeletonComment/GENERATOR':
+    'TabularCreationSkeletonComment.GENERATOR':
         '#,,HYDRO | NUCLEAR | WIND | THERMAL | SOLAR | OTHER,,,true | false,,TOP | BOTTOM | UNDEFINED,,,,,,,,,true | false,required if voltageRegulationOn is true,,LINE | TWO_WINDINGS_TRANSFORMER | GENERATOR | LOAD | BATTERY | SHUNT_COMPENSATOR | STATIC_VAR_COMPENSATOR | DANGLING_LINE | HVDC_CONVERTER_STATION,,,true | false,required if frequencyRegulation is true,,,,,,,',
-    'TabularModificationSkeletonComment/GENERATOR':
+    'TabularModificationSkeletonComment.GENERATOR':
         '#,HYDRO | NUCLEAR | WIND | THERMAL | SOLAR | OTHER,,,,,,true | false,,true | false,,,,,,',
-    'TabularModificationSkeletonComment/LOAD':
+    'TabularModificationSkeletonComment.LOAD':
         '#,AUXILIARY | FICTITIOUS,,,true | false',
-    'TabularModificationSkeletonComment/SUBSTATION':
+    'TabularModificationSkeletonComment.SUBSTATION':
         '#,2-letter code from ISO 3166-1 standard (FR ES PT IT CH DE BE LU NL GB ...)',
-    'TabularModificationSkeletonComment/SHUNT_COMPENSATOR':
+    'TabularModificationSkeletonComment.SHUNT_COMPENSATOR':
         '#For each shunt compensator it is possible to modify either the maximum reactive power (and the type) or the maximum susceptance. In case of conflicting input the maximum susceptance will be ignored.,,,REACTOR | CAPACITOR,,,true | false',
 };
 
