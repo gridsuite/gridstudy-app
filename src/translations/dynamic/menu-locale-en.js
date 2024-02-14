84c4ce02e58432a6a7d58b4ef7cd7a7efbc0be48
BOUTIER Charly <charly.boutier@rte-france.com>
2024-02-14 15:10:11 +0100

    Alphabetic sort
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/translations/dynamic/menu-locale-en.js
+++ src/translations/dynamic/menu-locale-en.js
@@ -7,23 +7,23 @@
 
 const menu_locale_en = {
     // Used in the right click menus on the map and SLD
-    UnableToLockoutLine: 'Unable to lockout the line',
-    UnableToTripLine: 'Unable to trip the line',
-    UnableToEnergiseOnOneEndLine: 'Unable to energise the line end',
-    UnableToSwitchOnLine: 'Unable to switch on the line',
-    UnableToTrip2WTransformer: 'Unable to trip the transformer',
-    UnableToLockout2WTransformer: 'Unable to lockout the transformer',
-    LockoutLine: 'Lock out the line',
-    TripLine: 'Trip the line',
+    Disconnect2WTransformer: 'Trip the transformer',
+    DisconnectGenerator: 'Trip the generator',
+    DisconnectLine: 'Trip the line',
+    DisconnectLoad: 'Trip the load',
     EnergiseOnOneEndLine: 'Energise on one end ({substation})',
+    Lockout2WTransformer: 'Lock out the transformer',
+    LockoutLine: 'Lock out the line',
+    NodeFaultBus: 'Node fault on bus',
     SwitchOnLine: 'Switch on the line',
     Trip2WTransformer: 'Trip the transformer',
-    Lockout2WTransformer: 'Lock out the transformer',
-    DisconnectLine: 'Trip the line',
-    Disconnect2WTransformer: 'Trip the transformer',
-    DisconnectLoad: 'Trip the load',
-    DisconnectGenerator: 'Trip the generator',
-    NodeFaultBus: 'Node fault on bus',
+    TripLine: 'Trip the line',
+    UnableToEnergiseOnOneEndLine: 'Unable to energise the line end',
+    UnableToLockout2WTransformer: 'Unable to lockout the transformer',
+    UnableToLockoutLine: 'Unable to lockout the line',
+    UnableToSwitchOnLine: 'Unable to switch on the line',
+    UnableToTrip2WTransformer: 'Unable to trip the transformer',
+    UnableToTripLine: 'Unable to trip the line',
 };
 
 export default menu_locale_en;
--- src/translations/dynamic/menu-locale-fr.js
+++ src/translations/dynamic/menu-locale-fr.js
@@ -7,23 +7,23 @@
 
 const menu_locale_fr = {
     // Used in the right click menus on the map and SLD
-    UnableToLockoutLine: 'Impossible de consigner la ligne',
-    UnableToTripLine: 'Impossible de déclencher la ligne',
-    UnableToEnergiseOnOneEndLine: 'Impossible de mettre sous tension à vide',
-    UnableToSwitchOnLine: 'Impossible de mettre la ligne en service',
-    UnableToTrip2WTransformer: 'Impossible de déclencher le transformateur',
-    UnableToLockout2WTransformer: 'Impossible de consigner le transformateur',
-    LockoutLine: 'Consigner la ligne',
-    TripLine: 'Déclencher la ligne',
+    Disconnect2WTransformer: 'Déclencher le transformateur',
+    DisconnectGenerator: 'Déclencher le group',
+    DisconnectLine: 'Déclencher la ligne',
+    DisconnectLoad: 'Déclencher la consommation',
     EnergiseOnOneEndLine: 'Mettre sous tension à vide depuis {substation}',
+    Lockout2WTransformer: 'Consigner le transformateur',
+    LockoutLine: 'Consigner la ligne',
+    NodeFaultBus: 'Court-circuit sur le bus',
     SwitchOnLine: 'Mettre en service la ligne',
     Trip2WTransformer: 'Déclencher le transformateur',
-    Lockout2WTransformer: 'Consigner le transformateur',
-    DisconnectLine: 'Déclencher la ligne',
-    Disconnect2WTransformer: 'Déclencher le transformateur',
-    DisconnectLoad: 'Déclencher la consommation',
-    DisconnectGenerator: 'Déclencher le group',
-    NodeFaultBus: 'Court-circuit sur le bus',
+    TripLine: 'Déclencher la ligne',
+    UnableToEnergiseOnOneEndLine: 'Impossible de mettre sous tension à vide',
+    UnableToLockout2WTransformer: 'Impossible de consigner le transformateur',
+    UnableToLockoutLine: 'Impossible de consigner la ligne',
+    UnableToSwitchOnLine: 'Impossible de mettre la ligne en service',
+    UnableToTrip2WTransformer: 'Impossible de déclencher le transformateur',
+    UnableToTripLine: 'Impossible de déclencher la ligne',
 };
 
 export default menu_locale_fr;
