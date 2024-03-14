404b9f1e4cd5fbefaadcdbd576a10d56e358c26f
sBouzols <sylvain.bouzols@gmail.com>
2024-03-14 16:57:52 +0100

    fix linter
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/graph/menus/modification-list-item.jsx
+++ src/components/graph/menus/modification-list-item.jsx
@@ -7,7 +7,7 @@
 
 import { Checkbox, ListItem, ListItemIcon } from '@mui/material';
 import { useIntl } from 'react-intl';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { OverflowableText } from '@gridsuite/commons-ui';
 import Divider from '@mui/material/Divider';
 import PropTypes from 'prop-types';
