739eb27ab5c34ad2c0bfb383a4142d7bdd908c28
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-03-22 14:34:45 +0100

    PR remark -> comment indent

--- src/components/diagrams/diagram-pane.jsx
+++ src/components/diagrams/diagram-pane.jsx
@@ -1063,11 +1063,10 @@ export function DiagramPane({
                             key={diagramView.svgType + diagramView.id}
                         >
                             {
-                                /*
-                We put a space (a separator) before the first right aligned diagram.
-                This space takes all the remaining space on screen and "pushes" the right aligned
-                diagrams to the right of the screen.
-                */
+                                /* We put a space (a separator) before the first right aligned diagram.
+                               This space takes all the remaining space on screen and "pushes" the right aligned
+                               diagrams to the right of the screen.
+                                */
                                 array[index]?.align === 'right' &&
                                     (index === 0 ||
                                         array[index - 1]?.align === 'left') && (
