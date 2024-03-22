8f9214690f1d83c173430f0003bc834ae01b3481
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-03-22 14:36:50 +0100

    Little tweak

--- src/components/diagrams/diagram-pane.jsx
+++ src/components/diagrams/diagram-pane.jsx
@@ -1065,8 +1065,8 @@ export function DiagramPane({
                             {
                                 /*
                                 We put a space (a separator) before the first right aligned diagram.
-                               This space takes all the remaining space on screen and "pushes" the right aligned
-                               diagrams to the right of the screen.
+                                This space takes all the remaining space on screen and "pushes" the right aligned
+                                diagrams to the right of the screen.
                                 */
                                 array[index]?.align === 'right' &&
                                     (index === 0 ||
