f6ad3ea53aba367652a83a55965e56472d7f8c7d
Florent MILLOT <millotflo@gmail.com>
2024-04-10 12:22:53 +0200

    Updated @gridsuite/commons-ui dependency version from 0.53.0 to 0.54.0 in package-lock.json and package.json files.
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- src/components/dialogs/parameters/common/parameters-creation-dialog.tsx
+++ src/components/dialogs/parameters/common/parameters-creation-dialog.tsx
@@ -11,7 +11,6 @@ import { useSelector } from 'react-redux';
 import { useCallback, useEffect, useState } from 'react';
 import {
     FieldValues,
-    FormProvider,
     useForm,
     UseFormGetValues,
 } from 'react-hook-form';
