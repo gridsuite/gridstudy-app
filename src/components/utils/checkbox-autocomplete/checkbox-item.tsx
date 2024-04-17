bfc80a81daf30a11c8b6681a8a9f82ee9a84426f
Thang PHAM <phamthang37@gmail.com>
2024-04-17 18:51:17 +0200

    ERROR one or more files does not have a valid license header

--- src/components/utils/checkbox-autocomplete/checkbox-item.tsx
+++ src/components/utils/checkbox-autocomplete/checkbox-item.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import { Checkbox, Typography } from '@mui/material';
-import { OverflowableText } from '@gridsuite/commons-ui';
-
 /**
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import * as React from 'react';
+import { Checkbox, Typography } from '@mui/material';
+import { OverflowableText } from '@gridsuite/commons-ui';
 
 export const LISTBOX_PADDING = 8; // px
 
