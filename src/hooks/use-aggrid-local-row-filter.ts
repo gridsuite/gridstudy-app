9242b2fead318ea7cec32fc846fb36df8a036ed0
Anis Touri <anistouri@gmail.com>
2024-04-26 11:26:36 +0200

    Implementing an Epsilon in Digital Filtering (#2056)
    * Implementing an Epsilon in Digital Filtering
    * align tolerance with decimal precision
    * change the concatenation of multiple filters
    ---------
    Signed-off-by: TOURI ANIS <anis-1.touri@rte-france.com>

--- src/hooks/use-aggrid-local-row-filter.ts
+++ src/hooks/use-aggrid-local-row-filter.ts
@@ -16,6 +16,7 @@ import {
     FilterStorePropsType,
     FILTER_DATA_TYPES,
 } from 'components/custom-aggrid/custom-aggrid-header.type';
+import { countDecimalPlaces, truncateNumber } from 'utils/rounding';
 
 interface FilterModel {
     [colId: string]: any;
@@ -42,25 +43,70 @@ export const useAggridLocalRowFilter = (
     );
 
     const formatCustomFiltersForAgGrid = useCallback(
-        (filters: FilterSelectorType[]) => {
-            const agGrifFilterModel: FilterModel = {};
+        (filters: FilterSelectorType[]): FilterModel => {
+            const agGridFilterModel: FilterModel = {};
+            const groupedFilters: { [key: string]: FilterSelectorType[] } = {};
+
+            // Group filters by column
             filters.forEach((filter) => {
-                if (Array.isArray(filter.value)) {
-                    //this case means that the filter is an enum
-                    agGrifFilterModel[filter.column] =
-                        generateEnumFilterModel(filter);
+                if (groupedFilters[filter.column]) {
+                    groupedFilters[filter.column].push(filter);
                 } else {
-                    agGrifFilterModel[filter.column] = {
+                    groupedFilters[filter.column] = [filter];
+                }
+            });
+
+            // Transform groups of filters into a FilterModel
+            Object.keys(groupedFilters).forEach((column) => {
+                const filters = groupedFilters[column];
+                if (filters.length === 1) {
+                    const filter = filters[0];
+                    if (Array.isArray(filter.value)) {
+                        agGridFilterModel[column] =
+                            generateEnumFilterModel(filter);
+                    } else {
+                        agGridFilterModel[column] = {
+                            filterType: filter.dataType,
+                            type: filter.type,
+                            filter:
+                                filter.dataType === FILTER_DATA_TYPES.NUMBER
+                                    ? Number(filter.value)
+                                    : filter.value,
+                        };
+                    }
+                } else {
+                    // Multiple filters on the same column
+                    const conditions = filters.map((filter) => ({
                         filterType: filter.dataType,
                         type: filter.type,
                         filter:
                             filter.dataType === FILTER_DATA_TYPES.NUMBER
                                 ? Number(filter.value)
                                 : filter.value,
+                    }));
+
+                    // Create a combined filter model with 'OR' for all conditions
+                    agGridFilterModel[column] = {
+                        filterType: filters[0].dataType,
+                        operator: 'OR',
+                        // Dynamically add additional conditions
+                        // Each additional condition is added as 'condition1', 'condition2', etc.
+                        ...conditions.reduce(
+                            (
+                                acc: FilterModel,
+                                condition: FilterModel,
+                                index: number
+                            ) => {
+                                acc[`condition${index + 1}`] = condition;
+                                return acc;
+                            },
+                            {}
+                        ),
                     };
                 }
             });
-            return agGrifFilterModel;
+
+            return agGridFilterModel;
         },
         [generateEnumFilterModel]
     );
@@ -90,13 +136,79 @@ export const useAggridLocalRowFilter = (
 
             // If all columns referenced by the filters exist, apply the filters
             if (allColumnsExist) {
+                const filterWithTolerance = addToleranceToFilter(filters);
                 // Format the filters for AG Grid and apply them using setFilterModel
-                const formattedFilters = formatCustomFiltersForAgGrid(filters);
+                const formattedFilters =
+                    formatCustomFiltersForAgGrid(filterWithTolerance);
                 gridRef.current.api.setFilterModel(formattedFilters);
             }
         },
         [formatCustomFiltersForAgGrid, gridRef]
     );
+
+    const addToleranceToFilter = (
+        filters: FilterSelectorType[],
+        tolerance: number = 0.00001
+    ): FilterSelectorType[] => {
+        const decimalPrecision: number = countDecimalPlaces(tolerance);
+        return filters
+            .map((filter): FilterSelectorType | FilterSelectorType[] => {
+                // Attempt to convert filter value to a number if it's a string, otherwise keep it as is
+                let valueAsNumber: number | string[] | null | undefined =
+                    typeof filter.value === 'string'
+                        ? parseFloat(filter.value)
+                        : filter.value;
+                // If the value is successfully converted to a number, apply tolerance adjustments
+                if (typeof valueAsNumber === 'number') {
+                    // Call the truncateNumber function to accurately truncate 'valueAsNumber' to 'decimalPrecision' decimal places.
+                    let truncatedNumber = truncateNumber(
+                        valueAsNumber,
+                        decimalPrecision
+                    );
+                    // Depending on the filter type, adjust the filter value by adding or subtracting the tolerance
+                    switch (filter.type) {
+                        case 'notEqual':
+                            // For 'notEqual', create two conditions: one for greaterThan and one for lessThan
+                            return [
+                                {
+                                    ...filter,
+                                    type: 'greaterThan',
+                                    value: (
+                                        truncatedNumber + tolerance
+                                    ).toFixed(decimalPrecision),
+                                },
+                                {
+                                    ...filter,
+                                    type: 'lessThan',
+                                    value: (
+                                        truncatedNumber - tolerance
+                                    ).toFixed(decimalPrecision),
+                                },
+                            ];
+                        case 'lessThanOrEqual':
+                            // For 'lessThanOrEqual', adjust the value upwards by the tolerance
+                            return {
+                                ...filter,
+                                value: (truncatedNumber + tolerance).toFixed(
+                                    decimalPrecision
+                                ),
+                            };
+                        case 'greaterThanOrEqual':
+                            // For 'greaterThanOrEqual', adjust the value downwards by the tolerance
+                            return {
+                                ...filter,
+                                value: (truncatedNumber - tolerance).toFixed(
+                                    decimalPrecision
+                                ),
+                            };
+                        default:
+                            return filter;
+                    }
+                }
+                return filter;
+            })
+            .flat(); // Flatten the array in case any filters were expanded into multiple conditions
+    };
 
     useEffect(() => {
         setFiltersInAgGrid(filterSelector);
