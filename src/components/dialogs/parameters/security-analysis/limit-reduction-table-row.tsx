6fb00752d0ea10bf886826f2f87b074bc57dac44
Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>
2024-07-22 17:30:47 +0200

    Add tab for limit reductions parameters

---
+++ src/components/dialogs/parameters/security-analysis/limit-reduction-table-row.tsx
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { TableRow } from '@mui/material';
+import React, { FunctionComponent } from 'react';
+import LimitReductionTableCell from './limit-reduction-table-cell';
+import { IColumnsDef } from './columns-definitions';
+
+interface TableRowComponentProps {
+    arrayFormName: string;
+    columnsDefinition: IColumnsDef[];
+    row: Record<'id', string>;
+    index: number;
+}
+
+const LimitReductionTableRow: FunctionComponent<TableRowComponentProps> = ({
+    arrayFormName,
+    columnsDefinition,
+    row,
+    index,
+}) => {
+    return (
+        <TableRow key={row.id}>
+            {columnsDefinition.map((column: IColumnsDef) =>
+                LimitReductionTableCell(arrayFormName, index, column)
+            )}
+        </TableRow>
+    );
+};
+
+export default LimitReductionTableRow;
