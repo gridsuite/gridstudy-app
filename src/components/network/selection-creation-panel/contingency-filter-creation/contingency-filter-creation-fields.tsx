77461cc8a6db27d2146a68bb55ec5f20b9f1eb00
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-08-09 12:36:50 +0200

    fix: copyrights
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/network/selection-creation-panel/contingency-filter-creation/contingency-filter-creation-fields.tsx
+++ src/components/network/selection-creation-panel/contingency-filter-creation/contingency-filter-creation-fields.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { ElementType, FILTER_EQUIPMENTS, FormEquipment, SelectInput } from '@gridsuite/commons-ui';
 import { Grid } from '@mui/material';
 import { EQUIPMENT_TYPES, equipmentTypeToLabel } from 'components/utils/equipment-types';
--- src/components/network/selection-creation-panel/selection-creation-panel-form.tsx
+++ src/components/network/selection-creation-panel/selection-creation-panel-form.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { SelectInput } from '@gridsuite/commons-ui';
 import { Grid } from '@mui/material';
 import { GridSection } from 'components/dialogs/dialogUtils';
