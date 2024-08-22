92eb0fbb7f6a3f7556479de9aeb500eeefc43669
Ayoub LABIDI <117761394+ayolab@users.noreply.github.com>
2024-08-22 10:21:15 +0200

    Adapt code to new report dto (#2210)
    * Adapt code to new report dto
    
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>
    
    * Remove useless reportType query param
    
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>
    
    ---------
    
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>
    Co-authored-by: dbraquart <107846716+dbraquart@users.noreply.github.com>

--- src/services/study/index.js
+++ src/services/study/index.js
@@ -73,15 +73,10 @@ export function fetchParentNodesReport(studyUuid, nodeUuid, nodeOnlyReport, seve
     return backendFetchJson(url);
 }
 
-export function fetchNodeReport(studyUuid, nodeUuid, reportId, severityFilterList, reportType) {
-    console.info('get report for node : ' + nodeUuid + ' in study ' + studyUuid + ' for ' + reportType);
+export function fetchNodeReport(studyUuid, nodeUuid, reportId, severityFilterList) {
+    console.info('get report for node : ' + nodeUuid + ' in study ' + studyUuid);
 
-    let url =
-        getStudyUrlWithNodeUuid(studyUuid, nodeUuid) +
-        '/report?reportId=' +
-        reportId +
-        '&reportType=' +
-        reportType.toString();
+    let url = getStudyUrlWithNodeUuid(studyUuid, nodeUuid) + '/report?reportId=' + reportId;
     if (severityFilterList?.length) {
         url += '&' + getRequestParamFromList(severityFilterList, 'severityLevels');
     }
