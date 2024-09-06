0dca65bd8d5ec8826fde19a78fdb3780d9c5bca8
BOUTIER Charly <charly.boutier@rte-france.com>
2024-09-06 15:07:10 +0200

    lint
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/diagrams/networkAreaDiagram/network-area-diagram-content.jsx
+++ src/components/diagrams/networkAreaDiagram/network-area-diagram-content.jsx
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useLayoutEffect, useMemo, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { RunningStatus } from '../../utils/running-status';
@@ -38,71 +38,74 @@ function NetworkAreaDiagramContent(props) {
      * DIAGRAM CONTENT BUILDING
      */
 
-    const dynamicCssRules = [
-        {
-            cssSelector: '.nad-edge-infos', // data on edges (arrows and values)
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 2200,
-            thresholdStatus: THRESHOLD_STATUS.ABOVE,
-        },
-        {
-            cssSelector: '.nad-label-box', // tooltips linked to nodes
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 3000,
-            thresholdStatus: THRESHOLD_STATUS.ABOVE,
-        },
-        {
-            cssSelector: '.nad-text-edges', // visual link between nodes and their tooltip
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 3000,
-            thresholdStatus: THRESHOLD_STATUS.ABOVE,
-        },
-        {
-            cssSelector: '[class^="nad-vl0to30"], [class*=" nad-vl0to30"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 4000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-        {
-            cssSelector: '[class^="nad-vl30to50"], [class*=" nad-vl30to50"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 4000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-        {
-            cssSelector: '[class^="nad-vl50to70"], [class*=" nad-vl50to70"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 9000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-        {
-            cssSelector: '[class^="nad-vl70to120"], [class*=" nad-vl70to120"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 9000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-        {
-            cssSelector: '[class^="nad-vl120to180"], [class*=" nad-vl120to180"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 12000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-        {
-            cssSelector: '[class^="nad-vl180to300"], [class*=" nad-vl180to300"]',
-            belowThresholdCssDeclaration: { display: 'block' },
-            aboveThresholdCssDeclaration: { display: 'none' },
-            threshold: 20000,
-            thresholdStatus: THRESHOLD_STATUS.BELOW,
-        },
-    ];
+    const dynamicCssRules = useMemo(
+        () => [
+            {
+                cssSelector: '.nad-edge-infos', // data on edges (arrows and values)
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 2200,
+                thresholdStatus: THRESHOLD_STATUS.ABOVE,
+            },
+            {
+                cssSelector: '.nad-label-box', // tooltips linked to nodes
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 3000,
+                thresholdStatus: THRESHOLD_STATUS.ABOVE,
+            },
+            {
+                cssSelector: '.nad-text-edges', // visual link between nodes and their tooltip
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 3000,
+                thresholdStatus: THRESHOLD_STATUS.ABOVE,
+            },
+            {
+                cssSelector: '[class^="nad-vl0to30"], [class*=" nad-vl0to30"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 4000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+            {
+                cssSelector: '[class^="nad-vl30to50"], [class*=" nad-vl30to50"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 4000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+            {
+                cssSelector: '[class^="nad-vl50to70"], [class*=" nad-vl50to70"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 9000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+            {
+                cssSelector: '[class^="nad-vl70to120"], [class*=" nad-vl70to120"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 9000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+            {
+                cssSelector: '[class^="nad-vl120to180"], [class*=" nad-vl120to180"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 12000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+            {
+                cssSelector: '[class^="nad-vl180to300"], [class*=" nad-vl180to300"]',
+                belowThresholdCssDeclaration: { display: 'block' },
+                aboveThresholdCssDeclaration: { display: 'none' },
+                threshold: 20000,
+                thresholdStatus: THRESHOLD_STATUS.BELOW,
+            },
+        ],
+        []
+    );
 
     useLayoutEffect(() => {
         if (props.svg) {
