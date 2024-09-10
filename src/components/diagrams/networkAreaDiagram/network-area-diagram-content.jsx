87b1f89ab7147893745aa728d164c5fa3281d9c0
Charly B <47325354+EstherDarkish@users.noreply.github.com>
2024-09-10 09:21:06 +0200

    Adds the possibility to move nods in a network area diagram, and acti… (#2245)
    * Adds the possibility to move nods in a network area diagram, and activates and customize the level of detail
    
    ---------
    
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/diagrams/networkAreaDiagram/network-area-diagram-content.jsx
+++ src/components/diagrams/networkAreaDiagram/network-area-diagram-content.jsx
@@ -16,11 +16,77 @@ import {
     MAX_WIDTH_NETWORK_AREA_DIAGRAM,
     styles,
 } from '../diagram-common';
-import { NetworkAreaDiagramViewer } from '@powsybl/diagram-viewer';
+import { NetworkAreaDiagramViewer, THRESHOLD_STATUS } from '@powsybl/diagram-viewer';
 import LinearProgress from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
 import { mergeSx } from '../../utils/functions';
 import ComputingType from 'components/computing-status/computing-type';
+
+const dynamicCssRules = [
+    {
+        cssSelector: '.nad-edge-infos', // data on edges (arrows and values)
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 2200,
+        thresholdStatus: THRESHOLD_STATUS.ABOVE,
+    },
+    {
+        cssSelector: '.nad-label-box', // tooltips linked to nodes
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 3000,
+        thresholdStatus: THRESHOLD_STATUS.ABOVE,
+    },
+    {
+        cssSelector: '.nad-text-edges', // visual link between nodes and their tooltip
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 3000,
+        thresholdStatus: THRESHOLD_STATUS.ABOVE,
+    },
+    {
+        cssSelector: '[class^="nad-vl0to30"], [class*=" nad-vl0to30"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 4000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+    {
+        cssSelector: '[class^="nad-vl30to50"], [class*=" nad-vl30to50"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 4000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+    {
+        cssSelector: '[class^="nad-vl50to70"], [class*=" nad-vl50to70"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 9000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+    {
+        cssSelector: '[class^="nad-vl70to120"], [class*=" nad-vl70to120"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 9000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+    {
+        cssSelector: '[class^="nad-vl120to180"], [class*=" nad-vl120to180"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 12000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+    {
+        cssSelector: '[class^="nad-vl180to300"], [class*=" nad-vl180to300"]',
+        belowThresholdCssDeclaration: { display: 'block' },
+        aboveThresholdCssDeclaration: { display: 'none' },
+        threshold: 20000,
+        thresholdStatus: THRESHOLD_STATUS.BELOW,
+    },
+];
 
 function NetworkAreaDiagramContent(props) {
     const { diagramSizeSetter } = props;
@@ -45,9 +111,9 @@ function NetworkAreaDiagramContent(props) {
                 null,
                 null,
                 null,
-                false,
-                false,
-                null
+                true,
+                true,
+                dynamicCssRules
             );
 
             // Update the diagram-pane's list of sizes with the width and height from the backend
