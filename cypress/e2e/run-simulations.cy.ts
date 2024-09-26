64b0bb980f71d20623b22cff8b9ff38e9468ba3f
jamal-khey <myjamal89@gmail.com>
2024-09-26 16:21:41 +0200

    add create and delete node

--- cypress/e2e/run-simulations.cy.ts
+++ cypress/e2e/run-simulations.cy.ts
@@ -16,24 +16,44 @@ describe('run simulations', () => {
         cy.intercept('GET', '**/api/gateway/study/v1/studies/**/nodes/**/geo-data/lines').as('geoData');
         //wait for the geo data to be loaded
         cy.wait('@geoData');
-        //
-        cy.get('button').contains('N1').click();
+        createNode('tmpNodeForLoadflow');
 
-        cy.get('button').contains('N1').rightclick();
-        cy.contains('new node').click();
-
-        cy.contains('in a new branch').click();
-
-        cy.get('button').contains('N2').click();
-        cy.get('[data-testid="EditIcon"]').click();
-
-        cy.get('input[value="N2"]').clear();
-        cy.get('input[value=""]').type('tmpNodeForLoadflow');
-        cy.get('button').contains('Validate').click();
-
-        cy.get('button').contains('tmpNodeForLoadflow').rightclick();
-        cy.contains('Build node').click();
+        deleteNode('tmpNodeForLoadflow');
     });
 
-    it('run a security analysis', () => {});
+    it('run a security analysis', () => {
+        // wait to make sure map is loaded , intercept the request and wait for it to be done
+        // http://localhost:84/api/gateway/study/v1/studies/b38e9420-4e98-4713-8541-510039101fb9/nodes/2619cf5f-16ad-43d2-bf1d-d28f1a95194e/geo-data/lines
+        //
+        cy.intercept('GET', '**/api/gateway/study/v1/studies/**/nodes/**/geo-data/lines').as('geoData');
+        //wait for the geo data to be loaded
+        cy.wait('@geoData');
+        createNode('tmpNodeForSecurityAnalysis');
+
+        deleteNode('tmpNodeForSecurityAnalysis');
+    });
+
 });
+
+function createNode(nodeName: string) {
+    cy.get('button').contains('N1').click();
+    cy.get('button').contains('N1').rightclick();
+    cy.contains('new node').click();
+    cy.contains('in a new branch').click();
+    cy.get('button').contains('N2').click();
+    cy.get('[data-testid="EditIcon"]').click();
+    cy.get('input[value="N2"]').clear();
+    cy.get('input[value=""]').type(nodeName);
+    cy.get('button').contains('Validate').click();
+    cy.get('button').contains(nodeName).rightclick();
+    cy.contains('Build node').click();
+}
+
+function deleteNode(nodeName: string) {
+    cy.get('button').contains(nodeName).rightclick();
+    // cyget('button').contains('Remove node').should('be.visible').and('not.be.disabled').click();
+    // test aria-disabled="false"
+    cy.wait(2000);
+    cy.contains('Remove node').click();
+    cy.get('button').contains('Delete').click();
+}
