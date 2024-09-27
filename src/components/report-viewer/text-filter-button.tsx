af4d34b3688b702f687ba9f2aa49df6e9a210be1
Abdelsalem <abdelsalem.hedhili@rte-france.com>
2024-09-27 14:05:33 +0200

    remove useless import
    Signed-off-by: Abdelsalem <abdelsalem.hedhili@rte-france.com>

--- src/components/report-viewer/text-filter-button.tsx
+++ src/components/report-viewer/text-filter-button.tsx
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React, { ChangeEvent, Dispatch, FunctionComponent, SetStateAction, useState } from 'react';
+import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 import { Box, IconButton, TextField } from '@mui/material';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import { useIntl } from 'react-intl';
