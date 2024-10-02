1f8ad1e83a5ab7801e47d7cd5a7a0d8a21e02aea
sBouzols <sylvain.bouzols@gmail.com>
2024-10-02 18:02:32 +0200

    refactor(useParametersBackend): comment code, remove useless and bad implemented FETCHING_STATUS, use ref where needed to avoid multiple effects proc, manage optionnal backendFetch* props
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/dialogs/parameters/parameters.jsx
+++ src/components/dialogs/parameters/parameters.jsx
@@ -218,12 +218,6 @@ export const TabPanel = (props) => {
 
 const INITIAL_PROVIDERS = {};
 
-const FETCHING_STATUS = {
-    NOT_STARTED: 'not_started',
-    FETCHING: 'fetching',
-    FINISHED: 'finished',
-};
-
 export const useParametersBackend = (
     user,
     type,
@@ -234,7 +228,7 @@ export const useParametersBackend = (
     backendUpdateProvider,
     backendFetchParameters,
     backendUpdateParameters,
-    backendFetchSpecificParameters
+    backendFetchSpecificParametersDescription
 ) => {
     const studyUuid = useSelector((state) => state.studyUuid);
     const studyUpdated = useSelector((state) => state.studyUpdated);
@@ -243,11 +237,16 @@ export const useParametersBackend = (
 
     const providersRef = useRef(INITIAL_PROVIDERS);
     const [provider, setProvider] = useState();
+    const providerRef = useRef(provider);
+    providerRef.current = provider;
 
-    const [fetching, setFetching] = useState(FETCHING_STATUS.NOT_STARTED);
     const [params, setParams] = useState(null);
+    const [specificParamsDescription, setSpecificParamsDescription] = useState(null);
 
-    // since provider is updated seperately, we need to update the params with the new provider
+    const optionalServiceStatusRef = useRef(optionalServiceStatus);
+    optionalServiceStatusRef.current = optionalServiceStatus;
+
+    // since provider is updated seperately sometimes we need to update the params with the new provider
     const currentParams = useMemo(() => {
         if (params && 'provider' in params && provider) {
             return { ...params, provider: provider };
@@ -255,46 +254,30 @@ export const useParametersBackend = (
         return params;
     }, [params, provider]);
 
-    const [specificParamsDescription, setSpecificParamsDescription] = useState(null);
-
-    const backendUpdateParametersCb = useCallback(
-        (studyUuid, newParams, oldParams) => {
-            backendUpdateParameters(studyUuid, newParams).catch((error) => {
-                setParams(oldParams);
+    // PROVIDER UPDATE
+    const updateProvider = useCallback(
+        (newProvider) => {
+            const oldProvider = providerRef.current;
+            setProvider(newProvider); // local state
+            backendUpdateProvider(studyUuid, newProvider).catch((error) => {
+                setProvider(oldProvider);
                 snackError({
                     messageTxt: error.message,
-                    headerId: 'update' + type + 'ParametersError',
+                    headerId: 'update' + type + 'ProviderError',
                 });
             });
         },
-        [backendUpdateParameters, snackError, type]
+        [backendUpdateProvider, studyUuid, snackError, type]
     );
 
-    const debouncedBackendUpdateParameters = useDebounce(backendUpdateParametersCb, 1000);
-
-    const updateProvider = useCallback(
-        (newProvider) => {
-            backendUpdateProvider(studyUuid, newProvider)
-                .then(() => {
-                    setProvider(newProvider);
-                })
-                .catch((error) => {
-                    snackError({
-                        messageTxt: error.message,
-                        headerId: 'update' + type + 'ProviderError',
-                    });
-                });
-        },
-        [type, backendUpdateProvider, studyUuid, snackError]
-    );
-
+    // PROVIDER RESET
     const resetProvider = useCallback(() => {
         backendFetchDefaultProvider()
             .then((defaultProvider) => {
                 const providerNames = Object.keys(providersRef.current);
                 if (providerNames.length > 0) {
                     const newProvider = defaultProvider in providersRef.current ? defaultProvider : providerNames[0];
-                    if (newProvider !== provider) {
+                    if (newProvider !== providerRef.current) {
                         updateProvider(newProvider);
                     }
                 }
@@ -305,20 +288,127 @@ export const useParametersBackend = (
                     headerId: 'fetchDefault' + type + 'ProviderError',
                 });
             });
-    }, [backendFetchDefaultProvider, provider, updateProvider, snackError, type]);
+    }, [backendFetchDefaultProvider, updateProvider, snackError, type]);
+
+    // PROVIDER SYNC
+    const fetchAvailableProviders = useCallback(() => {
+        return backendFetchProviders()
+            .then((providers) => {
+                // we can consider the provider gotten from back will be also used as
+                // a key for translation
+                const providersObj = providers.reduce((obj, v) => {
+                    // TODO keep an array there is no reason for this reduce
+                    obj[v] = v;
+                    return obj;
+                }, {});
+                providersRef.current = providersObj;
+            })
+            .catch((error) => {
+                snackError({
+                    messageTxt: error.message,
+                    headerId: 'fetch' + type + 'ProvidersError',
+                });
+            });
+    }, [backendFetchProviders, snackError, type]);
+
+    const fetchProvider = useCallback(
+        (studyUuid) => {
+            backendFetchProvider?.(studyUuid)
+                .then((newProvider) => {
+                    // if provider is not defined or not among allowed values, it's set to default value
+                    if (newProvider in providersRef.current) {
+                        setProvider(newProvider);
+                    } else {
+                        resetProvider();
+                    }
+                })
+                .catch((error) => {
+                    snackError({
+                        messageTxt: error.message,
+                        headerId: 'fetch' + type + 'ProviderError',
+                    });
+                });
+        },
+        [backendFetchProvider, resetProvider, snackError, type]
+    );
+
+    // We need to fetch available providers when optionalServiceStatus changes
+    // Then fetch saved provider for this study and set it
+    // other dependencies don't change this much
+    useEffect(() => {
+        if (user !== null && studyUuid && optionalServiceStatus === OptionalServicesStatus.Up) {
+            fetchAvailableProviders().then(() => fetchProvider(studyUuid));
+        }
+    }, [fetchAvailableProviders, fetchProvider, optionalServiceStatus, studyUuid, user]);
+
+    // we need to fetch provider when ever a computationParametersUpdated notification received.
+    // use optionalServiceStatusRef here to avoid double effects proc
+    // other dependencies don't change this much
+    useEffect(() => {
+        if (
+            isComputationParametersUpdated(type, studyUpdated) &&
+            studyUuid &&
+            optionalServiceStatusRef.current === OptionalServicesStatus.Up
+        ) {
+            fetchProvider(studyUuid);
+        }
+    }, [fetchProvider, studyUpdated, studyUuid, type]);
+
+    // SPECIFIC PARAMETERS DESCRIPTION
+    const fetchSpecificParametersDescription = useCallback(
+        (studyUuid) => {
+            backendFetchSpecificParametersDescription?.()
+                .then((specificParams) => {
+                    setSpecificParamsDescription(specificParams);
+                })
+                .catch((error) => {
+                    snackError({
+                        messageTxt: error.message,
+                        headerId: 'fetch' + type + 'SpecificParametersError',
+                    });
+                });
+        },
+        [backendFetchSpecificParametersDescription, snackError, type]
+    );
+
+    // We need to fetch specific parameters description when optionalServiceStatus changes
+    // other dependencies don't change this much
+    useEffect(() => {
+        if (studyUuid && optionalServiceStatus === OptionalServicesStatus.Up) {
+            fetchSpecificParametersDescription(studyUuid);
+        }
+    }, [optionalServiceStatus, studyUuid, fetchSpecificParametersDescription]);
+
+    // PARAMETERS UPDATE
+    const backendUpdateParametersCb = useCallback(
+        (studyUuid, newParams, oldParams) => {
+            backendUpdateParameters?.(studyUuid, newParams).catch((error) => {
+                // Restore old local params and provider if update didn't work
+                setParams(oldParams);
+                setProvider(oldParams['provider']);
+                snackError({
+                    messageTxt: error.message,
+                    headerId: 'update' + type + 'ParametersError',
+                });
+            });
+        },
+        [backendUpdateParameters, snackError, type]
+    );
+    const debouncedBackendUpdateParameters = useDebounce(backendUpdateParametersCb, 1000);
 
     const updateParameter = useCallback(
         (newParams) => {
-            if (debouncedBackendUpdateParameters) {
-                let oldParams = { ...currentParams };
-                setParams(newParams);
-                setProvider(newParams['provider']);
-                debouncedBackendUpdateParameters(studyUuid, newParams, oldParams);
-            }
+            const oldParams = { ...currentParams };
+            // Set local states first to components rendering
+            setParams(newParams);
+            setProvider(newParams['provider']);
+            // then send request to save it
+            debouncedBackendUpdateParameters(studyUuid, newParams, oldParams);
         },
         [debouncedBackendUpdateParameters, currentParams, studyUuid]
     );
 
+    // PARAMETERS RESET
     const resetParameters = useCallback(
         (callBack) => {
             return backendUpdateParameters(studyUuid, null)
@@ -328,6 +418,9 @@ export const useParametersBackend = (
                             headerId: 'reset' + type + 'ParametersWarning',
                         });
                     }
+                    // Parameters will be updated after an ComputationParametersUpdated notification
+                    // No need to set local params or provider states here
+                    // because a reset call with a button don't need an intermediate render like for forms
                 })
                 .catch((error) => {
                     snackError({
@@ -339,66 +432,7 @@ export const useParametersBackend = (
         [studyUuid, type, backendUpdateParameters, snackError, snackWarning]
     );
 
-    useEffect(() => {
-        if (user !== null && optionalServiceStatus === OptionalServicesStatus.Up) {
-            setFetching(FETCHING_STATUS.FETCHING);
-            backendFetchProviders()
-                .then((providers) => {
-                    // we can consider the provider gotten from back will be also used as
-                    // a key for translation
-                    const providersObj = providers.reduce(function (obj, v) {
-                        obj[v] = v;
-                        return obj;
-                    }, {});
-                    providersRef.current = providersObj;
-                })
-                .catch((error) => {
-                    snackError({
-                        messageTxt: error.message,
-                        headerId: 'fetch' + type + 'ProvidersError',
-                    });
-                });
-            setFetching(FETCHING_STATUS.FINISHED);
-        }
-    }, [user, backendFetchProviders, type, snackError, optionalServiceStatus]);
-
-    useEffect(() => {
-        if (studyUuid && optionalServiceStatus === OptionalServicesStatus.Up) {
-            if (fetching === FETCHING_STATUS.FINISHED && !provider && backendFetchProvider) {
-                backendFetchProvider(studyUuid)
-                    .then((provider) => {
-                        // if provider is not defined or not among allowed values, it's set to default value
-                        if (provider in providersRef.current) {
-                            setProvider(provider);
-                        } else {
-                            resetProvider();
-                        }
-                    })
-                    .catch((error) => {
-                        snackError({
-                            messageTxt: error.message,
-                            headerId: 'fetch' + type + 'ProviderError',
-                        });
-                    });
-            }
-        }
-    }, [optionalServiceStatus, backendFetchProvider, fetching, provider, resetProvider, snackError, studyUuid, type]);
-
-    useEffect(() => {
-        if (studyUuid && backendFetchSpecificParameters && optionalServiceStatus === OptionalServicesStatus.Up) {
-            backendFetchSpecificParameters()
-                .then((specificParams) => {
-                    setSpecificParamsDescription(specificParams);
-                })
-                .catch((error) => {
-                    snackError({
-                        messageTxt: error.message,
-                        headerId: 'fetch' + type + 'SpecificParametersError',
-                    });
-                });
-        }
-    }, [optionalServiceStatus, backendFetchSpecificParameters, snackError, studyUuid, type]);
-
+    // PARAMETERS SYNC
     const fetchParameters = useCallback(
         (studyUuid) => {
             backendFetchParameters(studyUuid)
@@ -418,23 +452,26 @@ export const useParametersBackend = (
         [backendFetchParameters, type, snackError]
     );
 
+    // We need to fetch parameters when optionalServiceStatus changes
+    // other dependencies don't change this much
     useEffect(() => {
-        if (studyUuid && backendFetchParameters && optionalServiceStatus === OptionalServicesStatus.Up) {
+        if (studyUuid && optionalServiceStatus === OptionalServicesStatus.Up) {
             fetchParameters(studyUuid);
         }
-    }, [optionalServiceStatus, backendFetchParameters, studyUuid, fetchParameters]);
+    }, [optionalServiceStatus, studyUuid, fetchParameters]);
 
-    // we need to call backendFetchParameters when ever a computationParametersUpdated notification received.
+    // we need to fetch parameters when ever a computationParametersUpdated notification received.
+    // use optionalServiceStatusRef here to avoid double effects proc
+    // other dependencies don't change this much
     useEffect(() => {
         if (
             isComputationParametersUpdated(type, studyUpdated) &&
-            backendFetchParameters &&
             studyUuid &&
-            optionalServiceStatus === OptionalServicesStatus.Up
+            optionalServiceStatusRef.current === OptionalServicesStatus.Up
         ) {
             fetchParameters(studyUuid);
         }
-    }, [optionalServiceStatus, backendFetchParameters, fetchParameters, studyUuid, type, studyUpdated]);
+    }, [fetchParameters, studyUuid, type, studyUpdated]);
 
     return [
         providersRef.current,
