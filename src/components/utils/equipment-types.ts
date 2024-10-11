47ad82c98974886796d567deda54f00db747e0d1
souissimai <133104748+souissimai@users.noreply.github.com>
2024-10-11 17:24:33 +0200

    homogenize contingency list (#2311)
    * homogenize contingency list
    ---------
    
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/components/utils/equipment-types.ts
+++ src/components/utils/equipment-types.ts
@@ -42,42 +42,6 @@ export enum EQUIPMENT_TYPES {
     SWITCH = 'SWITCH',
 }
 
-//TODO merge the labels with the equipment types
-export function equipmentTypeToLabel(equipmentType: EQUIPMENT_TYPES) {
-    switch (equipmentType) {
-        case EQUIPMENT_TYPES.GENERATOR:
-            return 'Generators';
-        case EQUIPMENT_TYPES.LOAD:
-            return 'Loads';
-        case EQUIPMENT_TYPES.BATTERY:
-            return 'Batteries';
-        case EQUIPMENT_TYPES.VOLTAGE_LEVEL:
-            return 'VoltageLevels';
-        case EQUIPMENT_TYPES.SUBSTATION:
-            return 'Substations';
-        case EQUIPMENT_TYPES.SHUNT_COMPENSATOR:
-            return 'ShuntCompensators';
-        case EQUIPMENT_TYPES.LINE:
-            return 'Lines';
-        case EQUIPMENT_TYPES.TIE_LINE:
-            return 'TIE_LINE';
-        case EQUIPMENT_TYPES.TWO_WINDINGS_TRANSFORMER:
-            return 'TwoWindingsTransformers';
-        case EQUIPMENT_TYPES.THREE_WINDINGS_TRANSFORMER:
-            return 'ThreeWindingsTransformers';
-        case EQUIPMENT_TYPES.DANGLING_LINE:
-            return 'DanglingLines';
-        case EQUIPMENT_TYPES.LCC_CONVERTER_STATION:
-            return 'LccConverterStations';
-        case EQUIPMENT_TYPES.VSC_CONVERTER_STATION:
-            return 'VscConverterStations';
-        case EQUIPMENT_TYPES.STATIC_VAR_COMPENSATOR:
-            return 'StaticVarCompensators';
-        default:
-            return equipmentType;
-    }
-}
-
 export interface VoltageLevel extends Identifiable {
     nominalV: number;
 }
