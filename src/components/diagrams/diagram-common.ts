bb4be28ee5e06248509807235da3eef6ec45e8be
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-10-15 14:43:40 +0200

    fix: prettier
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/components/diagrams/diagram-common.ts
+++ src/components/diagrams/diagram-common.ts
@@ -112,9 +112,9 @@ export const styles = {
             opacity: NAD_INVALID_LOADFLOW_OPACITY,
         },
         '& .nad-branch-edges .nad-overload .nad-edge-path, .nad-vl-nodes .nad-overvoltage, .nad-vl-nodes .nad-undervoltage':
-        {
-            animation: 'none',
-        },
+            {
+                animation: 'none',
+            },
     },
     paperBorders: (theme: Theme) => ({
         borderLeft: '1px solid ' + theme.palette.action.disabled,
