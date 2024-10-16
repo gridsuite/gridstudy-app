a6743e1edc8bf86c668b7cb5ea77011a06a011bd
YenguiSeddik <101111441+YenguiSeddik@users.noreply.github.com>
2024-10-16 09:50:56 +0200

    Collect network visualizations parameters (#2315)
    Signed-off-by: Seddik Yengui <seddik.yengui@rte-france.com>

---
+++ src/components/dialogs/parameters/network-visualizations/network-visualizations-parameters.tsx
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import React, { FunctionComponent, SyntheticEvent, useCallback, useState } from 'react';
+import { Tab, Tabs } from '@mui/material';
+import { FormattedMessage } from 'react-intl';
+import { TabPanel } from '../parameters';
+import { SingleLineDiagramParameters, useGetAvailableComponentLibraries } from './single-line-diagram-parameters';
+import { NetworkAreaDiagramParameters } from './network-area-diagram-parameters';
+import { MapParameters } from './map-parameters';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../../../redux/reducer';
+
+enum TabValue {
+    MAP = 'Map',
+    SINGLE_LINE_DIAGRAM = 'SingleLineDiagram',
+    NETWORK_AREA_DIAGRAM = 'NetworkAreaDiagram',
+}
+
+export const NetworkVisualizationsParameters: FunctionComponent = () => {
+    const user = useSelector((state: AppState) => state.user);
+    const componentLibraries = useGetAvailableComponentLibraries(user);
+    const [tabValue, setTabValue] = useState(TabValue.MAP);
+
+    const handleTabChange = useCallback((_: SyntheticEvent, newValue: TabValue) => {
+        setTabValue(newValue);
+    }, []);
+
+    return (
+        <>
+            <Tabs value={tabValue} variant="scrollable" onChange={handleTabChange}>
+                <Tab label={<FormattedMessage id={'Map'} />} value={TabValue.MAP} />
+                <Tab label={<FormattedMessage id={'SingleLineDiagram'} />} value={TabValue.SINGLE_LINE_DIAGRAM} />
+                <Tab label={<FormattedMessage id={'NetworkAreaDiagram'} />} value={TabValue.NETWORK_AREA_DIAGRAM} />
+            </Tabs>
+            <TabPanel value={tabValue} index={TabValue.MAP}>
+                <MapParameters />
+            </TabPanel>
+            <TabPanel value={tabValue} index={TabValue.SINGLE_LINE_DIAGRAM}>
+                <SingleLineDiagramParameters componentLibraries={componentLibraries} />
+            </TabPanel>
+            <TabPanel value={tabValue} index={TabValue.NETWORK_AREA_DIAGRAM}>
+                <NetworkAreaDiagramParameters />
+            </TabPanel>
+        </>
+    );
+};
---
+++ src/components/dialogs/parameters/network-visualizations/single-line-diagram-parameters.jsx
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { Grid } from '@mui/material';
+import PropTypes from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
+import { getAvailableComponentLibraries } from '../../../../services/study/index.js';
+import {
+    PARAM_CENTER_LABEL,
+    PARAM_COMPONENT_LIBRARY,
+    PARAM_DIAGONAL_LABEL,
+    PARAM_SUBSTATION_LAYOUT,
+} from '../../../../utils/config-params.js';
+import { LineSeparator } from '../../dialogUtils.jsx';
+import { styles } from '../parameters.jsx';
+import { SubstationLayout } from '../../../diagrams/diagram-common';
+import ParameterLineDropdown from '../widget/parameter-line-dropdown';
+import ParameterLineSwitch from '../widget/parameter-line-switch';
+
+export const useGetAvailableComponentLibraries = (user) => {
+    const [componentLibraries, setComponentLibraries] = useState([]);
+
+    useEffect(() => {
+        if (user !== null) {
+            getAvailableComponentLibraries().then((libraries) => {
+                if (libraries != null) {
+                    setComponentLibraries(libraries);
+                }
+            });
+        }
+    }, [user]);
+
+    return componentLibraries;
+};
+
+export const SingleLineDiagramParameters = ({ componentLibraries }) => {
+    const componentLibsRenderCache = useMemo(
+        () => Object.fromEntries(componentLibraries.filter(Boolean).map((libLabel) => [libLabel, libLabel])),
+        [componentLibraries]
+    );
+
+    return (
+        <Grid xl={6} container spacing={1} sx={styles.scrollableGrid} marginTop={-3} justifyContent={'space-between'}>
+            <ParameterLineSwitch paramNameId={PARAM_DIAGONAL_LABEL} label="diagonalLabel" />
+            <LineSeparator />
+            <ParameterLineSwitch paramNameId={PARAM_CENTER_LABEL} label="centerLabel" />
+            <LineSeparator />
+            <ParameterLineDropdown
+                paramNameId={PARAM_SUBSTATION_LAYOUT}
+                labelTitle="SubstationLayout"
+                labelValue="substation-layout-select-label"
+                defaultValueIfNull={true}
+                values={{
+                    [SubstationLayout.HORIZONTAL]: 'HorizontalSubstationLayout',
+                    [SubstationLayout.VERTICAL]: 'VerticalSubstationLayout',
+                    // the following layouts are not yet supported
+                    //[SubstationLayout.SMART]: 'SmartSubstationLayout',
+                    //[SubstationLayout.SMARTHORIZONTALCOMPACTION]:
+                    //'SmartWithHorizontalCompactionSubstationLayout',
+                    //[SubstationLayout.SMARTVERTICALCOMPACTION]:
+                    //'SmartWithVerticalCompactionSubstationLayout',
+                }}
+            />
+            <LineSeparator />
+            <ParameterLineDropdown
+                paramNameId={PARAM_COMPONENT_LIBRARY}
+                labelTitle="ComponentLibrary"
+                labelValue="component-library-select-label"
+                values={componentLibsRenderCache}
+            />
+        </Grid>
+    );
+};
+
+SingleLineDiagramParameters.propTypes = {
+    componentLibraries: PropTypes.arrayOf(PropTypes.string),
+};
