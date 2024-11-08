42f8516e13a017f02535d5d81ba37494f1ca648d
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-11-08 14:26:24 +0100

    fix grid section component

--- src/components/dialogs/commons/grid-section.tsx
+++ src/components/dialogs/commons/grid-section.tsx
@@ -4,33 +4,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { Grid, styled } from '@mui/material';
+import { Box, Grid, SxProps, Theme } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
-import { ComponentProps, createElement, CSSProperties } from 'react';
 
 export interface GridSectionProps {
     title: string;
-    heading?: string;
+    heading?: 1 | 2 | 3 | 4 | 5 | 6;
     size?: number;
-    customStyle?: CSSProperties;
+    customStyle?: SxProps<Theme>;
 }
 
-const createCustomTag = (heading: string, customStyle: CSSProperties) => {
-    const StyledComponent = styled('div')(() => ({
-        ...customStyle,
-    }));
-
-    return (props: ComponentProps<'div'>) => createElement(`h${heading}`, { ...props, className: StyledComponent });
-};
-
-export default function GridSection({ title, heading = '3', size = 12, customStyle }: Readonly<GridSectionProps>) {
-    const CustomTag = createCustomTag(heading, customStyle ?? {});
+export default function GridSection({ title, heading = 3, size = 12, customStyle }: Readonly<GridSectionProps>) {
     return (
         <Grid container spacing={2}>
             <Grid item xs={size}>
-                <CustomTag>
+                <Box sx={customStyle} component={`h${heading}`}>
                     <FormattedMessage id={title} />
-                </CustomTag>
+                </Box>
             </Grid>
         </Grid>
     );
