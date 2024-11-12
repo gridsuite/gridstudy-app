8b814f339ae00a11c0d4e0e7e42eb06b3dc20e70
Ghazoua Rehili <ghazwarhili@gmail.com>
2024-11-12 18:40:35 +0100

    migration TS load creation form (#2357)
    Signed-off-by: REHILI Ghazwa <ghazwarhili@gmail.com>

--- src/components/dialogs/network-modifications/load/creation/load-creation-form.tsx
+++ src/components/dialogs/network-modifications/load/creation/load-creation-form.tsx
@@ -13,10 +13,17 @@ import { FloatInput, SelectInput, TextInput } from '@gridsuite/commons-ui';
 import { ConnectivityForm } from '../../../connectivity/connectivity-form';
 import PropertiesForm from '../../common/properties/properties-form';
 import useVoltageLevelsListInfos from '../../../../../hooks/use-voltage-levels-list-infos';
+import { UUID } from 'crypto';
+import { CurrentTreeNode } from '../../../../../redux/reducer';
 import GridItem from '../../../commons/grid-item';
 import GridSection from '../../../commons/grid-section';
 
-const LoadCreationForm = ({ currentNode, studyUuid }) => {
+export interface LoadCreationFormProps {
+    studyUuid: UUID;
+    currentNode: CurrentTreeNode;
+}
+
+export default function LoadCreationForm({ studyUuid, currentNode }: Readonly<LoadCreationFormProps>) {
     const voltageLevelOptions = useVoltageLevelsListInfos(studyUuid, currentNode?.id);
 
     const loadIdField = (
@@ -46,6 +53,7 @@ const LoadCreationForm = ({ currentNode, studyUuid }) => {
             withPosition={true}
             studyUuid={studyUuid}
             currentNode={currentNode}
+            previousValues={undefined}
         />
     );
 
@@ -58,18 +66,15 @@ const LoadCreationForm = ({ currentNode, studyUuid }) => {
             </Grid>
             <GridSection title="Connectivity" />
             <Grid container spacing={2}>
-                <GridItem field={connectivityForm} size={12} />
+                <GridItem size={12}>{connectivityForm}</GridItem>
             </Grid>
             <GridSection title="Setpoints" />
             <Grid container spacing={2}>
                 <GridItem size={4}>{activePowerField}</GridItem>
-                <GridItem size={4}>{loadNameField}</GridItem>
                 <GridItem size={4}>{reactivePowerField}</GridItem>
-                <GridItem size={4}>{loadNameField}</GridItem>
+                <GridItem size={'auto'}></GridItem>
             </Grid>
             <PropertiesForm networkElementType={'load'} />
         </>
     );
-};
-
-export default LoadCreationForm;
+}
