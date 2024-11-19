f25112d4a160e81c6a6cf5a666e10385cce30cde
Ayoub LABIDI <117761394+ayolab@users.noreply.github.com>
2024-11-19 16:36:13 +0100

    Update custom columns name constraint (#2402)
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>
    Co-authored-by: David BRAQUART <david.braquart@rte-france.com>

--- src/translations/spreadsheet-en.ts
+++ src/translations/spreadsheet-en.ts
@@ -10,6 +10,7 @@ const spreadsheetEn = {
     'spreadsheet/custom_column/column_name': 'Column name',
     'spreadsheet/custom_column/column_content': 'Column content',
     'spreadsheet/custom_column/column_content_tooltip': `Column content is described with variable names (in order to reference grid data) and operators provided by MathJS library (in order to transform grid data). Example: maxP - p in order to display active power reserve within the generator spreadsheet`,
+    'spreadsheet/custom_column/error/not_unique': 'Column names must be unique',
     'spreadsheet/custom_column/save_columns': 'Save',
     'spreadsheet/custom_column/save_dialog_title': 'Save a spreadsheet model',
     'spreadsheet/custom_column/save_error_message': 'Spreadsheet model creation error',
