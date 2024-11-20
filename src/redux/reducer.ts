0758ede03035f99449d4442dc8d95b303390d7e6
BOUTIER Charly <charly.boutier@rte-france.com>
2024-11-20 10:12:08 +0100

    PR Review
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -637,7 +637,7 @@ const initialState: AppState = {
     centerOnSubstation: null,
     notificationIdList: [],
     isModificationsInProgress: false,
-    studyDisplayMode: StudyDisplayMode.TREE,
+    studyDisplayMode: StudyDisplayMode.TREE, // TODO Revert to HYBRID before merge
     diagramStates: [],
     nadNodeMovements: [],
     reloadMap: true,
