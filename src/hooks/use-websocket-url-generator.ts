c3787d2e33f7118b988f98f1caef0d044d9a6a37
sBouzols <sylvain.bouzols@gmail.com>
2024-11-22 10:30:15 +0100

    add license headers
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/hooks/use-websocket-url-generator.ts
+++ src/hooks/use-websocket-url-generator.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { WS_URL_KEYS } from 'components/utils/websocket-utils';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
