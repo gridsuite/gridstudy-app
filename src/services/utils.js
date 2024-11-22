3cb2c41d70d8672f4a496627aabdadf34a1adfbc
sBouzols <sylvain.bouzols@gmail.com>
2024-11-22 16:26:08 +0100

    revert getWsBase change
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/services/utils.js
+++ src/services/utils.js
@@ -14,11 +14,7 @@ export const FetchStatus = {
     RUNNING: 'RUNNING',
 };
 
-export const getWsBase = () =>
-    document.baseURI
-        .replace(/^http:\/\//, 'ws://')
-        .replace(/^https:\/\//, 'wss://')
-        .replace(/:3004/, ':84');
+export const getWsBase = () => document.baseURI.replace(/^http:\/\//, 'ws://').replace(/^https:\/\//, 'wss://');
 
 export const getRequestParamFromList = (params, paramName) => {
     return new URLSearchParams(params?.length ? params.map((param) => [paramName, param]) : []);
