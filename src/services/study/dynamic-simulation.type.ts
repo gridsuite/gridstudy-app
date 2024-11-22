d0d2328323f541138b8c898dce4818c9595beb7f
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-11-22 10:56:23 +0100

    fix: licence misplaced
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/services/study/dynamic-simulation.type.ts
+++ src/services/study/dynamic-simulation.type.ts
@@ -1,11 +1,11 @@
-import { UUID } from 'crypto';
-
 /**
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+import { UUID } from 'crypto';
+
 export enum SolverTypeInfos {
     IDA = 'IDA',
     SIM = 'SIM',
