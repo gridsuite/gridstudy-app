26740f85c42b65aed6bca37296d80dc7d235fb7e
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-12-11 12:11:51 +0100

    Fix

--- src/utils/report/report-tree.mapper.ts
+++ src/utils/report/report-tree.mapper.ts
@@ -5,19 +5,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { Report, ReportTree, ReportType } from './report.type';
-import { getHighestSeverity } from './report-severity';
+import { Report, ReportSeverity, ReportTree, ReportType } from './report.type';
 import { GLOBAL_REPORT_NODE_LABEL } from './report.constant';
+import { REPORT_SEVERITY } from './report-severity';
 
 export function mapReportsTree(report: Report, reportType?: ReportType): ReportTree {
-    const severityList = report.severity || report.subReports.flatMap((subReport) => subReport.severity);
     return {
         type: reportType ?? (report.message === GLOBAL_REPORT_NODE_LABEL ? ReportType.GLOBAL : ReportType.NODE),
         id: report.id,
         message: report.message,
         parentId: report.parentId,
-        severity: severityList,
-        highestSeverity: getHighestSeverity(severityList),
+        severity: report.severity,
+        highestSeverity: Object.values(REPORT_SEVERITY)
+            .filter((s) => report.severity === s.name)
+            .reduce((p: ReportSeverity, c: ReportSeverity) => (c.level > p.level ? c : p), REPORT_SEVERITY.UNKNOWN),
         subReports: report.subReports
             .filter((subReport) => subReport.subReports.length > 0 || subReport.id)
             .map((subReport) => mapReportsTree(subReport, ReportType.NODE)),
