ab90e6ce73958c8d83b568d0599b10630e63ea47
Joris Mancini <joris.mancini_externe@rte-france.com>
2024-12-17 16:39:24 +0100

    fix: eslint
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/components/report-viewer/report-viewer.tsx
+++ src/components/report-viewer/report-viewer.tsx
@@ -9,7 +9,14 @@ import Grid from '@mui/material/Grid';
 import LogTable from './log-table';
 import { mapReportsTree } from '../../utils/report/report-tree.mapper';
 import { useDispatch } from 'react-redux';
-import { Report, ReportLog, ReportTree, ReportTree as ReportTreeType, ReportType, SeverityLevel } from 'utils/report/report.type';
+import {
+    Report,
+    ReportLog,
+    ReportTree,
+    ReportTree as ReportTreeType,
+    ReportType,
+    SeverityLevel,
+} from 'utils/report/report.type';
 import { VirtualizedTreeview } from './virtualized-treeview';
 import Label from '@mui/icons-material/Label';
 import { ReportItem } from './treeview-item';
