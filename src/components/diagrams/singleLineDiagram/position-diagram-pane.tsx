1aa4cd24f397e48f49096898bb26e9f9d45cda70
dbraquart <107846716+dbraquart@users.noreply.github.com>
2024-12-19 15:06:35 +0100

    Use spreadsheet-config-server to store network visualization parameters per study (#2449)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/diagrams/singleLineDiagram/position-diagram-pane.tsx
+++ src/components/diagrams/singleLineDiagram/position-diagram-pane.tsx
@@ -8,13 +8,7 @@
 import Dialog from '@mui/material/Dialog';
 import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import {
-    PARAM_CENTER_LABEL,
-    PARAM_COMPONENT_LIBRARY,
-    PARAM_DIAGONAL_LABEL,
-    PARAM_LANGUAGE,
-    PARAM_USE_NAME,
-} from '../../../utils/config-params';
+import { PARAM_LANGUAGE, PARAM_USE_NAME } from '../../../utils/config-params';
 import PositionDiagram from './position-diagram';
 import { SLD_DISPLAY_MODE } from '../../network/constants';
 import { DiagramType } from '../diagram-common';
@@ -39,10 +33,8 @@ const PositionDiagramPane: FC<PositionDiagramPaneProps> = ({
     studyUuid,
 }) => {
     const useName = useSelector((state: AppState) => state[PARAM_USE_NAME]);
-    const centerName = useSelector((state: AppState) => state[PARAM_CENTER_LABEL]);
-    const diagonalName = useSelector((state: AppState) => state[PARAM_DIAGONAL_LABEL]);
-    const componentLibrary = useSelector((state: AppState) => state[PARAM_COMPONENT_LIBRARY]);
     const language = useSelector((state: AppState) => state[PARAM_LANGUAGE]);
+    const networkVisuParams = useSelector((state: AppState) => state.networkVisualizationsParameters);
 
     const [svgUrl, setSvgUrl] = useState<string | null>(null);
     const handleClose = () => {
@@ -57,13 +49,22 @@ const PositionDiagramPane: FC<PositionDiagramPaneProps> = ({
                 currentNodeUuid,
                 voltageLevelId?.id,
                 useName,
-                centerName,
-                diagonalName,
-                componentLibrary,
+                networkVisuParams.singleLineDiagramParameters.centerLabel,
+                networkVisuParams.singleLineDiagramParameters.diagonalLabel,
+                networkVisuParams.singleLineDiagramParameters.componentLibrary,
                 SLD_DISPLAY_MODE.FEEDER_POSITION,
                 language
             ),
-        [studyUuid, currentNodeUuid, voltageLevelId?.id, useName, centerName, diagonalName, componentLibrary, language]
+        [
+            studyUuid,
+            currentNodeUuid,
+            voltageLevelId?.id,
+            useName,
+            networkVisuParams.singleLineDiagramParameters.centerLabel,
+            networkVisuParams.singleLineDiagramParameters.diagonalLabel,
+            networkVisuParams.singleLineDiagramParameters.componentLibrary,
+            language,
+        ]
     );
 
     useEffect(() => {
