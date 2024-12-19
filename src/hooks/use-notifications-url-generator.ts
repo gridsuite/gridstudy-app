ed488cc84538983fbf3d55c9507bed79496c89b0
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2024-12-19 18:58:26 +0100

    unnecessary callback

--- src/hooks/use-notifications-url-generator.ts
+++ src/hooks/use-notifications-url-generator.ts
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 import { NOTIFICATIONS_URL_KEYS, PREFIX_CONFIG_NOTIFICATION_WS } from '../components/utils/notificationsProvider-utils';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { type AppState } from '../redux/reducer';
 import { getUrlWithToken, getWsBase } from '../services/utils';
@@ -15,14 +15,6 @@ export default function useNotificationsUrlGenerator() {
     // The websocket API doesn't allow relative urls
     const wsBase = getWsBase();
     const tokenId = useSelector((state: AppState) => state.user?.id_token);
-    // Add params to Url
-    const urlParams = useCallback((mapper: Record<string, string>) => {
-        const usp = new URLSearchParams();
-        Object.entries(mapper).forEach(([key, value]) => {
-            usp.append(key, value);
-        });
-        return usp;
-    }, []);
 
     const urlMapper = useMemo(() => {
         // return a mapper with NOTIFICATIONS_URL_KEYS and undefined value if URL is not yet buildable (tokenId)
@@ -30,11 +22,11 @@ export default function useNotificationsUrlGenerator() {
         let mapper: Object = {
             [NOTIFICATIONS_URL_KEYS.CONFIG]: tokenId
                 ? getUrlWithToken(
-                      `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${urlParams({ appName: APP_NAME })}`
+                      `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${new URLSearchParams({ appName: APP_NAME })}`
                   )
                 : undefined,
         };
         return mapper;
-    }, [wsBase, urlParams, tokenId]);
+    }, [wsBase, tokenId]);
     return urlMapper;
 };
