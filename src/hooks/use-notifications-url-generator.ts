c4d6e1fae3dd55e8d18b5a3427396237171607e1
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-12-20 15:39:27 +0100

    review

--- src/hooks/use-notifications-url-generator.ts
+++ src/hooks/use-notifications-url-generator.ts
@@ -4,14 +4,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { NOTIFICATIONS_URL_KEYS, PREFIX_CONFIG_NOTIFICATION_WS } from '../components/utils/notificationsProvider-utils';
+import { NOTIFICATIONS_URL_KEYS, PREFIX_CONFIG_NOTIFICATION_WS } from 'components/utils/notificationsProvider-utils';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { type AppState } from '../redux/reducer';
-import { getUrlWithToken, getWsBase } from '../services/utils';
-import { APP_NAME } from '../utils/config-params';
+import { type AppState } from 'redux/reducer';
+import { getUrlWithToken, getWsBase } from 'services/utils';
+import { APP_NAME } from 'utils/config-params';
 
-export default function useNotificationsUrlGenerator() {
+const useNotificationsUrlGenerator = (): Record<NOTIFICATIONS_URL_KEYS, string | undefined> => {
     // The websocket API doesn't allow relative urls
     const wsBase = getWsBase();
     const tokenId = useSelector((state: AppState) => state.user?.id_token);
@@ -19,16 +19,17 @@ export default function useNotificationsUrlGenerator() {
     // return a mapper with NOTIFICATIONS_URL_KEYS and undefined value if URL is not yet buildable (tokenId)
     // it will be used to register listeners as soon as possible.
     return useMemo(
-        () =>
-            ({
-                [NOTIFICATIONS_URL_KEYS.CONFIG]: tokenId
-                    ? getUrlWithToken(
-                          `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${new URLSearchParams({
-                              appName: APP_NAME,
-                          })}`
-                      )
-                    : undefined,
-            } satisfies Record<NOTIFICATIONS_URL_KEYS, string | undefined>),
+        () => ({
+            [NOTIFICATIONS_URL_KEYS.CONFIG]: tokenId
+                ? getUrlWithToken(
+                      `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${new URLSearchParams({
+                          appName: APP_NAME,
+                      })}`
+                  )
+                : undefined,
+        }),
         [wsBase, tokenId]
     );
-}
+};
+
+export default useNotificationsUrlGenerator;
