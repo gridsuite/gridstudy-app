eed0b3a15919b37e902a93a4c6c356260c8b2525
achour94 <berrahmaachour@gmail.com>
2024-12-30 15:56:05 +0100

    Use of library react-virtualized-auto-sizer for AutoSizer
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,6 @@ export default defineConfig((config) => ({
             lintOnStart: true,
         }),
         svgr(), // works on every import with the pattern "**/*.svg?react"
-        reactVirtualized(),
         tsconfigPaths(), // to resolve absolute path via tsconfig cf https://stackoverflow.com/a/68250175/5092999
     ],
     base: './',
@@ -49,28 +48,3 @@ export default defineConfig((config) => ({
     },
 }));
 
-// Workaround for react-virtualized with vite
-// https://github.com/bvaughn/react-virtualized/issues/1632#issuecomment-1483966063
-function reactVirtualized(): PluginOption {
-    const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
-    return {
-        name: 'flat:react-virtualized',
-        // Note: we cannot use the `transform` hook here
-        //       because libraries are pre-bundled in vite directly,
-        //       plugins aren't able to hack that step currently.
-        //       so instead we manually edit the file in node_modules.
-        //       all we need is to find the timing before pre-bundling.
-        configResolved: async () => {
-            const require = createRequire(import.meta.url);
-            const reactVirtualizedPath = require.resolve('react-virtualized');
-            const { pathname: reactVirtualizedFilePath } = new url.URL(reactVirtualizedPath, import.meta.url);
-            const file = reactVirtualizedFilePath.replace(
-                path.join('dist', 'commonjs', 'index.js'),
-                path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js')
-            );
-            const code = await fs.readFile(file, 'utf-8');
-            const modified = code.replace(WRONG_CODE, '');
-            await fs.writeFile(file, modified);
-        },
-    };
-}
