8f5cffe005ec487b6dfa717775ecfe512d8a59fb
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-01-09 17:03:45 +0100

    Copyright
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/dialogs/limits/operational-limits-groups-tabs.tsx
+++ src/components/dialogs/limits/operational-limits-groups-tabs.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { Box, Tab, Tabs, TextField } from '@mui/material';
 import { OperationalLimitsGroup } from './limits-type';
 import IconButton from '@mui/material/IconButton';
