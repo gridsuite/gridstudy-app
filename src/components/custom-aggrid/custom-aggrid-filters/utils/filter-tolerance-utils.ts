4b9528fd6526c57d8840e406316c0b85399994aa
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-01-20 13:07:31 +0100

    fix: add copyrights
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/components/custom-aggrid/custom-aggrid-filters/utils/filter-tolerance-utils.ts
+++ src/components/custom-aggrid/custom-aggrid-filters/utils/filter-tolerance-utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { isNumber } from 'mathjs';
 import { countDecimalPlaces, countDecimalPlacesFromString } from '../../../../utils/rounding';
 import { FILTER_NUMBER_COMPARATORS, UNDISPLAYED_FILTER_NUMBER_COMPARATORS } from '../../custom-aggrid-header.type';
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export const isStringOrNonEmptyArray = (value: unknown): value is string | unknown[] => {
     if (typeof value === 'string' && value.length > 0) {
         return true;
