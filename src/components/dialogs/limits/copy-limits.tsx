6eedfafad60d67d70cc670e95976fced7ce73b0e
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-01-21 13:50:03 +0100

    copyright
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/dialogs/limits/copy-limits.tsx
+++ src/components/dialogs/limits/copy-limits.tsx
@@ -1,6 +1,13 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { LIMITS, OPERATIONAL_LIMITS_GROUPS_1, OPERATIONAL_LIMITS_GROUPS_2 } from '../../utils/field-constants';
 import React, { useCallback } from 'react';
-import { useFieldArray, useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import { Box, Button } from '@mui/material';
 import KeyboardDoubleArrowRight from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowLeft from '@mui/icons-material/KeyboardDoubleArrowLeft';
@@ -13,27 +20,21 @@ export interface CopyLimitsProps {
 }
 
 export function CopyLimits({ id = LIMITS, indexSelectedLimitSet1, indexSelectedLimitSet2 }: Readonly<CopyLimitsProps>) {
-    const { getValues } = useFormContext();
-    const { update: updateLimitsGroups1 } = useFieldArray({
-        name: `${id}.${OPERATIONAL_LIMITS_GROUPS_1}`,
-    });
-    const { update: updateLimitsGroups2 } = useFieldArray({
-        name: `${id}.${OPERATIONAL_LIMITS_GROUPS_2}`,
-    });
+    const { setValue, getValues } = useFormContext();
 
     const copyToRight = useCallback(() => {
-        updateLimitsGroups2(
-            indexSelectedLimitSet2,
+        setValue(
+            `${id}.${OPERATIONAL_LIMITS_GROUPS_2}[${indexSelectedLimitSet2}]`,
             getValues(`${id}.${OPERATIONAL_LIMITS_GROUPS_1}[${indexSelectedLimitSet1}]`)
         );
-    }, [id, getValues, updateLimitsGroups2, indexSelectedLimitSet1, indexSelectedLimitSet2]);
+    }, [id, setValue, getValues, indexSelectedLimitSet1, indexSelectedLimitSet2]);
 
     const copyToLeft = useCallback(() => {
-        updateLimitsGroups1(
-            indexSelectedLimitSet1,
+        setValue(
+            `${id}.${OPERATIONAL_LIMITS_GROUPS_1}[${indexSelectedLimitSet1}]`,
             getValues(`${id}.${OPERATIONAL_LIMITS_GROUPS_2}[${indexSelectedLimitSet2}]`)
         );
-    }, [id, getValues, updateLimitsGroups1, indexSelectedLimitSet1, indexSelectedLimitSet2]);
+    }, [id, setValue, getValues, indexSelectedLimitSet1, indexSelectedLimitSet2]);
 
     return (
         <>
