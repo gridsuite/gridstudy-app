89f5b103a3c3fa4796317141706a3c020be8bea1
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-01-27 15:48:24 +0100

    remove duplicate prop
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/custom-aggrid/custom-aggrid-filters/custom-aggrid-autocomplete-filter.tsx
+++ src/components/custom-aggrid/custom-aggrid-filters/custom-aggrid-autocomplete-filter.tsx
@@ -4,17 +4,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import React, { FunctionComponent, SyntheticEvent, useMemo } from 'react';
+import React, { FunctionComponent, SyntheticEvent } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 import { useIntl } from 'react-intl';
 import { useCustomAggridFilter } from './hooks/use-custom-aggrid-filter';
-import { CustomAggridFilterParams, FILTER_TEXT_COMPARATORS, FilterEnumsType } from '../custom-aggrid-header.type';
+import { CustomAggridFilterParams, FILTER_TEXT_COMPARATORS } from '../custom-aggrid-header.type';
 import { isNonEmptyStringOrArray } from '../../../utils/types-utils';
 
 export interface CustomAggridAutocompleteFilterParams extends CustomAggridFilterParams {
-    filterEnums?: FilterEnumsType;
-    getEnumLabel?: (value: string) => string; // Used for translation of enum values in the filter
-    customOptions?: string[];
+    getOptionLabel?: (value: string) => string; // Used for translation of enum values in the filter
+    options?: string[];
 }
 
 /**
@@ -24,9 +23,8 @@ export const CustomAggridAutocompleteFilter: FunctionComponent<CustomAggridAutoc
     api,
     colId,
     filterParams,
-    filterEnums,
-    getEnumLabel,
-    customOptions,
+    getOptionLabel,
+    options,
 }) => {
     const intl = useIntl();
     const { selectedFilterData, handleChangeFilterValue } = useCustomAggridFilter(api, colId, filterParams);
@@ -35,21 +33,12 @@ export const CustomAggridAutocompleteFilter: FunctionComponent<CustomAggridAutoc
         handleChangeFilterValue({ value: data, type: FILTER_TEXT_COMPARATORS.EQUALS });
     };
 
-    const enumMode: boolean = useMemo(() => filterEnums !== undefined, [filterEnums]);
-
-    const filterOption = useMemo(() => {
-        if (enumMode) {
-            return filterEnums?.[colId] ?? [];
-        }
-        return customOptions ?? [];
-    }, [colId, filterEnums, customOptions, enumMode]);
-
     return (
         <Autocomplete
             multiple
             value={Array.isArray(selectedFilterData) ? selectedFilterData : []}
-            options={filterOption}
-            getOptionLabel={getEnumLabel}
+            options={options ?? []}
+            getOptionLabel={getOptionLabel}
             onChange={handleFilterAutoCompleteChange}
             size="small"
             disableCloseOnSelect
