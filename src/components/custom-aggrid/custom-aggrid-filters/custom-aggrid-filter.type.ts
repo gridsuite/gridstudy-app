c796b5d75f7374b58f5bdb82c807bdf1ae94e691
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-03-04 15:47:30 +0100

    Remove duplicate licence

--- src/components/custom-aggrid/custom-aggrid-filters/custom-aggrid-filter.type.ts
+++ src/components/custom-aggrid/custom-aggrid-filters/custom-aggrid-filter.type.ts
@@ -63,12 +63,6 @@ export interface ColumnContext<F extends CustomAggridFilterParams = CustomAggrid
     sortParams?: SortParams;
 }
 
-/**
- * Copyright (c) 2025, RTE (http://www.rte-france.com)
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
 export interface CustomColDef<TData = any, F extends CustomAggridFilterParams = CustomAggridFilterParams>
     extends ColDef<TData, boolean | string | number | CustomCellType> {
     colId: string;
