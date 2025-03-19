edfebf85462f4379675c916a899add73c445b326
dbraquart <107846716+dbraquart@users.noreply.github.com>
2025-03-19 11:28:29 +0100

    Dont manage spreadsheet loading message with no-rows overlay (#2706)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/spreadsheet/table-wrapper.tsx
+++ src/components/spreadsheet/table-wrapper.tsx
@@ -112,7 +112,7 @@ export const TableWrapper: FunctionComponent<TableWrapperProps> = ({
 
     const [manualTabSwitch, setManualTabSwitch] = useState<boolean>(true);
 
-    const [rowData, setRowData] = useState<Identifiable[]>([]);
+    const [rowData, setRowData] = useState<Identifiable[]>();
     const [equipmentToUpdateId, setEquipmentToUpdateId] = useState<string | null>(null);
 
     // Initialize activeTabUuid with the first tab's UUID if not already set
@@ -335,7 +335,7 @@ export const TableWrapper: FunctionComponent<TableWrapperProps> = ({
         // wait a moment  before removing the loading message.
         timerRef.current = setTimeout(() => {
             gridRef.current?.api?.hideOverlay();
-            if (rowData.length === 0 && !isFetching) {
+            if (rowData?.length === 0 && !isFetching) {
                 // we need to call showNoRowsOverlay in order to show message when rowData is empty
                 gridRef.current?.api?.showNoRowsOverlay();
             }
@@ -452,7 +452,7 @@ export const TableWrapper: FunctionComponent<TableWrapperProps> = ({
                             columns={reorderedColsDefs}
                             tableName={tableDefinition?.name}
                             disabled={shouldDisableButtons}
-                            dataSize={rowData.length}
+                            dataSize={rowData ? rowData.length : 0}
                         />
                     </Grid>
                 </Grid>
@@ -469,7 +469,7 @@ export const TableWrapper: FunctionComponent<TableWrapperProps> = ({
                         currentNode={currentNode}
                         rowData={rowData}
                         columnData={reorderedColsDefs}
-                        fetched={equipments?.nodesId.find((nodeId) => nodeId === currentNode.id) !== undefined}
+                        isFetching={isFetching}
                         handleColumnDrag={handleColumnDrag}
                         handleRowDataUpdated={handleRowDataUpdated}
                         shouldHidePinnedHeaderRightBorder={isLockedColumnNamesEmpty}
