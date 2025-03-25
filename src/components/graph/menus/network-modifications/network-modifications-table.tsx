61cbb44e3711fe8c88bfd88394e4336dd474e81c
souissimai <maissa.souissi@rte-france.com>
2025-03-25 19:54:10 +0100

    hide the  No Data  overlay in AG Grid
    Signed-off-by: souissimai <maissa.souissi@rte-france.com>

--- src/components/graph/menus/network-modifications/network-modifications-table.tsx
+++ src/components/graph/menus/network-modifications/network-modifications-table.tsx
@@ -179,6 +179,7 @@ const NetworkModificationsTable: React.FC<NetworkModificationsTableProps> = ({
                 onRowDragEnter={onRowDragStart}
                 onRowDragEnd={onRowDragEnd}
                 rowDragManaged={isRowDragEnabled}
+                suppressNoRowsOverlay={true}
             />
         </div>
     );
