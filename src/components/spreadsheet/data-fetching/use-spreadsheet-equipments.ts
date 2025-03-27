4c1e89bcdd352c2f97515c88bc3f073171ec4f62
David BRAQUART <david.braquart@rte-france.com>
2025-03-27 19:43:58 +0100

    fix prettier after conflicts
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/spreadsheet/data-fetching/use-spreadsheet-equipments.ts
+++ src/components/spreadsheet/data-fetching/use-spreadsheet-equipments.ts
@@ -47,7 +47,9 @@ export const useSpreadsheetEquipments = (
             return;
         }
         let set = new Set<UUID>();
-        const aliasedNodesIds = nodeAliases.filter((nodeAlias) => nodeAlias.id !== undefined).map((nodeAlias) => nodeAlias.id)
+        const aliasedNodesIds = nodeAliases
+            .filter((nodeAlias) => nodeAlias.id !== undefined)
+            .map((nodeAlias) => nodeAlias.id);
         if (aliasedNodesIds.length > 0) {
             treeNodes?.forEach((treeNode) => {
                 if (
