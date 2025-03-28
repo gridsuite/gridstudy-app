6da251b1007bf99ee2cbcfefe1034716a8d7c890
dbraquart <107846716+dbraquart@users.noreply.github.com>
2025-03-28 10:35:04 +0100

    Typescript migration for modification dialog (#2735)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>
    Co-authored-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/components/dialogs/network-modifications/substation/modification/substation-modification-dialog.jsx
+++ src/components/dialogs/network-modifications/substation/modification/substation-modification-dialog.jsx
@@ -6,7 +6,7 @@
  */
 
 import { useForm } from 'react-hook-form';
-import ModificationDialog from '../../../commons/modificationDialog';
+import { ModificationDialog } from '../../../commons/modificationDialog';
 import { useCallback, useEffect, useState } from 'react';
 import { CustomFormProvider, useSnackMessage } from '@gridsuite/commons-ui';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -177,7 +177,6 @@ const SubstationModificationDialog = ({
                 fullWidth
                 onClear={clear}
                 onSave={onSubmit}
-                aria-labelledby="dialog-modify-substation"
                 maxWidth={'md'}
                 titleId="ModifySubstation"
                 open={open}
