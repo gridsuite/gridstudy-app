530c16231e74a614179db36185484b5d80ba7cc7
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-04-02 16:42:09 +0200

    adaptative OverflowTooltip
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/results/common/global-filter/OverflowTooltip.tsx
+++ src/components/results/common/global-filter/OverflowTooltip.tsx
@@ -4,27 +4,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { Dispatch, PropsWithChildren, SetStateAction } from 'react';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { Box } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
 
 export interface OverflowTooltipProps extends PropsWithChildren {
     label: string;
-    maxLabelLength?: number;
 }
 
 /**
- * if the label is longer than maxLabelLength it is truncated and a tooltip is added to display the full text
+ * if the label is too long (reaches overflow) a tooltip is added to display the full text
  */
-function OverflowTooltip({ label, maxLabelLength = 20 }: Readonly<OverflowTooltipProps>) {
-    let truncatedLabel = '';
-    const truncate = label.length > maxLabelLength;
-    if (truncate) {
-        truncatedLabel = label.substring(0, maxLabelLength - 1) + '...';
-    }
+function OverflowTooltip({ label }: Readonly<OverflowTooltipProps>) {
+    const [displayTooltip, setDisplayTooltip] = useState(false);
+    const textElementRef = useRef<HTMLInputElement | null>(null);
+
+    const compareSize = () => {
+        if (textElementRef.current) {
+            const compare = textElementRef.current.scrollWidth <= textElementRef.current.clientWidth;
+            setDisplayTooltip(compare);
+        }
+    };
+
+    useEffect(() => {
+        compareSize();
+        window.addEventListener('resize', compareSize);
+    }, []);
+
+    useEffect(
+        () => () => {
+            window.removeEventListener('resize', compareSize);
+        },
+        []
+    );
+
     return (
-        <Tooltip title={label} arrow disableHoverListener={!truncate}>
+        <Tooltip title={label} arrow disableHoverListener={displayTooltip}>
             <Box
+                ref={textElementRef}
                 component="span"
                 sx={{
                     textOverflow: 'ellipsis',
@@ -33,7 +50,7 @@ function OverflowTooltip({ label, maxLabelLength = 20 }: Readonly<OverflowToolti
                     whiteSpace: 'nowrap',
                 }}
             >
-                {truncate ? truncatedLabel : label}
+                {label}
             </Box>
         </Tooltip>
     );
