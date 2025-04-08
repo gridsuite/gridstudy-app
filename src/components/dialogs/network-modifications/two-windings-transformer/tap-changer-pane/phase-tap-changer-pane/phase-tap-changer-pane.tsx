78fde4ff45bc5323f72e687659f73d4923285a72
Rehili Ghazwa <ghazwarhili@gmail.com>
2025-04-08 16:06:36 +0200

    add sideField

--- src/components/dialogs/network-modifications/two-windings-transformer/tap-changer-pane/phase-tap-changer-pane/phase-tap-changer-pane.tsx
+++ src/components/dialogs/network-modifications/two-windings-transformer/tap-changer-pane/phase-tap-changer-pane/phase-tap-changer-pane.tsx
@@ -277,7 +277,10 @@ export default function PhaseTapChangerPane({
                         <GridItem size={4}>{regulationTypeField}</GridItem>
                         {regulationType === REGULATION_TYPES.LOCAL.id && <GridItem size={4}>{sideField}</GridItem>}
                         {regulationType === REGULATION_TYPES.DISTANT.id && (
-                            <GridItem size={8}>{regulatingTerminalField}</GridItem>
+                            <Grid item container spacing={1}>
+                                <GridItem size={8}>{regulatingTerminalField}</GridItem>
+                                <GridItem size={4}>{sideField}</GridItem>
+                            </Grid>
                         )}
                     </Grid>
                 </>
--- src/components/dialogs/network-modifications/two-windings-transformer/tap-changer-pane/ratio-tap-changer-pane/ratio-tap-changer-pane.tsx
+++ src/components/dialogs/network-modifications/two-windings-transformer/tap-changer-pane/ratio-tap-changer-pane/ratio-tap-changer-pane.tsx
@@ -249,7 +249,10 @@ export default function RatioTapChangerPane({
                         <GridItem size={4}>{regulationTypeField}</GridItem>
                         {regulationType === REGULATION_TYPES.LOCAL.id && <GridItem size={4}>{sideField}</GridItem>}
                         {regulationType === REGULATION_TYPES.DISTANT.id && (
-                            <GridItem size={8}>{regulatingTerminalField}</GridItem>
+                            <Grid item container spacing={1}>
+                                <GridItem size={8}>{regulatingTerminalField}</GridItem>
+                                <GridItem size={4}>{sideField}</GridItem>
+                            </Grid>
                         )}
                     </Grid>
 
