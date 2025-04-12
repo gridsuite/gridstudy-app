690a1ab3d1687580c8ab63d129d58865474a9d16
Thang PHAM <phamthang37@gmail.com>
2025-04-12 13:43:34 +0200

    Rectify
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/components/results/common/global-filter/global-filter-selector.tsx
+++ src/components/results/common/global-filter/global-filter-selector.tsx
@@ -4,15 +4,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 import GlobalFilterProvider from './global-filter-provider';
-import GlobalFilterAutocomplete from './global-filter-autocomplete';
-import { EQUIPMENT_TYPES } from '../../../utils/equipment-types';
+import GlobalFilterAutocomplete, { GlobalFilterAutocompleteProps } from './global-filter-autocomplete';
 import { GlobalFilter } from './global-filter-types';
 
-export type GlobalFilterSelectorProps = {
+export type GlobalFilterSelectorProps = GlobalFilterAutocompleteProps & {
     onChange: (globalFilters: GlobalFilter[]) => void;
-    filterableEquipmentTypes: EQUIPMENT_TYPES[];
-    filters: GlobalFilter[];
 };
 export default function GlobalFilterSelector({
     onChange,
