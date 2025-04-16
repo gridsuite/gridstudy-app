bb4e479ef03d0520a7499b5c8024f41414e7019e
Thang PHAM <phamthang37@gmail.com>
2025-04-16 17:45:20 +0200

    [Dynamic simulation] Button close of plot no longer works
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/components/results/dynamicsimulation/dynamic-simulation-result-time-series.tsx
+++ src/components/results/dynamicsimulation/dynamic-simulation-result-time-series.tsx
@@ -7,7 +7,7 @@
 
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
-import { Box, LinearProgress, Theme } from '@mui/material';
+import { Box, Grid, LinearProgress, Theme } from '@mui/material';
 import DynamicSimulationResultChart from './timeseries/dynamic-simulation-result-chart';
 import { memo, SyntheticEvent, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
@@ -27,10 +27,11 @@ import { DropResult } from '@hello-pangea/dnd';
 const styles = {
     root: {
         height: '100%',
+        maxWidth: '100vw',
     },
     addButton: (theme: Theme) => ({
         borderRadius: '50%',
-        marginRight: theme.spacing(10),
+        marginRight: theme.spacing(2),
         color: theme.palette.primary.main,
     }),
     loader: {
@@ -123,61 +124,65 @@ const DynamicSimulationResultTimeSeries = memo(function ({
                 </Box>
             )}
             <Overlay message={overlayMessage}>
-                <Box sx={styles.root}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        {/* tab headers */}
-                        <DroppableTabs
-                            id={'1'}
-                            value={selectedIndex}
-                            onChange={handleTabsChange}
-                            tabsRender={() =>
-                                tabs.map((tab, index) => {
-                                    return (
-                                        <DraggableTab
-                                            key={`tab-${tab.id}`}
-                                            id={`tab-${index}`}
-                                            index={index}
-                                            value={index}
-                                            label={
-                                                <span
-                                                    style={{
-                                                        whiteSpace: 'nowrap',
-                                                    }}
-                                                >
-                                                    {`${intl.formatMessage({
-                                                        id: 'DynamicSimulationResultTab',
-                                                    })} ${tab.id}`}
-                                                    <TooltipIconButton
-                                                        tooltip={intl.formatMessage({
-                                                            id: 'DynamicSimulationCloseTab',
-                                                        })}
-                                                        size="small"
-                                                        component="span"
-                                                        onClick={handleClose(index)}
+                <Grid sx={styles.root}>
+                    {/* tab headers */}
+                    <Grid container direction="row" wrap="nowrap" item>
+                        <Grid item sx={{ overflow: 'hidden' }}>
+                            <DroppableTabs
+                                id={'1'}
+                                value={selectedIndex}
+                                onChange={handleTabsChange}
+                                tabsRender={() =>
+                                    tabs.map((tab, index) => {
+                                        return (
+                                            <DraggableTab
+                                                key={`tab-${tab.id}`}
+                                                id={`tab-${index}`}
+                                                index={index}
+                                                value={index}
+                                                label={
+                                                    <span
+                                                        style={{
+                                                            whiteSpace: 'nowrap',
+                                                        }}
                                                     >
-                                                        <CloseIcon />
-                                                    </TooltipIconButton>
-                                                </span>
-                                            }
-                                        />
-                                    );
-                                })
-                            }
-                            onDragEnd={handleDragEnd}
-                        />
-                        <TooltipIconButton
-                            tooltip={intl.formatMessage({
-                                id: 'DynamicSimulationAddTab',
-                            })}
-                            sx={styles.addButton}
-                            onClick={handleAddNewTab}
-                        >
-                            <AddIcon />
-                        </TooltipIconButton>
-                    </Box>
+                                                        {`${intl.formatMessage({
+                                                            id: 'DynamicSimulationResultTab',
+                                                        })} ${tab.id}`}
+                                                        <TooltipIconButton
+                                                            tooltip={intl.formatMessage({
+                                                                id: 'DynamicSimulationCloseTab',
+                                                            })}
+                                                            size="small"
+                                                            component="span"
+                                                            onClick={handleClose(index)}
+                                                        >
+                                                            <CloseIcon />
+                                                        </TooltipIconButton>
+                                                    </span>
+                                                }
+                                            />
+                                        );
+                                    })
+                                }
+                                onDragEnd={handleDragEnd}
+                            />
+                        </Grid>
+                        <Grid item padding={1}>
+                            <TooltipIconButton
+                                tooltip={intl.formatMessage({
+                                    id: 'DynamicSimulationAddTab',
+                                })}
+                                sx={styles.addButton}
+                                onClick={handleAddNewTab}
+                            >
+                                <AddIcon />
+                            </TooltipIconButton>
+                        </Grid>
+                    </Grid>
 
                     {/* tab contents */}
-                    <Box
+                    <Grid
                         sx={{
                             height: 'calc(100vh - 220px)', // TODO fix layout to use flexGrow : 1
                         }}
@@ -192,8 +197,8 @@ const DynamicSimulationResultTimeSeries = memo(function ({
                                 />
                             </VisibilityBox>
                         ))}
-                    </Box>
-                </Box>
+                    </Grid>
+                </Grid>
             </Overlay>
         </>
     );
