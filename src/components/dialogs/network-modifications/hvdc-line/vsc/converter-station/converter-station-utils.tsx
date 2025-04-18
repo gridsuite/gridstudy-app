c2fb636977ead30271b6481dbd78da03511e362a
Etienne LESOT <etienne.lesot@rte-france.com>
2025-04-18 16:49:56 +0200

    add missing checks on vsc stations
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/dialogs/network-modifications/hvdc-line/vsc/converter-station/converter-station-utils.tsx
+++ src/components/dialogs/network-modifications/hvdc-line/vsc/converter-station/converter-station-utils.tsx
@@ -98,7 +98,7 @@ export function getVscConverterStationSchema(id: string) {
         [id]: yup.object().shape({
             [CONVERTER_STATION_ID]: yup.string().nullable().required(),
             [CONVERTER_STATION_NAME]: yup.string().nullable(),
-            [LOSS_FACTOR]: yup.number().nullable().required(),
+            [LOSS_FACTOR]: yup.number().nullable().required().min(0, 'NormalizedPercentage').max(100, 'NormalizedPercentage'),
             [VOLTAGE_REGULATION_ON]: yup.boolean(),
             [REACTIVE_POWER]: yup
                 .number()
@@ -110,6 +110,7 @@ export function getVscConverterStationSchema(id: string) {
             [VOLTAGE]: yup
                 .number()
                 .nullable()
+                .min(0, 'mustBeGreaterOrEqualToZero')
                 .when([VOLTAGE_REGULATION_ON], {
                     is: true,
                     then: (schema) => schema.required(),
