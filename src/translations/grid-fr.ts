215f8f81b63e028bf0e135746f9e33a111e6a647
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2025-04-23 16:41:06 +0200

    Add missing translations for AgGrid (#2802)

---
+++ src/translations/grid-fr.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export const grid_fr = {
+    'grid.noCalculation': 'Pas de calcul lancé',
+    'grid.failed': 'Echec du calcul',
+    'grid.running': 'Calcul en cours...',
+    'grid.noLimitViolation': 'Pas de contrainte',
+};
