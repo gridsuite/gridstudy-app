6073b24ee6c5ad34728712210bc848ec6a8a6b93
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-04-30 17:44:12 +0200

    extra DATA_KEY_TO_FILTER_KEY values
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/results/sensitivity-analysis/sensitivity-analysis-result-utils.ts
+++ src/components/results/sensitivity-analysis/sensitivity-analysis-result-utils.ts
@@ -40,9 +40,11 @@ export const SUFFIX_TYPES = {
 export const PAGE_OPTIONS = [10, 25, 100, { label: 'All', value: -1 }];
 export const DEFAULT_PAGE_COUNT = PAGE_OPTIONS[1];
 export const DATA_KEY_TO_FILTER_KEY = {
-    funcId: 'functionIds',
-    varId: 'variableIds',
-    contingencyId: 'contingencyIds',
+    funcId: 'functionId',
+    varId: 'variableId',
+    contingencyId: 'contingencyId',
+    value: 'rawSensitivityResult.value',
+    functionReference: 'rawSensitivityResult.functionReference',
 };
 export const DATA_KEY_TO_SORT_KEY = {
     funcId: 'FUNCTION',
