3ae08f29d1c431955486b741cc9e7c917ff6902e
Ghazoua Rhili <ghazwarhili@gmail.com>
2025-05-02 16:01:08 +0200

    implement voltage level topology modification (#2814)
    Signed-off-by: Rehili Ghazwa <ghazwarhili@gmail.com>

---
+++ src/components/dialogs/network-modifications/voltage-level-topology-modification/connection-cell-render.tsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import React from 'react';
+import CheckboxNullableInput from '../../../utils/rhf-inputs/boolean-nullable-input';
+import { useIntl } from 'react-intl';
+
+type ConnectionCellRendererProps = {
+    name: string;
+};
+
+export default function ConnectionCellRenderer({ name }: Readonly<ConnectionCellRendererProps>) {
+    const intl = useIntl();
+    return (
+        <CheckboxNullableInput
+            name={name}
+            label={(value: boolean | null) =>
+                value !== null
+                    ? intl.formatMessage({ id: value ? 'Open' : 'Closed' })
+                    : intl.formatMessage({ id: 'NoModification' })
+            }
+        />
+    );
+}
