6212e1cc9e9720f282483a64f45211d71d9b6cb6
sBouzols <sylvain.bouzols@gmail.com>
2025-05-09 17:01:58 +0200

    feat(): Add a diagram session storage custom hook
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

---
+++ src/components/diagrams/hooks/use-diagram-session-storage.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { UUID } from 'crypto';
+import { Diagram, DiagramParams } from '../diagram.type';
+import { useSelector } from 'react-redux';
+import { AppState } from 'redux/reducer';
+import { useEffect } from 'react';
+import { loadDiagramsFromSessionStorage, syncDiagramsWithSessionStorage } from 'redux/session-storage/diagram-state';
+
+const keyToKeepInSessionStorage = ['type', 'voltageLevelId', 'substationId', 'voltageLevelIds']; // satic
+
+type useDiagramSessionStorageProps = {
+    diagrams: Record<UUID, Diagram>;
+    onLoadFromSessionStorage: (diagramParams: DiagramParams) => void;
+};
+
+export const useDiagramSessionStorage = ({ diagrams, onLoadFromSessionStorage }: useDiagramSessionStorageProps) => {
+    const studyUuid = useSelector((state: AppState) => state.studyUuid);
+
+    // at mount
+    useEffect(() => {
+        if (!studyUuid) {
+            return;
+        }
+        const diagrams: DiagramParams[] = loadDiagramsFromSessionStorage(studyUuid);
+        diagrams.forEach((diagramParams) => onLoadFromSessionStorage(diagramParams));
+    });
+
+    // at update
+    useEffect(() => {
+        if (!studyUuid) {
+            return;
+        }
+        // save diagrams to session storage
+        const diagramParams = Object.values(diagrams).map((diagram) =>
+            Object.fromEntries(Object.entries(diagram).filter(([key]) => keyToKeepInSessionStorage.includes(key)))
+        );
+        syncDiagramsWithSessionStorage(diagramParams, studyUuid);
+    }, [diagrams, studyUuid]);
+};
