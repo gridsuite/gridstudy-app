6f2752725ac28cc13b60171cee8c64ccf9e092e3
AAJELLAL <ali.ajellalod@gmail.com>
2025-05-20 11:09:35 +0200

    Add the files licenses.
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/components/graph/menus/root-network/root-network-search-results.tsx
+++ src/components/graph/menus/root-network/root-network-search-results.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { Box, Divider, Typography } from '@mui/material';
 import { NetworkNodeIcon, useModificationLabelComputer } from '@gridsuite/commons-ui';
 import { Modification, ModificationsSearchResult } from './root-network.types';
