73ff9e1865fdcc08a93f72288e81c3a2e2aa54d9
sBouzols <sylvain.bouzols@gmail.com>
2025-06-03 11:19:40 +0200

    fix(DiagramPane): getDefaultHeightByDiagramType and getDefaultWidthByDiagramType returned 1 for `NAD_FROM_CONFIG` type
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/components/diagrams/diagram-pane.tsx
+++ src/components/diagrams/diagram-pane.tsx
@@ -962,6 +962,8 @@ export function DiagramPane({
                 return DEFAULT_HEIGHT_VOLTAGE_LEVEL;
             case DiagramType.NETWORK_AREA_DIAGRAM:
                 return DEFAULT_HEIGHT_NETWORK_AREA_DIAGRAM;
+            case DiagramType.NAD_FROM_CONFIG:
+                return DEFAULT_HEIGHT_NETWORK_AREA_DIAGRAM;
             default:
                 console.warn('Unknown diagram type !');
                 return 1;
@@ -975,6 +977,8 @@ export function DiagramPane({
             case DiagramType.VOLTAGE_LEVEL:
                 return DEFAULT_WIDTH_VOLTAGE_LEVEL;
             case DiagramType.NETWORK_AREA_DIAGRAM:
+                return DEFAULT_WIDTH_NETWORK_AREA_DIAGRAM;
+            case DiagramType.NAD_FROM_CONFIG:
                 return DEFAULT_WIDTH_NETWORK_AREA_DIAGRAM;
             default:
                 console.warn('Unknown diagram type !');
