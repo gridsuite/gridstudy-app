b1acf4ed2cba88f8f76073465c2bfd5dd57ff8cc
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2025-06-12 10:54:18 +0200

    Fix: restore depth tools footer for NAD in Diagram Grid Layout (#2906)
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>
    Co-authored-by: Charly B <47325354+EstherDarkish@users.noreply.github.com>

--- src/components/diagrams/diagram-controls.tsx
+++ src/components/diagrams/diagram-controls.tsx
@@ -6,7 +6,7 @@
  */
 
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
 import {
     DirectoryItemSelector,
@@ -22,7 +22,6 @@ import SaveIcon from '@mui/icons-material/Save';
 import { Theme, Tooltip } from '@mui/material';
 import { AppState } from 'redux/reducer';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { setEditNadMode } from 'redux/actions';
 
 const styles = {
     actionIcon: (theme: Theme) => ({
@@ -57,15 +56,15 @@ const styles = {
 interface DiagramControlsProps {
     onSave?: (data: IElementCreationDialog) => void;
     onLoad?: (nadConfigId: string, nadName: string) => void;
+    isEditNadMode: boolean;
+    onToggleEditNadMode?: (isEditMode: boolean) => void;
 }
 
-const DiagramControls: React.FC<DiagramControlsProps> = ({ onSave, onLoad }) => {
+const DiagramControls: React.FC<DiagramControlsProps> = ({ onSave, onLoad, isEditNadMode, onToggleEditNadMode }) => {
     const intl = useIntl();
     const [isSaveDialogOpen, setIsSaveDialogOpen] = useState(false);
     const [isLoadSelectorOpen, setIsLoadSelectorOpen] = useState(false);
     const studyUuid = useSelector((state: AppState) => state.studyUuid);
-    const isEditMode = useSelector((state: AppState) => state.isEditMode);
-    const dispatch = useDispatch();
 
     const handleCloseSaveDialog = () => {
         setIsSaveDialogOpen(false);
@@ -103,7 +102,7 @@ const DiagramControls: React.FC<DiagramControlsProps> = ({ onSave, onLoad }) => 
     };
 
     const handleToggleEditMode = () => {
-        dispatch(setEditNadMode(!isEditMode));
+        onToggleEditNadMode?.(!isEditNadMode);
     };
 
     /**
@@ -133,7 +132,7 @@ const DiagramControls: React.FC<DiagramControlsProps> = ({ onSave, onLoad }) => 
             </Box>
             <Box sx={styles.buttonPanel}>
                 <Button size="small" sx={styles.button} onClick={handleToggleEditMode}>
-                    <FormattedMessage id={isEditMode ? 'save' : 'EditNad'} />
+                    <FormattedMessage id={isEditNadMode ? 'save' : 'EditNad'} />
                 </Button>
             </Box>
             {studyUuid && (
