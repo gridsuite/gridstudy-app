5154910cbd717d6561768185c042ad71a7d5776d
David BRAQUART <david.braquart@rte-france.com>
2025-06-13 19:38:26 +0200

    use table-inputs from commons

--- src/components/dialogs/limits/temporary-limits-table.tsx
+++ src/components/dialogs/limits/temporary-limits-table.tsx
@@ -8,13 +8,19 @@
 import { useState } from 'react';
 import { useFieldArray } from 'react-hook-form';
 import { Box, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import { ColumnNumeric, ColumnText, DndColumnType, ErrorInput, FieldErrorAlert } from '@gridsuite/commons-ui';
+import {
+    ColumnNumeric,
+    ColumnText,
+    DndColumnType,
+    ErrorInput,
+    FieldErrorAlert,
+    TableNumericalInput,
+    TableTextInput,
+} from '@gridsuite/commons-ui';
 import IconButton from '@mui/material/IconButton';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { SELECTED } from '../../utils/field-constants';
-import { TableNumericalInput } from '../../utils/rhf-inputs/table-inputs/table-numerical-input';
-import { TableTextInput } from '../../utils/rhf-inputs/table-inputs/table-text-input';
 import { TemporaryLimit } from '../../../services/network-modification-types';
 
 const styles = {
