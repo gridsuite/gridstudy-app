eeade9d41c18c9aee7ef7b8beb3f20e74915b289
achour94 <berrahmaachour@gmail.com>
2025-06-18 09:58:52 +0200

    Fix French translation for generator participation requirement in CSV locale
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- src/translations/dynamic/csv-locale-fr.ts
+++ src/translations/dynamic/csv-locale-fr.ts
@@ -8,7 +8,7 @@
 const csv_locale_fr = {
     // Used in the tabular creation and modification
     'TabularCreationSkeletonComment.GENERATOR':
-        '#,,HYDRO | NUCLEAR | WIND | THERMAL | SOLAR | OTHER,,,true | false,,TOP | BOTTOM | UNDEFINED,,,,,,,true | false,,,,,,,,,,,,true | false,required if voltageRegulationOn is true,,LINE | TWO_WINDINGS_TRANSFORMER | GENERATOR | LOAD | BATTERY | SHUNT_COMPENSATOR | STATIC_VAR_COMPENSATOR | DANGLING_LINE | HVDC_CONVERTER_STATION,,,true | false,requis si participate is true,,,,,,,',
+        '#,,HYDRO | NUCLEAR | WIND | THERMAL | SOLAR | OTHER,,,true | false,,TOP | BOTTOM | UNDEFINED,,,,,,,true | false,,,,,,,,,,,,true | false,required if voltageRegulationOn is true,,LINE | TWO_WINDINGS_TRANSFORMER | GENERATOR | LOAD | BATTERY | SHUNT_COMPENSATOR | STATIC_VAR_COMPENSATOR | DANGLING_LINE | HVDC_CONVERTER_STATION,,,true | false,requis si participate est true,,,,,,,',
     'TabularCreationSkeletonComment.BATTERY':
         '#,,,,true | false,,TOP | BOTTOM | UNDEFINED,,,,,,true | false,,,,,,,,,,,,true | false,requis si participate est true',
     'TabularCreationSkeletonComment.LOAD':
