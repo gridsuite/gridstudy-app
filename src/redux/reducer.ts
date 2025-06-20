386eed957a5193a602222eb22d61516ff26b2f7b
BOUTIER Charly <charly.boutier@rte-france.com>
2025-06-20 21:40:50 +0200

    switch to grid on diagram opening
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1381,6 +1381,14 @@ export const reducer = createReducer(initialState, (builder) => {
                 voltageLevelIds: [action.id as UUID],
             };
         }
+
+        // Switch to the grid layout in order to see the newly opened diagram
+        if (
+            state.studyDisplayMode !== StudyDisplayMode.DIAGRAM_GRID_LAYOUT_AND_TREE &&
+            state.studyDisplayMode !== StudyDisplayMode.DIAGRAM_GRID_LAYOUT
+        ) {
+            state.studyDisplayMode = StudyDisplayMode.DIAGRAM_GRID_LAYOUT_AND_TREE;
+        }
     });
 
     builder.addCase(OPEN_NAD_LIST, (state, action: OpenNadListAction) => {
@@ -1401,6 +1409,14 @@ export const reducer = createReducer(initialState, (builder) => {
             eventType: DiagramEventType.CREATE,
             voltageLevelIds: uniqueIds as UUID[],
         };
+
+        // Switch to the grid layout in order to see the newly opened diagram
+        if (
+            state.studyDisplayMode !== StudyDisplayMode.DIAGRAM_GRID_LAYOUT_AND_TREE &&
+            state.studyDisplayMode !== StudyDisplayMode.DIAGRAM_GRID_LAYOUT
+        ) {
+            state.studyDisplayMode = StudyDisplayMode.DIAGRAM_GRID_LAYOUT_AND_TREE;
+        }
     });
 
     builder.addCase(MINIMIZE_DIAGRAM, (state, action: MinimizeDiagramAction) => {
