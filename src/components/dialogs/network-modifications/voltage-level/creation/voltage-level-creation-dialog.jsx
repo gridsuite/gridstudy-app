5f417e1bbe05f733a71cb2d66e1181f56f3bcadf
Etienne LESOT <etienne.lesot@rte-france.com>
2025-06-23 11:09:32 +0200

    refactor
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
+++ src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
@@ -258,21 +258,13 @@ const VoltageLevelCreationDialog = ({
         [setValue, intl, reset, snackWarning]
     );
 
-    // Watch EQUIPMENT_ID changed
+    // Watch EQUIPMENT_ID and SUBSTATION_CREATION_ID changed
     useEffect(() => {
         const subscription = watch((value, { name }) => {
             // force trigger validation on SUBSTATION_CREATION_ID if it has a value
             if (name === EQUIPMENT_ID && getValues(SUBSTATION_CREATION_ID)) {
                 trigger(SUBSTATION_CREATION_ID);
             }
-        });
-        return () => subscription.unsubscribe();
-    }, [watch, trigger, getValues]);
-
-    // Watch SUBSTATION_CREATION_ID changed
-    useEffect(() => {
-        const subscription = watch((value, { name }) => {
-            // force trigger validation on EQUIPMENT_ID if it has a value
             if (name === SUBSTATION_CREATION_ID && getValues(EQUIPMENT_ID)) {
                 trigger(EQUIPMENT_ID);
             }
