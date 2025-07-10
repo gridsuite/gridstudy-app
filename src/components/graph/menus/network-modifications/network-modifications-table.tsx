65fbd159a3c324392c286c2adcba95a48927ce67
Ayoub LABIDI <ayoub.labidi@protonmail.com>
2025-07-10 23:34:07 +0200

    Fix modification table row drag text
    Signed-off-by: Ayoub LABIDI <ayoub.labidi@protonmail.com>

--- src/components/graph/menus/network-modifications/network-modifications-table.tsx
+++ src/components/graph/menus/network-modifications/network-modifications-table.tsx
@@ -5,12 +5,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useCallback, useMemo, ReactNode, SetStateAction } from 'react';
+import React, { useCallback, useMemo, SetStateAction } from 'react';
 import { CustomAGGrid, NetworkModificationMetadata, useModificationLabelComputer } from '@gridsuite/commons-ui';
 import {
     CellClickedEvent,
     ColDef,
     GetRowIdParams,
+    IRowDragItem,
     RowClassParams,
     RowDragEndEvent,
     RowDragEnterEvent,
@@ -93,7 +94,7 @@ const NetworkModificationsTable: React.FC<NetworkModificationsTableProps> = ({
     const currentRootNetworkUuid = useSelector((state: AppState) => state.currentRootNetworkUuid);
 
     const getModificationLabel = useCallback(
-        (modif?: NetworkModificationMetadata): ReactNode => {
+        (modif?: NetworkModificationMetadata, formatBold: boolean = true) => {
             if (!modif) {
                 return '';
             }
@@ -101,11 +102,19 @@ const NetworkModificationsTable: React.FC<NetworkModificationsTableProps> = ({
                 { id: 'network_modifications.' + modif.messageType },
                 {
                     ...modif,
-                    ...computeLabel(modif),
+                    ...computeLabel(modif, formatBold),
                 }
             );
         },
         [computeLabel, intl]
+    );
+
+    const getRowDragText = useCallback(
+        (params: IRowDragItem) => {
+            const label = getModificationLabel(params?.rowNode?.data, false);
+            return typeof label === 'string' ? label : '';
+        },
+        [getModificationLabel]
     );
 
     const columnDefs = useMemo(() => {
@@ -213,6 +222,7 @@ const NetworkModificationsTable: React.FC<NetworkModificationsTableProps> = ({
                 rowDragManaged={!isRowDragDisabled}
                 suppressNoRowsOverlay={true}
                 overrideLocales={AGGRID_LOCALES}
+                rowDragText={getRowDragText}
             />
         </Box>
     );
