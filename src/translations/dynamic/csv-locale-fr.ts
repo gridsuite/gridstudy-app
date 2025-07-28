c44a35c960f4b026969934a300759ed3400d33b3
David BRAQUART <david.braquart@rte-france.com>
2025-07-28 13:01:48 +0200

    rev remark; misspelled french
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/translations/dynamic/csv-locale-fr.ts
+++ src/translations/dynamic/csv-locale-fr.ts
@@ -23,7 +23,7 @@ const csv_locale_fr = {
     'TabularModificationSkeletonComment.SUBSTATION':
         '#;;Code à 2 lettres de la norme ISO 3166-1 (FR ES PT IT CH DE BE LU NL GB ...)',
     'TabularCreationSkeletonComment.SHUNT_COMPENSATOR':
-        '#Pour chaque MCS il est possible de modifier soit la puissance réactive installée (et le type) soit la susceptance installée. En cas de conflit la susceptance installée sera ignorée.;;;;true | false;;TOP | BOTTOM;;;;REACTOR | CAPACITOR | requis si maxQAtNominalV est définit;;',
+        '#Pour chaque MCS il est possible de modifier soit la puissance réactive installée (et le type) soit la susceptance installée. En cas de conflit la susceptance installée sera ignorée.;;;;true | false;;TOP | BOTTOM;;;;REACTOR | CAPACITOR | requis si maxQAtNominalV est défini;;',
     'TabularModificationSkeletonComment.SHUNT_COMPENSATOR':
         '#Pour chaque MCS il est possible de modifier soit la puissance réactive installée (et le type) soit la susceptance installée. En cas de conflit la susceptance installée sera ignorée.;;true | false;;TOP | BOTTOM;;;;REACTOR | CAPACITOR;;',
 };
