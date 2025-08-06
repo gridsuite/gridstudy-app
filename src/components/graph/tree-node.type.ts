5315ff9e365c1db85d23f52bcf7fd14c116ea5d2
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-08-06 09:44:51 +0200

    fix sonar issues
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/components/graph/tree-node.type.ts
+++ src/components/graph/tree-node.type.ts
@@ -65,7 +65,7 @@ export type NetworkModificationNodeData = AbstractNode & {
     nodeType?: NetworkModificationNodeType;
 };
 
-type NodeCommonData = {
+export type NodeCommonData = {
     label: string;
     globalBuildStatus?: BUILD_STATUS;
     description?: string;
@@ -78,8 +78,7 @@ export type ModificationNode = Node<ReactFlowModificationNodeData, NodeType.NETW
     id: UUID;
 };
 
-export type ReactFlowRootNodeData = NodeCommonData;
-export type RootNode = Node<ReactFlowRootNodeData, NodeType.ROOT> & { id: UUID };
+export type RootNode = Node<NodeCommonData, NodeType.ROOT> & { id: UUID };
 
 export type CurrentTreeNode = ModificationNode | RootNode;
 
