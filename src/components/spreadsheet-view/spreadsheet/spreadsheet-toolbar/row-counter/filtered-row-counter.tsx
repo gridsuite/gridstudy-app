dc94939def82c31f4045677a06710be5fa00f797
Hugo Marcellin <marcellinhugo@orange.fr>
2025-08-12 14:35:24 +0200

    Add filter counter to spreadsheet (#3092)
    Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

---
+++ src/components/spreadsheet-view/spreadsheet/spreadsheet-toolbar/row-counter/filtered-row-counter.tsx
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { type RefObject, useCallback } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../../../../redux/reducer';
+import { SpreadsheetTabDefinition } from '../../../types/spreadsheet.type';
+import { Box, Fade, Theme, CircularProgress, Button, Tooltip } from '@mui/material';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
+import { resetSpreadsheetColumnsFilters } from '../../../../../services/study/study-config';
+import { useFilteredRowCounterInfo } from './use-filtered-row-counter';
+import { useSnackMessage } from '@gridsuite/commons-ui';
+
+const styles = {
+    getContainer: (theme: Theme) => ({
+        minWidth: theme.spacing(18),
+        paddingRight: '10px',
+        paddingLeft: '10px',
+    }),
+    innerContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        whiteSpace: 'pre-line',
+    },
+    restoreButton: {
+        paddingRight: '3px',
+    },
+};
+
+export type SpreadsheetFilteredRowCountProps = {
+    gridRef: RefObject<AgGridReact>;
+    tableDefinition: SpreadsheetTabDefinition;
+    disabled: boolean;
+};
+
+export function FilteredRowCounter({ gridRef, tableDefinition, disabled }: Readonly<SpreadsheetFilteredRowCountProps>) {
+    const studyUuid = useSelector((state: AppState) => state.studyUuid);
+
+    const { isLoading, isAnyFilterPresent, rowCountLabel, tooltipContent } = useFilteredRowCounterInfo({
+        gridRef,
+        tableDefinition,
+        disabled,
+    });
+    const { snackError } = useSnackMessage();
+
+    const handleResetFilters = useCallback(() => {
+        if (isAnyFilterPresent && studyUuid) {
+            resetSpreadsheetColumnsFilters(studyUuid, tableDefinition.uuid).catch((error) => {
+                console.error('Failed to update global filters:', error);
+                snackError({ headerId: 'spreadsheet/reset_filters_error', messageTxt: error.messageTxt ?? error });
+            });
+        }
+    }, [isAnyFilterPresent, snackError, studyUuid, tableDefinition.uuid]);
+
+    return (
+        <Tooltip title={tooltipContent} placement="bottom-start" sx={{ marginLeft: 1 }}>
+            <span>
+                {
+                    <Button
+                        variant={'text'}
+                        onClick={handleResetFilters}
+                        disabled={!isAnyFilterPresent}
+                        sx={styles.getContainer}
+                    >
+                        {isLoading ? (
+                            <CircularProgress size="0.75rem" />
+                        ) : (
+                            <Fade in timeout={600} key={rowCountLabel}>
+                                <Box sx={styles.innerContainer}>
+                                    <FilterAltOffIcon sx={styles.restoreButton} />
+                                    {rowCountLabel}
+                                </Box>
+                            </Fade>
+                        )}
+                    </Button>
+                }
+            </span>
+        </Tooltip>
+    );
+}
