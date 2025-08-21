33a065bda151398927af00cf0d1e4bf4defe3dbc
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-08-21 13:42:58 +0200

    fixes
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/components/graph/menus/network-modifications/node-editor-header.tsx
+++ src/components/graph/menus/network-modifications/node-editor-header.tsx
@@ -6,7 +6,7 @@
  */
 
 import { useState } from 'react';
-import { IconButton, Box, Theme, Button, Tooltip, Typography, Collapse, Card } from '@mui/material';
+import { IconButton, Box, Theme, Button, Tooltip, Typography, Collapse, Paper } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CloseIcon from '@mui/icons-material/Close';
 import { lighten, darken } from '@mui/material/styles';
@@ -46,41 +46,6 @@ const styles = {
         flexGrow: 1,
         overflow: 'hidden',
     },
-    accordion: (theme: Theme) => ({
-        backgroundColor: theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[900],
-        marginRight: theme.spacing(1),
-        borderRadius: theme.shape.borderRadius,
-        width: '100%',
-        boxSizing: 'border-box',
-    }),
-    accordionSummary: (theme: Theme) => ({
-        minHeight: 0,
-        '&.Mui-expanded .MuiTypography-root': {
-            color: theme.palette.text.secondary,
-        },
-        '& .MuiAccordionSummary-content': {
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'pre-wrap',
-            flexGrow: 1,
-        },
-    }),
-    topographySummary: {
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        WebkitLineClamp: 1,
-        overflow: 'hidden',
-        width: 'auto',
-        textOverflow: 'ellipsis',
-        wordBreak: 'break-word',
-    },
-    accordionDetails: {
-        overflowWrap: 'break-word',
-        whiteSpace: 'pre-wrap',
-        paddingTop: 0,
-        maxHeight: 200,
-        overflowY: 'auto',
-    },
 };
 
 interface NodeEditorHeaderProps {
@@ -117,18 +82,16 @@ export const NodeEditorHeader = ({ onClose }: NodeEditorHeaderProps) => {
             </Box>
 
             {description && (
-                <Card
+                <Paper
+                    elevation={0}
                     sx={(theme) => ({
                         marginRight: 1,
-                        marginBottom: 1,
-                        p: 2,
+                        p: 1,
+                        pl: 2,
                         position: 'relative',
                         color: !expanded ? theme.palette.text.secondary : 'inherit',
-                        backgroundColor: !expanded
-                            ? theme.palette.mode === 'light'
-                                ? theme.palette.grey[200]
-                                : theme.palette.grey[900]
-                            : 'inherit',
+                        backgroundColor:
+                            theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[900],
                     })}
                 >
                     <IconButton
@@ -169,7 +132,7 @@ export const NodeEditorHeader = ({ onClose }: NodeEditorHeaderProps) => {
                             {description}
                         </Typography>
                     </Collapse>
-                </Card>
+                </Paper>
             )}
         </>
     );
