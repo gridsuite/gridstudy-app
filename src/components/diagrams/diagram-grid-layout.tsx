777fa99564f74aec5d28b22f557c4a84190e2ed9
BOUTIER Charly <charly.boutier@rte-france.com>
2025-09-01 20:23:31 +0200

    Workaround for responsiveGridLayout bug
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/components/diagrams/diagram-grid-layout.tsx
+++ src/components/diagrams/diagram-grid-layout.tsx
@@ -156,7 +156,7 @@ function DiagramGridLayout({ studyUuid, showInSpreadsheet, showGrid, visible }: 
     const theme = useTheme();
     const [layouts, setLayouts] = useState<Layouts>(initialLayouts);
     const [blinkingDiagrams, setBlinkingDiagrams] = useState<UUID[]>([]);
-    const responsiveGridLayoutRef = useRef<HTMLDivElement | null>(null);
+    const responsiveGridLayoutRef = useRef<any>(null);
     const currentBreakpointRef = useRef<string>('lg');
     const lastModifiedBreakpointRef = useRef<string>('lg'); // Track the last modified breakpoint
 
@@ -314,7 +314,9 @@ function DiagramGridLayout({ studyUuid, showInSpreadsheet, showGrid, visible }: 
     }, []);
 
     const handleResize = useCallback<ItemCallback>((layout, oldItem, newItem, placeholder, event, _el) => {
-        const container = responsiveGridLayoutRef.current;
+        // We cannot use the ResponsiveGridLayout's innerRef prop (see https://github.com/react-grid-layout/react-grid-layout/issues/1444)
+        // so we manually fetch its inner ref inside to get the HTMLDivElement.
+        const container = responsiveGridLayoutRef.current?.elementRef?.current;
         if (!container) {
             return;
         }
@@ -427,7 +429,9 @@ function DiagramGridLayout({ studyUuid, showInSpreadsheet, showGrid, visible }: 
                 onLayoutSave={debouncedGridLayoutSave}
             />
             <ResponsiveGridLayout
-                innerRef={responsiveGridLayoutRef}
+                ref={responsiveGridLayoutRef} // the provided innerRef prop is bugged (see https://github.com/react-grid-layout/react-grid-layout/issues/1444)
+                autoSize={false} // This props should do the resizing for us but is bugged too. We force it to false, otherwise the grid has strange behaviors.
+                onResize={handleResize} // We manually scroll down when resizing downward. This is a workaround due to the bugs.
                 className="layout"
                 breakpoints={GRID_CONFIG.breakpoints}
                 cols={GRID_CONFIG.cols}
@@ -435,7 +439,6 @@ function DiagramGridLayout({ studyUuid, showInSpreadsheet, showGrid, visible }: 
                 compactType={'vertical'}
                 onLayoutChange={handleLayoutChange}
                 onResizeStop={handleResizeStop}
-                onResize={handleResize}
                 onBreakpointChange={handleBreakpointChange}
                 layouts={layouts}
                 style={{
@@ -457,7 +460,6 @@ function DiagramGridLayout({ studyUuid, showInSpreadsheet, showGrid, visible }: 
                     }
                     handleDragStop(layout);
                 }}
-                autoSize={false} // otherwise the grid has strange behavior
                 resizeHandle={<CustomResizeHandle />}
             >
                 {Object.values(diagrams).map((diagram) => {
