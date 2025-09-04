72cbb50b9671946d83c1347b50a8cfaa22ac5b03
achour94 <berrahmaachour@gmail.com>
2025-09-04 12:04:22 +0200

    Add save logs pagination functionality and reset actions
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

---
+++ src/components/report-viewer/use-logs-pagination.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setLogsResultPagination } from 'redux/actions';
+import { AppState } from 'redux/reducer';
+import { LogsPaginationConfig } from 'types/custom-aggrid-types';
+import { ComputingAndNetworkModificationType } from 'utils/report/report.type';
+import { LOGS_PAGINATION_STORE_FIELD } from 'utils/store-sort-filter-fields';
+
+export const useLogsPagination = (reportType: ComputingAndNetworkModificationType) => {
+    const dispatch = useDispatch();
+
+    const pagination = useSelector((state: AppState) => state[LOGS_PAGINATION_STORE_FIELD][reportType]);
+
+    const setPagination = useCallback(
+        (paginationConfig: LogsPaginationConfig) => {
+            dispatch(setLogsResultPagination(reportType, paginationConfig));
+        },
+        [dispatch, reportType]
+    );
+
+    return {
+        pagination,
+        setPagination,
+    };
+};
+
+export const useLogsPaginationResetByType = () => {
+    const dispatch = useDispatch();
+    const allLogsPagination = useSelector((state: AppState) => state[LOGS_PAGINATION_STORE_FIELD]);
+
+    return useCallback(
+        (computingType: ComputingAndNetworkModificationType) => {
+            const currentPagination = allLogsPagination[computingType];
+            dispatch(
+                setLogsResultPagination(computingType, {
+                    page: 0,
+                    rowsPerPage: currentPagination?.rowsPerPage || 25,
+                })
+            );
+        },
+        [dispatch, allLogsPagination]
+    );
+};
