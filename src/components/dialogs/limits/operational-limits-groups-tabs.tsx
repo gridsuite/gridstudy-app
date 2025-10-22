96302d3d42adf7ef9e698c601b18435532dad8c2
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-10-22 11:19:11 +0200

    Add properties modification on line and two windings transformers dialog
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/components/dialogs/limits/operational-limits-groups-tabs.tsx
+++ src/components/dialogs/limits/operational-limits-groups-tabs.tsx
@@ -339,11 +339,9 @@ export const OperationalLimitsGroupsTabs = forwardRef<any, OperationalLimitsGrou
                                                     ''
                                                 )}
                                             </Stack>
-                                            {!isAModification && (
-                                                <LimitsPropertiesStack
-                                                    formName={`${parentFormName}.${OPERATIONAL_LIMITS_GROUPS}[${index}].${LIMITS_PROPERTIES}`}
-                                                />
-                                            )}
+                                            <LimitsPropertiesStack
+                                                formName={`${parentFormName}.${OPERATIONAL_LIMITS_GROUPS}[${index}].${LIMITS_PROPERTIES}`}
+                                            />
                                         </Stack>
 
                                         {(index === hoveredRowIndex || index === activatedByMenuTabIndex) && (
