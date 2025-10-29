b181a355597dad4b113463b2f9929b18b0d3dbb9
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-10-29 16:36:46 +0100

    first commit

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -5,22 +5,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-export function getNominalVoltageColor(nominalVoltage: number): number[] {
-    if (nominalVoltage >= 300) {
-        return [255, 0, 0];
-    } else if (nominalVoltage >= 180 && nominalVoltage < 300) {
-        return [34, 139, 34];
-    } else if (nominalVoltage >= 120 && nominalVoltage < 180) {
-        return [1, 175, 175];
-    } else if (nominalVoltage >= 70 && nominalVoltage < 120) {
-        return [204, 85, 0];
-    } else if (nominalVoltage >= 50 && nominalVoltage < 70) {
-        return [160, 32, 240];
-    } else if (nominalVoltage >= 30 && nominalVoltage < 50) {
-        return [255, 130, 144];
-    } else {
-        return [171, 175, 40];
-    }
-}
+import { getNominalVoltageInterval } from './constants';
+
+export const getNominalVoltageColor = (voltageValue: number): number[] | undefined => {
+    return getNominalVoltageInterval(voltageValue)?.color;
+};
 
 export const INVALID_LOADFLOW_OPACITY = 0.2;
