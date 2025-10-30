3ca9095de89d7361fc004217650e73109cec92db
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-10-30 09:17:13 +0100

    add color to base voltages constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,16 +11,73 @@ export interface VoltageLevelInterval {
     maxValue: number;
     label: string;
     color: number[];
+    profile: string;
 }
 
 export const BASE_VOLTAGES: VoltageLevelInterval[] = [
-    { name: 'vl300to500', vlValue: 400, minValue: 300, maxValue: Infinity, label: '400 kV', color: [255, 0, 0] },
-    { name: 'vl200to300', vlValue: 225, minValue: 200, maxValue: 300, label: '225 kV', color: [34, 139, 34] },
-    { name: 'vl100to200', vlValue: 150, minValue: 100, maxValue: 200, label: '150 kV', color: [1, 175, 175] },
-    { name: 'vl70to100', vlValue: 90, minValue: 70, maxValue: 100, label: '90 kV', color: [204, 85, 0] },
-    { name: 'vl50to70', vlValue: 63, minValue: 50, maxValue: 70, label: '63 kV', color: [160, 32, 240] },
-    { name: 'vl40to50', vlValue: 42, minValue: 40, maxValue: 50, label: '42 kV', color: [255, 130, 144] },
-    { name: 'vl0to40', vlValue: 20, minValue: 0, maxValue: 40, label: 'HTA', color: [171, 175, 40] },
+    {
+        name: 'vl300to500',
+        vlValue: 400,
+        minValue: 300,
+        maxValue: Infinity,
+        label: '400 kV',
+        color: [255, 0, 0],
+        profile: 'Default',
+    },
+    {
+        name: 'vl200to300',
+        vlValue: 225,
+        minValue: 200,
+        maxValue: 300,
+        label: '225 kV',
+        color: [34, 139, 34],
+        profile: 'Default',
+    },
+    {
+        name: 'vl100to200',
+        vlValue: 150,
+        minValue: 100,
+        maxValue: 200,
+        label: '150 kV',
+        color: [1, 175, 175],
+        profile: 'Default',
+    },
+    {
+        name: 'vl70to100',
+        vlValue: 90,
+        minValue: 70,
+        maxValue: 100,
+        label: '90 kV',
+        color: [204, 85, 0],
+        profile: 'Default',
+    },
+    {
+        name: 'vl50to70',
+        vlValue: 63,
+        minValue: 50,
+        maxValue: 70,
+        label: '63 kV',
+        color: [160, 32, 240],
+        profile: 'Default',
+    },
+    {
+        name: 'vl40to50',
+        vlValue: 42,
+        minValue: 40,
+        maxValue: 50,
+        label: '42 kV',
+        color: [255, 130, 144],
+        profile: 'Default',
+    },
+    {
+        name: 'vl0to40',
+        vlValue: 20,
+        minValue: 0,
+        maxValue: 40,
+        label: 'HTA',
+        color: [171, 175, 40],
+        profile: 'Default',
+    },
 ];
 
 export const MAX_VOLTAGE = 500;
