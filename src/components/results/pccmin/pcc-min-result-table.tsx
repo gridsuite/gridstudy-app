a1f36a04921c5a53421bf3d30e58d1d8378a2e2b
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-11-05 16:59:06 +0100

    refactor code
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/components/results/pccmin/pcc-min-result-table.tsx
+++ src/components/results/pccmin/pcc-min-result-table.tsx
@@ -8,22 +8,15 @@
 import { FunctionComponent, useCallback, useMemo, useRef } from 'react';
 import { useIntl } from 'react-intl';
 import { Box } from '@mui/material';
-import { GridReadyEvent, RowDataUpdatedEvent } from 'ag-grid-community';
 import { getNoRowsMessage, getRows, useIntlResultStatusMessages } from '../../utils/aggrid-rows-handler';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../../redux/reducer';
 import { DefaultCellRenderer } from '../../custom-aggrid/cell-renderers';
 import { makeAgGridCustomHeaderColumn } from '../../custom-aggrid/utils/custom-aggrid-header-utils';
 import { ComputingType } from '@gridsuite/commons-ui';
-import { CustomAggridComparatorFilter } from '../../custom-aggrid/custom-aggrid-filters/custom-aggrid-comparator-filter';
-import { PCCMIN_ANALYSIS_RESULT_SORT_STORE, PCCMIN_RESULT } from '../../../utils/store-sort-filter-fields';
-import {
-    FilterType as AgGridFilterType,
-    numericFilterParams,
-    textFilterParams,
-} from '../../../types/custom-aggrid-types';
+
 import { ColumnContext } from '../../custom-aggrid/custom-aggrid-filters/custom-aggrid-filter.type';
-import { PccMinResultTableProps, SinglePccMinResultInfos } from './pcc-min-result.type';
+import { getPccMinColumns, PccMinResultTableProps } from './pcc-min-result.type';
 import { AgGridReact } from 'ag-grid-react';
 import { RenderTableAndExportCsv } from 'components/utils/renderTable-ExportCsv';
 import { RESULTS_LOADING_DELAY } from 'components/network/constants';
@@ -35,108 +28,11 @@ const PccMinResultTable: FunctionComponent<PccMinResultTableProps> = ({ result, 
     const pccMinStatus = useSelector((state: AppState) => state.computingStatus[ComputingType.PCC_MIN]);
     const gridRef = useRef<AgGridReact>(null);
 
-    const columns = useMemo(() => {
-        const data = <T,>(value: T) => value;
+    const columns = useMemo(() => getPccMinColumns(intl, onFilter), [intl, onFilter]);
 
-        const sortParams: ColumnContext['sortParams'] = {
-            table: PCCMIN_ANALYSIS_RESULT_SORT_STORE,
-            tab: PCCMIN_RESULT,
-        };
+    const statusMessage = useIntlResultStatusMessages(intl, true, filters.length > 0);
 
-        const filterParams = {
-            type: AgGridFilterType.PccMin,
-            tab: PCCMIN_RESULT,
-            updateFilterCallback: onFilter,
-        };
-
-        const inputFilterParams = (
-            filterDefinition: Pick<
-                Required<ColumnContext>['filterComponentParams']['filterParams'],
-                'dataType' | 'comparators'
-            >
-        ) => {
-            return {
-                filterComponent: CustomAggridComparatorFilter,
-                filterComponentParams: {
-                    filterParams: {
-                        ...filterDefinition,
-                        ...filterParams,
-                    },
-                },
-            };
-        };
-
-        return [
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'Bus' }),
-                colId: 'busId',
-                field: 'busId',
-                context: {
-                    ...data({ sortParams, ...inputFilterParams(textFilterParams) }),
-                },
-                minWidth: 180,
-            }),
-
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'Contingency' }),
-                colId: 'limitingEquipment',
-                field: 'limitingEquipment',
-                context: {
-                    ...data({ sortParams, ...inputFilterParams(textFilterParams) }),
-                },
-
-                minWidth: 180,
-            }),
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'PccMinTri' }),
-                colId: 'pccMinTri',
-                field: 'pccMinTri',
-                context: {
-                    numeric: true,
-                    fractionDigits: 2,
-                    sortParams,
-                    ...inputFilterParams(numericFilterParams),
-                },
-            }),
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'IccMinTri' }),
-                colId: 'iccMinTri',
-                field: 'iccMinTri',
-                context: {
-                    numeric: true,
-                    fractionDigits: 2,
-                    sortParams,
-                    ...inputFilterParams(numericFilterParams),
-                },
-            }),
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'xOhm' }),
-                colId: 'x',
-                field: 'x',
-                context: {
-                    numeric: true,
-                    fractionDigits: 2,
-                    sortParams,
-                    ...inputFilterParams(numericFilterParams),
-                },
-            }),
-            makeAgGridCustomHeaderColumn({
-                headerName: intl.formatMessage({ id: 'rOhm' }),
-                colId: 'r',
-                field: 'r',
-                context: {
-                    numeric: true,
-                    fractionDigits: 2,
-                    sortParams,
-                    ...inputFilterParams(numericFilterParams),
-                },
-            }),
-        ];
-    }, [onFilter, intl]);
-
-    const messages = useIntlResultStatusMessages(intl, true, filters.length > 0);
-
-    const defaultColDef = useMemo(
+    const pccMinDefaultColDef = useMemo(
         () => ({
             suppressMovable: true,
             resizable: true,
@@ -147,7 +43,7 @@ const PccMinResultTable: FunctionComponent<PccMinResultTableProps> = ({ result, 
     );
 
     const rowsToShow = getRows(result, pccMinStatus);
-    const message = getNoRowsMessage(messages, result, pccMinStatus, !isFetching);
+    const noRowMessage = getNoRowsMessage(statusMessage, result, pccMinStatus, !isFetching);
 
     const openPccMinLoader = useOpenLoaderShortWait({
         isLoading: pccMinStatus === RunningStatus.RUNNING || isFetching,
@@ -159,10 +55,10 @@ const PccMinResultTable: FunctionComponent<PccMinResultTableProps> = ({ result, 
                 <RenderTableAndExportCsv
                     gridRef={gridRef}
                     columns={columns}
-                    defaultColDef={defaultColDef}
+                    defaultColDef={pccMinDefaultColDef}
                     tableName={intl.formatMessage({ id: 'Results' })}
                     rows={rowsToShow}
-                    overlayNoRowsTemplate={message}
+                    overlayNoRowsTemplate={noRowMessage}
                     skipColumnHeaders={false}
                     showLinearProgress={openPccMinLoader}
                 />
--- src/components/results/pccmin/pcc-min-result.type.ts
+++ src/components/results/pccmin/pcc-min-result.type.ts
@@ -6,9 +6,19 @@
  */
 
 import type { UUID } from 'node:crypto';
-import { FilterConfig } from 'types/custom-aggrid-types';
+import {
+    FilterConfig,
+    numericFilterParams,
+    textFilterParams,
+    FilterType as AgGridFilterType,
+} from 'types/custom-aggrid-types';
 import { GlobalFilters } from '../common/global-filter/global-filter-types';
 import { Page, Selector } from '../common/utils';
+import { makeAgGridCustomHeaderColumn } from 'components/custom-aggrid/utils/custom-aggrid-header-utils';
+import { ColumnContext } from 'components/custom-aggrid/custom-aggrid-filters/custom-aggrid-filter.type';
+import { CustomAggridComparatorFilter } from 'components/custom-aggrid/custom-aggrid-filters/custom-aggrid-comparator-filter';
+import { PCCMIN_ANALYSIS_RESULT_SORT_STORE, PCCMIN_RESULT } from 'utils/store-sort-filter-fields';
+import { IntlShape } from 'react-intl';
 
 export interface SinglePccMinResultInfos {
     singlePccMinResultUuid: string;
@@ -51,4 +61,73 @@ export const FROM_COLUMN_TO_FIELD_PCC_MIN: Record<string, string> = {
     limitingEquipment: 'limitingEquipment',
     x: 'x',
     r: 'r',
+};
+
+export const getPccMinColumns = (intl: IntlShape, onFilter: (filters: any) => void) => {
+    const sortParams: ColumnContext['sortParams'] = {
+        table: PCCMIN_ANALYSIS_RESULT_SORT_STORE,
+        tab: PCCMIN_RESULT,
+    };
+
+    const filterParams = {
+        type: AgGridFilterType.PccMin,
+        tab: PCCMIN_RESULT,
+        updateFilterCallback: onFilter,
+    };
+
+    const inputFilterParams = (
+        filterDefinition: Pick<
+            Required<ColumnContext>['filterComponentParams']['filterParams'],
+            'dataType' | 'comparators'
+        >
+    ) => ({
+        filterComponent: CustomAggridComparatorFilter,
+        filterComponentParams: {
+            filterParams: {
+                ...filterDefinition,
+                ...filterParams,
+            },
+        },
+    });
+
+    return [
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'Bus' }),
+            colId: 'busId',
+            field: 'busId',
+            context: { sortParams, ...inputFilterParams(textFilterParams) },
+            minWidth: 180,
+        }),
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'Contingency' }),
+            colId: 'limitingEquipment',
+            field: 'limitingEquipment',
+            context: { sortParams, ...inputFilterParams(textFilterParams) },
+            minWidth: 180,
+        }),
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'PccMinTri' }),
+            colId: 'pccMinTri',
+            field: 'pccMinTri',
+            context: { numeric: true, fractionDigits: 2, sortParams, ...inputFilterParams(numericFilterParams) },
+        }),
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'IccMinTri' }),
+            colId: 'iccMinTri',
+            field: 'iccMinTri',
+            context: { numeric: true, fractionDigits: 2, sortParams, ...inputFilterParams(numericFilterParams) },
+        }),
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'xOhm' }),
+            colId: 'x',
+            field: 'x',
+            context: { numeric: true, fractionDigits: 2, sortParams, ...inputFilterParams(numericFilterParams) },
+        }),
+        makeAgGridCustomHeaderColumn({
+            headerName: intl.formatMessage({ id: 'rOhm' }),
+            colId: 'r',
+            field: 'r',
+            context: { numeric: true, fractionDigits: 2, sortParams, ...inputFilterParams(numericFilterParams) },
+        }),
+    ];
 };
