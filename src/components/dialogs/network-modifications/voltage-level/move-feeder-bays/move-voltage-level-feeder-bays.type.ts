476d3bc60a64f9b8ce252e6553e22f0876e3d472
Ghazoua Rhili <ghazwarhili@gmail.com>
2025-11-07 14:13:32 +0100

    remove duplication connection position check (#3423)
    Signed-off-by: Rehili Ghazwa <ghazwarhili@gmail.com>

--- src/components/dialogs/network-modifications/voltage-level/move-feeder-bays/move-voltage-level-feeder-bays.type.ts
+++ src/components/dialogs/network-modifications/voltage-level/move-feeder-bays/move-voltage-level-feeder-bays.type.ts
@@ -13,7 +13,7 @@ export type FeederBaysFormInfos = {
     connectionSide: string | null;
     connectionName: string | null;
     connectionDirection: string | null;
-    connectionPosition: string | null;
+    connectionPosition: number | null;
     isRemoved: boolean;
     rowId: string | null;
 };
