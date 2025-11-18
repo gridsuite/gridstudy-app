66a61b31ba63498dc28506f26615eac660c2e426
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-11-18 18:02:51 +0100

    fix
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/components/dialogs/parameters/use-get-pcc-min-parameters.ts
+++ src/components/dialogs/parameters/use-get-pcc-min-parameters.ts
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../../redux/reducer';
 import {
