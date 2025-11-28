eed6aad4ff26d0c2c4e35271abbd44ad0925187b
Etienne LESOT <etienne.lesot@rte-france.com>
2025-11-28 16:56:15 +0100

    fix
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
+++ src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
@@ -47,7 +47,7 @@ import {
 } from 'components/utils/field-constants';
 import yup from 'components/utils/yup-config';
 import PropTypes from 'prop-types';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { ModificationDialog } from 'components/dialogs/commons/modificationDialog';
 
@@ -197,7 +197,7 @@ const VoltageLevelCreationDialog = ({
 }) => {
     const currentNodeUuid = currentNode?.id;
     const { snackError, snackWarning } = useSnackMessage();
-
+    const [isAttachementPointModificationState] = useState(isAttachementPointModification);
     const formMethods = useForm({
         defaultValues: emptyFormData,
         resolver: yupResolver(formSchema),
@@ -205,10 +205,12 @@ const VoltageLevelCreationDialog = ({
 
     const { reset, setValue, getValues, watch, trigger, subscribe } = formMethods;
 
-    if (isAttachementPointModification) {
-        setValue(ADD_SUBSTATION_CREATION, true);
-        setValue(NOMINAL_V, 0); // need a value to pass yup but not taken into account for attachmentLine
-    }
+    useEffect(() => {
+        if (isAttachementPointModificationState) {
+            setValue(ADD_SUBSTATION_CREATION, true);
+            setValue(NOMINAL_V, 0); // need a value to pass yup but not taken into account for attachmentLine
+        }
+    }, [isAttachementPointModificationState, setValue]);
     // Watch LOW_VOLTAGE_LIMIT changed
     useEffect(() => {
         const callback = subscribe({
