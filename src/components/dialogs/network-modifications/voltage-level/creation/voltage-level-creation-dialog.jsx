cd16e1c2a33e6bf761c19420848ed447f2510512
Etienne LESOT <etienne.lesot@rte-france.com>
2025-12-02 09:22:50 +0100

    fix sonar
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
+++ src/components/dialogs/network-modifications/voltage-level/creation/voltage-level-creation-dialog.jsx
@@ -403,7 +403,7 @@ const VoltageLevelCreationDialog = ({
                 onClear={clear}
                 onSave={onSubmit}
                 maxWidth={'md'}
-                titleId={overrideTitle ? overrideTitle : 'CreateVoltageLevel'}
+                titleId={overrideTitle || 'CreateVoltageLevel'}
                 searchCopy={searchCopy}
                 open={open}
                 isDataFetching={isUpdate && editDataFetchStatus === FetchStatus.RUNNING}
