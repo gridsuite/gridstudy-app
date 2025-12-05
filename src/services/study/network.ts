021de0411df1b2c80f86eabd4d69d29c886e9cf1
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-12-05 10:30:11 +0100

    fix

--- src/services/study/network.ts
+++ src/services/study/network.ts
@@ -1,8 +1,9 @@
 /**
- * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023-2025, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
  */
 
 import type { UUID } from 'node:crypto';
@@ -12,7 +13,6 @@ import {
     backendFetchText,
     EquipmentType,
     ExtendedEquipmentType,
-    type GsLang,
     type Identifiable,
 } from '@gridsuite/commons-ui';
 import type { MapHvdcLine, MapLine, MapSubstation, MapTieLine } from '@powsybl/network-viewer';
