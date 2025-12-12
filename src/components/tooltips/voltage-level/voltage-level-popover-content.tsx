b9463a9b7f50de12e98b7a294cd16bcf4c40ace0
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2025-12-12 10:26:05 +0100

    fix: copyright
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/components/tooltips/voltage-level/voltage-level-popover-content.tsx
+++ src/components/tooltips/voltage-level/voltage-level-popover-content.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { VoltageLevelTooltipInfos } from '../equipment-popover-type';
 import { Grid } from '@mui/material';
 import { VoltageLevelPopoverInfos } from './voltage-level-popover-infos';
