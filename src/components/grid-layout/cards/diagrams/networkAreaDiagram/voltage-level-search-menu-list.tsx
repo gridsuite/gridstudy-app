f44474125924f236d9fb31ec9ca4226a4e429dcd
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-01-08 10:27:09 +0100

    fix: copyright + add comment
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/components/grid-layout/cards/diagrams/networkAreaDiagram/voltage-level-search-menu-list.tsx
+++ src/components/grid-layout/cards/diagrams/networkAreaDiagram/voltage-level-search-menu-list.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import * as React from 'react';
 import { List, RowComponentProps } from 'react-window';
 
@@ -6,11 +12,12 @@ function MenuRow({
     style,
     items,
 }: RowComponentProps<{ items: React.ReactElement<React.HTMLAttributes<HTMLLIElement>>[] }>) {
-    const option = items[index];
+    const optionComponent = items[index];
 
-    return React.cloneElement(option, {
+    // this seems to be the way to customize an existing ReactElement by adding some style to it
+    return React.cloneElement(optionComponent, {
         style: {
-            ...(option.props.style ?? {}),
+            ...optionComponent.props.style,
             ...style,
         },
     });
