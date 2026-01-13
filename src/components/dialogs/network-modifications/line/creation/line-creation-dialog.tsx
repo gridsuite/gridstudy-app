fc4bf1449d6c6fbc9fd49a5386121a4194152586
Franck LECUYER <franck.lecuyer@rte-france.com>
2026-01-13 15:48:52 +0100

    Fix sonar issue on duplicated lines
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/dialogs/network-modifications/line/creation/line-creation-dialog.tsx
+++ src/components/dialogs/network-modifications/line/creation/line-creation-dialog.tsx
@@ -98,10 +98,7 @@ import { CurrentLimitsInfo } from '../../../line-types-catalog/line-catalog.type
 import { CurrentLimitsData } from '../../../../../services/study/network-map.type';
 import { DeepNullable } from '../../../../utils/ts-utils';
 import { Connectivity } from 'components/dialogs/connectivity/connectivity.type';
-import {
-    OperationalLimitsGroupFormSchema,
-    OperationalLimitsGroupsFormSchema,
-} from '../../../limits/operational-limits-groups-types';
+import { OperationalLimitsGroupFormSchema } from '../../../limits/operational-limits-groups-types';
 
 interface LineCreationFormData {
     [TAB_HEADER]: {
@@ -184,51 +181,11 @@ interface LineInfo {
     properties: Record<string, string>;
 }
 
-interface LineCreationFormInfos {
+interface LineCreationFormInfos extends LineModificationFormInfos {
     tabHeader: {
         equipmentId: string;
         equipmentName?: string;
     };
-    studyUuid: string;
-    nodeUuid: UUID;
-    modificationUuid: string;
-    lineId: string;
-    lineName: string | null;
-    r: number | null;
-    x: number | null;
-    g1: number | null;
-    b1: number | null;
-    g2: number | null;
-    b2: number | null;
-    operationalLimitsGroups: OperationalLimitsGroup[];
-    selectedOperationalLimitsGroupId1: string | null;
-    selectedOperationalLimitsGroupId2: string | null;
-    voltageLevelId1: string;
-    busOrBusbarSectionId1: string;
-    voltageLevelId2: string;
-    busOrBusbarSectionId2: string;
-    connectionName1: string | null;
-    connectionName2: string | null;
-    connectionDirection1: string | null;
-    connectionDirection2: string | null;
-    connectionPosition1: string | null;
-    connectionPosition2: string | null;
-    connected1: boolean;
-    connected2: boolean;
-    properties: Property[] | null | undefined;
-    p1MeasurementValue: number | null;
-    p1MeasurementValidity: boolean | null;
-    q1MeasurementValue: number | null;
-    q1MeasurementValidity: boolean | null;
-    p2MeasurementValue: number | null;
-    p2MeasurementValidity: boolean | null;
-    q2MeasurementValue: number | null;
-    q2MeasurementValidity: boolean | null;
-    connectivity: any;
-    AdditionalProperties: any;
-    characteristics: any;
-    stateEstimation: any;
-    limits: OperationalLimitsGroupsFormSchema;
 }
 
 interface LineCreationDialogProps {
