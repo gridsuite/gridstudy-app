d70f84211109f1a2ea4c1a6db32e13bdb85ae2b1
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-01-15 14:46:17 +0100

    hide global filters panel when switching
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/results/common/global-filter/global-filter-paper.tsx
+++ src/components/results/common/global-filter/global-filter-paper.tsx
@@ -174,7 +174,7 @@ function GlobalFilterPaper({ children, autocompleteRef }: Readonly<GlobalFilterP
     return (
         <>
             <ClickAwayListener
-                mouseEvent="onMouseDown"
+                mouseEvent="onClick"
                 onClickAway={(event) => {
                     const target = event.target as HTMLElement;
                     // The autocomplete is considered "outside" of the dropdown
