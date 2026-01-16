7e56690071397db4bbb42f6e5dbdfef3acb04bc4
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-01-16 15:02:29 +0100

    use default mouseEvent
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/results/common/global-filter/global-filter-paper.tsx
+++ src/components/results/common/global-filter/global-filter-paper.tsx
@@ -174,7 +174,6 @@ function GlobalFilterPaper({ children, autocompleteRef }: Readonly<GlobalFilterP
     return (
         <>
             <ClickAwayListener
-                mouseEvent="onClick"
                 onClickAway={(event) => {
                     const target = event.target as HTMLElement;
                     // The autocomplete is considered "outside" of the dropdown
