e7ac61eda4c57c4ff8ad19ba77639d59f03395f3
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-02-02 13:47:57 +0100

    merge main into branch

--- src/components/results/shortcircuit/shortcircuit-analysis-result-tab.tsx
+++ src/components/results/shortcircuit/shortcircuit-analysis-result-tab.tsx
@@ -182,7 +182,7 @@ export const ShortCircuitAnalysisResultTab: FunctionComponent<ShortCircuitAnalys
                             csvHeader={csvHeader}
                             analysisType={tabIndex}
                             disabled={isCsvButtonDisabled}
-                            globalFilters={globalFilters}
+                            globalFilter={globalFiltersFromState}
                         />
                     )}
             </Box>
