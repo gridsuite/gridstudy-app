191c38a7938ef3b47165f4681e011fc28cde1408
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-02-05 16:27:06 +0100

    fix issue sonar

--- src/components/results/common/global-filter/use-computation-column-filters.ts
+++ src/components/results/common/global-filter/use-computation-column-filters.ts
@@ -15,29 +15,34 @@ export type ComputationResultColumnFilterInfos = {
     columnId: string;
     columnFilterInfos: FilterConfig;
 };
+function toColumnFilterInfos(infos: ComputationResultColumnFilterInfos[] | null): FilterConfig[] {
+    if (!Array.isArray(infos)) {
+        return EMPTY_ARRAY;
+    }
+    return infos.flatMap(mapColumnFilters);
+}
+function mapColumnFilters({ columnId, columnFilterInfos }: ComputationResultColumnFilterInfos): FilterConfig[] {
+    const filters = Array.isArray(columnFilterInfos) ? columnFilterInfos : [columnFilterInfos];
+
+    return filters.map((filter) => ({
+        column: columnId,
+        value: filter.filterValue,
+        type: filter.filterType,
+        dataType: filter.filterDataType,
+        tolerance: filter.filterTolerance ?? undefined,
+    }));
+}
+
 const EMPTY_ARRAY: FilterConfig[] = [];
 export function useComputationColumnFilters(filterType: FilterType, computationSubType: string) {
     const dispatch = useDispatch();
     const studyUuid = useSelector((state: AppState) => state.studyUuid);
     useEffect(() => {
         studyUuid &&
-            getComputationResultColumnFilters(studyUuid, filterType, computationSubType).then(
-                (computationResultColumnFilterInfos: ComputationResultColumnFilterInfos[] | null) => {
-                    const filters: FilterConfig[] = Array.isArray(computationResultColumnFilterInfos)
-                        ? computationResultColumnFilterInfos.flatMap(({ columnId, columnFilterInfos }) => {
-                              const list = Array.isArray(columnFilterInfos) ? columnFilterInfos : [columnFilterInfos];
-                              return list.map((filter) => ({
-                                  column: columnId,
-                                  value: filter.filterValue,
-                                  type: filter.filterType,
-                                  dataType: filter.filterDataType,
-                                  tolerance: filter.filterTolerance ?? undefined,
-                              }));
-                          })
-                        : EMPTY_ARRAY;
-                    dispatch(updateColumnFiltersAction(filterType, computationSubType, filters));
-                }
-            );
+            getComputationResultColumnFilters(studyUuid, filterType, computationSubType).then((infos) => {
+                const filters = toColumnFilterInfos(infos);
+                dispatch(updateColumnFiltersAction(filterType, computationSubType, filters));
+            });
     }, [dispatch, studyUuid, filterType, computationSubType]);
     const filters = useSelector<AppState, FilterConfig[]>(
         (state) => state.computationFilters?.[filterType]?.columnsFilters?.[computationSubType]?.columns ?? EMPTY_ARRAY
