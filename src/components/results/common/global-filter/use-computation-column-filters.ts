dd571df29aa55f211d7568ae6486bfbff713b303
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-02-06 15:31:59 +0100

    add parseFilterValue

--- src/components/results/common/global-filter/use-computation-column-filters.ts
+++ src/components/results/common/global-filter/use-computation-column-filters.ts
@@ -21,12 +21,19 @@ function toColumnFilterInfos(infos: ComputationResultColumnFilterInfos[] | null)
     }
     return infos.flatMap(mapColumnFilters);
 }
+function parseFilterValue(filterValue: string) {
+    const parsed = JSON.parse(filterValue);
+    if (!Array.isArray(parsed)) {
+        return parsed;
+    }
+    return parsed.flatMap((v) => (typeof v === 'string' ? v.split(',').map((s) => s.trim()) : v));
+}
 function mapColumnFilters({ columnId, columnFilterInfos }: ComputationResultColumnFilterInfos): FilterConfig[] {
     const filters = Array.isArray(columnFilterInfos) ? columnFilterInfos : [columnFilterInfos];
 
     return filters.map((filter) => ({
         column: columnId,
-        value: filter.filterValue,
+        value: parseFilterValue(filter.filterValue),
         type: filter.filterType,
         dataType: filter.filterDataType,
         tolerance: filter.filterTolerance ?? undefined,
