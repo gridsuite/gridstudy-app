e31fefe3b9d2916c4e2309f1a96760a32ab734c3
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2026-02-20 11:02:45 +0100

    add nad edge infos
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/styles/light-theme-css-vars.ts
+++ src/styles/light-theme-css-vars.ts
@@ -12,6 +12,9 @@ export const lightThemeCssVars = {
     '.nad-active': {
         fill: '#212121', // Text color of the values and arrows on lines.
     },
+    '.nad-reactive': {
+        fill: '#212121',
+    },
     '.nad-label-box': {
         backgroundColor: '#9c9c9c20',
     },
