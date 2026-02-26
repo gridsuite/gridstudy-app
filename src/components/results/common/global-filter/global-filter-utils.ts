7e28598fe53a94625ef3424277de58ac45dc7fd9
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2026-02-26 12:39:12 +0100

    Fix a Redux circular dependency by moving a function in another file. (#3763)
    Signed-off-by: Florent MILLOT <75525996+flomillot@users.noreply.github.com>

--- src/components/results/common/global-filter/global-filter-utils.ts
+++ src/components/results/common/global-filter/global-filter-utils.ts
@@ -10,8 +10,6 @@ import { FilterType } from '../utils';
 import { fetchStudyMetadata } from '@gridsuite/commons-ui';
 import { IntlShape } from 'react-intl';
 import { UUID } from 'node:crypto';
-import { store } from '../../../../redux/store';
-import { AppState } from '../../../../redux/reducer';
 
 export type GlobalFilterWithoutId = Omit<GlobalFilter, 'id'>;
 
@@ -67,17 +65,4 @@ export async function fetchSubstationPropertiesGlobalFilters(): Promise<{
     return {
         substationPropertiesGlobalFilters: definedSubstationPropertiesGlobalFilters,
     };
-}
-
-/**
- * Reads the selected global filter IDs for the given table key from the store,
- * joins them with the global filter options, and returns the resolved `GlobalFilter[]`.
- * Use this when you need the selected filters at a T time, e.g. when fetching the data.
- * Otherwise, use `useSelectedGlobalFilters` which subscribes to the store.
- */
-export function getSelectedGlobalFilters(tableKey: string): GlobalFilter[] {
-    const state = store.getState() as AppState;
-    const filterIds = state.tableFilters.globalFilters[tableKey] ?? [];
-    const globalFilterOptions = state.globalFilterOptions;
-    return filterIds.map((id) => globalFilterOptions.find((opt) => opt.id === id)).filter((f) => f !== undefined);
 }
--- src/components/results/common/global-filter/use-selected-global-filters.ts
+++ src/components/results/common/global-filter/use-selected-global-filters.ts
@@ -7,8 +7,8 @@
 
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { AppState } from '../../../../redux/reducer';
 import { GlobalFilter } from './global-filter-types';
+import { RootState, store } from '../../../../redux/store';
 
 /**
  * Subscribes to the store and returns the selected global filters for the given table key.
@@ -16,8 +16,8 @@ import { GlobalFilter } from './global-filter-types';
  * Otherwise, use `getSelectedGlobalFilters` which get the current state of the selected filters.
  */
 export function useSelectedGlobalFilters(tableKey: string): GlobalFilter[] {
-    const globalFilterOptions = useSelector((state: AppState) => state.globalFilterOptions);
-    const selectedFilterIds = useSelector((state: AppState) => state.tableFilters.globalFilters[tableKey]);
+    const globalFilterOptions = useSelector((state: RootState) => state.globalFilterOptions);
+    const selectedFilterIds = useSelector((state: RootState) => state.tableFilters.globalFilters[tableKey]);
 
     return useMemo(
         () =>
@@ -28,4 +28,17 @@ export function useSelectedGlobalFilters(tableKey: string): GlobalFilter[] {
                 : [],
         [selectedFilterIds, globalFilterOptions]
     );
+}
+
+/**
+ * Reads the selected global filter IDs for the given table key from the store,
+ * joins them with the global filter options, and returns the resolved `GlobalFilter[]`.
+ * Use this when you need the selected filters at a T time, e.g. when fetching the data.
+ * Otherwise, use `useSelectedGlobalFilters` which subscribes to the store.
+ */
+export function getSelectedGlobalFilters(tableKey: string): GlobalFilter[] {
+    const state = store.getState() as RootState;
+    const filterIds = state.tableFilters.globalFilters[tableKey] ?? [];
+    const globalFilterOptions = state.globalFilterOptions;
+    return filterIds.map((id) => globalFilterOptions.find((opt) => opt.id === id)).filter((f) => f !== undefined);
 }
