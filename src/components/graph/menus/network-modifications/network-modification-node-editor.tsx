179dae967c78fd022900f70370ec6c0dd7903645
Hugo Marcellin <marcellinhugo@orange.fr>
2026-04-16 16:41:34 +0200

    Fix composite overwrite (#3886)
    Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

--- src/components/graph/menus/network-modifications/network-modification-node-editor.tsx
+++ src/components/graph/menus/network-modifications/network-modification-node-editor.tsx
@@ -128,6 +128,7 @@ const nonEditableModificationTypes = new Set([
     'EQUIPMENT_ATTRIBUTE_MODIFICATION',
     'GROOVY_SCRIPT',
     'OPERATING_STATUS_MODIFICATION',
+    'COMPOSITE_MODIFICATION',
 ]);
 
 const isEditableModification = (modif: NetworkModificationMetadata) => {
