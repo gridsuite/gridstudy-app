42d4eaf64fef94e813295767426b256f7ebcb44c
David BRAQUART <david.braquart@rte-france.com>
2026-04-20 14:32:17 +0200

    rev remark: fix error management
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/dialogs/network-modifications/generator/modification/generator-modification-form.tsx
+++ src/components/dialogs/network-modifications/generator/modification/generator-modification-form.tsx
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { useTabsWithError } from '@gridsuite/commons-ui';
+import { FieldConstants } from '@gridsuite/commons-ui';
 import { Grid } from '@mui/material';
 import { GeneratorDialogTab } from './generatorTabs.utils';
 import { GeneratorDialogHeader, GeneratorDialogHeaderProps } from './GeneratorDialogHeader';
@@ -13,6 +13,26 @@ import { GeneratorDialogTabs } from './GeneratorDialogTabs';
 import { GeneratorDialogTabsContent, GeneratorDialogTabsContentProps } from './GeneratorDialogTabsContent';
 import { UUID } from 'node:crypto';
 import { CurrentTreeNode } from 'components/graph/tree-node.type';
+import { useEffect, useState } from 'react';
+import { useFormState } from 'react-hook-form';
+import {
+    DROOP,
+    FORCED_OUTAGE_RATE,
+    FREQUENCY_REGULATION,
+    MARGINAL_COST,
+    MAXIMUM_ACTIVE_POWER,
+    MINIMUM_ACTIVE_POWER,
+    PLANNED_ACTIVE_POWER_SET_POINT,
+    PLANNED_OUTAGE_RATE,
+    Q_PERCENT,
+    RATED_NOMINAL_POWER,
+    REACTIVE_LIMITS,
+    TRANSFORMER_REACTANCE,
+    TRANSIENT_REACTANCE,
+    VOLTAGE_REGULATION,
+    VOLTAGE_REGULATION_TYPE,
+    VOLTAGE_SET_POINT,
+} from '../../../../utils/field-constants';
 
 interface GeneratorModificationFormProps
     extends GeneratorDialogHeaderProps,
@@ -33,10 +53,54 @@ export default function GeneratorModificationForm({
     PositionDiagramPane,
     equipmentId,
 }: Readonly<GeneratorModificationFormProps>) {
-    const { tabIndex, setTabIndex, tabIndexesWithError } = useTabsWithError<GeneratorDialogTab>(
-        {},
-        GeneratorDialogTab.CONNECTIVITY_TAB
-    );
+    const [tabIndexesWithError, setTabIndexesWithError] = useState<number[]>([]);
+    const [tabIndex, setTabIndex] = useState<number>(GeneratorDialogTab.CONNECTIVITY_TAB);
+
+    const { errors } = useFormState();
+
+    useEffect(() => {
+        let tabsInError: number[] = [];
+        if (errors?.[FieldConstants.CONNECTIVITY] !== undefined) {
+            tabsInError.push(GeneratorDialogTab.CONNECTIVITY_TAB);
+        }
+        if (
+            // setpoints
+            errors?.[FieldConstants.ACTIVE_POWER_SET_POINT] !== undefined ||
+            errors?.[FieldConstants.REACTIVE_POWER_SET_POINT] !== undefined ||
+            errors?.[VOLTAGE_REGULATION] !== undefined ||
+            errors?.[VOLTAGE_SET_POINT] !== undefined ||
+            errors?.[VOLTAGE_REGULATION_TYPE] !== undefined ||
+            errors?.[Q_PERCENT] !== undefined ||
+            errors?.[FREQUENCY_REGULATION] !== undefined ||
+            errors?.[DROOP] !== undefined ||
+            // limits
+            errors?.[MINIMUM_ACTIVE_POWER] !== undefined ||
+            errors?.[MAXIMUM_ACTIVE_POWER] !== undefined ||
+            errors?.[RATED_NOMINAL_POWER] !== undefined ||
+            errors?.[REACTIVE_LIMITS] !== undefined
+        ) {
+            tabsInError.push(GeneratorDialogTab.SETPOINTS_AND_LIMITS_TAB);
+        }
+        if (
+            errors?.[TRANSIENT_REACTANCE] !== undefined ||
+            errors?.[TRANSFORMER_REACTANCE] !== undefined ||
+            errors?.[PLANNED_ACTIVE_POWER_SET_POINT] !== undefined ||
+            errors?.[MARGINAL_COST] !== undefined ||
+            errors?.[PLANNED_OUTAGE_RATE] !== undefined ||
+            errors?.[FORCED_OUTAGE_RATE] !== undefined
+        ) {
+            tabsInError.push(GeneratorDialogTab.SPECIFIC_TAB);
+        }
+        if (errors?.[FieldConstants.ADDITIONAL_PROPERTIES] !== undefined) {
+            tabsInError.push(GeneratorDialogTab.ADDITIONAL_INFORMATION_TAB);
+        }
+        if (tabsInError.length > 0) {
+            setTabIndex((currentTabIndex) => {
+                return tabsInError.includes(currentTabIndex) ? currentTabIndex : tabsInError[0];
+            });
+        }
+        setTabIndexesWithError(tabsInError);
+    }, [errors]);
 
     return (
         <Grid container direction="column" spacing={2}>
