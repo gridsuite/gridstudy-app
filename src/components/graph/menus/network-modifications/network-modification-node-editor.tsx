efb0438f28aebe27371dd53c2a31c9c2caf05b99
Hugo Marcellin <hugo.marcelin@rte-france.com>
2026-04-28 11:54:36 +0200

    Use constant

--- src/components/graph/menus/network-modifications/network-modification-node-editor.tsx
+++ src/components/graph/menus/network-modifications/network-modification-node-editor.tsx
@@ -1193,7 +1193,7 @@ const NetworkModificationNodeEditor = () => {
     }, [modificationsToRestore.length, isAnyNodeBuilding, deleteInProgress]);
 
     const isCompositeNestingLimitReached = useMemo(
-        () => !selectedNetworkModifications.every((row) => (row.maxDepth ?? 0) < 5),
+        () => !selectedNetworkModifications.every((row) => (row.maxDepth ?? 0) < MAX_COMPOSITE_NESTING_DEPTH),
         [selectedNetworkModifications]
     );
 
