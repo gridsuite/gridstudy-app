02ca747cff278f67228190f2e87df3c8335f930f
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2026-06-01 10:11:31 +0200

    Add more equipment types for the global-filter in security-analysis N-K results (#3967)
    * Add more equipment types for the global-filter in security-analysis n-k results tab
    * Temporary remove of previously added HVDC_LINE equipment type, due to a bug in filter library
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/results/securityanalysis/security-analysis-result-tab.tsx
+++ src/components/results/securityanalysis/security-analysis-result-tab.tsx
@@ -265,7 +265,20 @@ export const SecurityAnalysisResultTab: FunctionComponent<SecurityAnalysisTabPro
 
     const filterableEquipmentTypes: EquipmentType[] = useMemo(() => {
         if (tabIndex === NMK_RESULTS_TAB_INDEX) {
-            return [EquipmentType.TWO_WINDINGS_TRANSFORMER, EquipmentType.LINE];
+            return [
+                EquipmentType.LINE,
+                EquipmentType.TWO_WINDINGS_TRANSFORMER,
+                EquipmentType.THREE_WINDINGS_TRANSFORMER,
+                EquipmentType.BATTERY,
+                EquipmentType.GENERATOR,
+                EquipmentType.LOAD,
+                EquipmentType.SHUNT_COMPENSATOR,
+                EquipmentType.STATIC_VAR_COMPENSATOR,
+                EquipmentType.BOUNDARY_LINE,
+                // TODO : temporary removed, waiting for a fix in filter library on nominal voltage filtering for hvdc line
+                //EquipmentType.HVDC_LINE,
+                EquipmentType.VSC_CONVERTER_STATION,
+            ];
         }
         return [];
     }, [tabIndex]);
