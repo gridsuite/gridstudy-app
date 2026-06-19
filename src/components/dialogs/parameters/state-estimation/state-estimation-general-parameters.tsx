a1dece41d267acf7d7e087e25d2a9f0ea7465dba
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2026-06-19 15:27:47 +0200

    resolving review comments

--- src/components/dialogs/parameters/state-estimation/state-estimation-general-parameters.tsx
+++ src/components/dialogs/parameters/state-estimation/state-estimation-general-parameters.tsx
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { Grid } from '@mui/material';
+import { Grid2 as Grid } from '@mui/material';
 import {
     ESTIM_ALGO_TYPE,
     ESTIM_LOG_LEVEL,
@@ -18,29 +18,29 @@ import { parametersStyles } from '../util/styles';
 export const StateEstimationGeneralParameters = () => {
     return (
         <Grid container>
-            <Grid container item alignItems="center" spacing={2} direction={'row'}>
-                <Grid item xs={10} sx={parametersStyles.parameterName}>
+            <Grid container size={12} alignItems="center" spacing={2} direction={'row'}>
+                <Grid size={10} sx={parametersStyles.parameterName}>
                     <FieldLabel label={'StateEstimationParametersPrincipalObservableZoneLabel'} />
                 </Grid>
-                <Grid item xs={2}>
+                <Grid size={2}>
                     <SwitchInput name={`${TabValue.GENERAL}.${PRINCIPAL_OBSERVABLE_ZONE}`} />
                 </Grid>
             </Grid>
 
-            <Grid container item alignItems="center" spacing={2} direction={'row'}>
-                <Grid item xs={10} sx={parametersStyles.parameterName}>
-                    <FieldLabel label={'StateEstimationParametersUniquePaseLabel'} />
+            <Grid container size={12} alignItems="center" spacing={2} direction={'row'}>
+                <Grid size={10} sx={parametersStyles.parameterName}>
+                    <FieldLabel label={'StateEstimationParametersUniquePhaseLabel'} />
                 </Grid>
-                <Grid item xs={2}>
+                <Grid size={2}>
                     <SwitchInput name={`${TabValue.GENERAL}.${UNIQUE_PHASE}`} />
                 </Grid>
             </Grid>
 
-            <Grid container item spacing={1} paddingTop={3}>
-                <Grid item xs={8} sx={parametersStyles.parameterName}>
+            <Grid container size={12} spacing={1} paddingTop={3}>
+                <Grid size={8} sx={parametersStyles.parameterName}>
                     <FieldLabel label={'StateEstimationParametersLogLevelLabel'} />
                 </Grid>
-                <Grid item xs={4}>
+                <Grid size={4}>
                     <MuiSelectInput
                         name={`${TabValue.GENERAL}.${ESTIM_LOG_LEVEL}`}
                         options={estimLogLevelValues}
@@ -48,11 +48,11 @@ export const StateEstimationGeneralParameters = () => {
                     />
                 </Grid>
             </Grid>
-            <Grid container item spacing={1} paddingTop={3}>
-                <Grid item xs={8} sx={parametersStyles.parameterName}>
+            <Grid container size={12} spacing={1} paddingTop={3}>
+                <Grid size={8} sx={parametersStyles.parameterName}>
                     <FieldLabel label={'StateEstimationParametersAlgoTypeLabel'} />
                 </Grid>
-                <Grid item xs={4}>
+                <Grid size={4}>
                     <MuiSelectInput
                         name={`${TabValue.GENERAL}.${ESTIM_ALGO_TYPE}`}
                         options={estimAlgoTypeValues}
