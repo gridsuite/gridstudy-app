9901442bcadc6336f04431ad47e92523b9f1778f
Radouane Khouadri <redouane.khouadri_externe@rte-france.com>
2026-06-22 10:46:44 +0200

    review fixes

--- src/components/dialogs/network-modifications/hvdc-line/lcc/modification/lcc-modification-form.tsx
+++ src/components/dialogs/network-modifications/hvdc-line/lcc/modification/lcc-modification-form.tsx
@@ -59,7 +59,7 @@ function LccModificationDialogHeader({ lccToModify }: Readonly<LccModificationHe
         />
     );
     return (
-        <Grid container spacing={2} size={12}>
+        <Grid container spacing={2} sx={{ width: '100%' }}>
             <GridItem size={4}>{LccIdField}</GridItem>
             <GridItem size={4}>{LccNameField}</GridItem>
         </Grid>
