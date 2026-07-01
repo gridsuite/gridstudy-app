6c146a23d365771dcc4f35d7e4100e45213b75b0
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-07-01 16:52:59 +0200

    Fix Grid migration

--- src/components/dialogs/network-modifications/voltage-level/move-feeder-bays/move-voltage-level-feeder-bays-form.tsx
+++ src/components/dialogs/network-modifications/voltage-level/move-feeder-bays/move-voltage-level-feeder-bays-form.tsx
@@ -342,7 +342,7 @@ export function MoveVoltageLevelFeederBaysForm({
             <Grid paddingTop={2}>
                 <FormattedMessage id={'moveFeederBaysSections'} />
             </Grid>
-            <Box sx={{ flexGrow: 1, minHeight: 0, pt: 1 }}>
+            <Box sx={{ pt: 1 }}>
                 <CustomAGGrid
                     rowData={groupedRowData}
                     defaultColDef={defaultColDef}
