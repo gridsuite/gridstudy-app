6c195734e31f4074175f6fadee27509c0f66db37
Etienne LESOT <etienne.lesot@rte-france.com>
2026-08-11 08:18:22 +0200

    fix duplication
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/dialogs/network-modifications/tabular/generation/prefillable-columns-config.ts
+++ src/components/dialogs/network-modifications/tabular/generation/prefillable-columns-config.ts
@@ -516,36 +516,6 @@ export const PREFILLED_COLUMNS_CONFIG: Record<string, PrefilledColumnGroup[]> = 
         },
         ...VOLTAGE_REGULATION_FIELDS,
         {
-            labelId: VOLTAGE_REGULATION_ON,
-            csvColumns: [VOLTAGE_REGULATION_ON],
-            networkFields: ['voltageRegulatorOn'],
-        },
-        {
-            labelId: TARGET_V,
-            csvColumns: [TARGET_V],
-            networkFields: ['targetV'],
-        },
-        {
-            labelId: REGULATION_TYPE,
-            csvColumns: [REGULATION_TYPE],
-            networkFields: ['regulationType'],
-        },
-        {
-            labelId: REGULATING_TERMINAL_ID,
-            csvColumns: [REGULATING_TERMINAL_ID],
-            networkFields: ['regulatingTerminalConnectableId'],
-        },
-        {
-            labelId: REGULATING_TERMINAL_TYPE,
-            csvColumns: [REGULATING_TERMINAL_TYPE],
-            networkFields: ['regulatingTerminalConnectableType'],
-        },
-        {
-            labelId: REGULATING_TERMINAL_VOLTAGE_LEVEL_ID,
-            csvColumns: [REGULATING_TERMINAL_VOLTAGE_LEVEL_ID],
-            networkFields: ['regulatingTerminalVlId'],
-        },
-        {
             labelId: PARTICIPATE,
             csvColumns: [PARTICIPATE],
             networkFields: ['activePowerControl.participate'],
