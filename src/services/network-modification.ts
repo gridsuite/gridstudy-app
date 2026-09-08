c56e4d9f0326f943dff2889a8e1a12ca0a96a97e
Hugo Marcellin <hugo.marcelin@rte-france.com>
2026-09-08 12:56:20 +0200

    Lint

--- src/services/network-modification.ts
+++ src/services/network-modification.ts
@@ -7,7 +7,6 @@
 
 import { backendFetchJson, LineTypeInfo } from '@gridsuite/commons-ui';
 import { PREFIX_STUDY_QUERIES } from './study';
-import type { UUID } from 'node:crypto';
 
 const PREFIX_NETWORK_MODIFICATION_QUERIES = PREFIX_STUDY_QUERIES + '/v1';
 
