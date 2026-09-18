22afd95d6b78b00d6115ef8e2eb38d09e4297d26
Florent MILLOT <75525996+flomillot@users.noreply.github.com>
2026-09-18 17:58:18 +0200

    Warn about shared modifications when renaming a root network tag
    Signed-off-by: Florent MILLOT <75525996+flomillot@users.noreply.github.com>

--- src/services/root-network.ts
+++ src/services/root-network.ts
@@ -90,6 +90,12 @@ export function checkRootNetworkTagExistence(studyUuid: UUID, tag: string): Prom
     });
 }
 
+export function hasSharedModifications(studyUuid: UUID): Promise<boolean> {
+    const hasSharedModificationsUrl = getStudyUrl(studyUuid) + '/network-modifications/references/exists';
+    console.debug(hasSharedModificationsUrl);
+    return backendFetchJson(hasSharedModificationsUrl);
+}
+
 export function getModifications(studyUuid: UUID, rootNetworkUuid: UUID, userInput: string) {
     const fetchUrl =
         getStudyUrl(studyUuid) +
